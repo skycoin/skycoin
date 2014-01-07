018849b93919a4eb03a59c01279adf23bd6a5915
TNN <tnnnow@gmail.com>
2014-01-07 18:16:05 -0500

    script to install pkg dependencies

---
+++ compile/getdeps.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$DIR" >/dev/null
+
+deps=($(grep -r "github.com" ../src/ ../main.go | cut -d ":" -f 2 | \
+    sed -e "s/[ \t]*//g" | sed -e "s/\"//g" | \
+    sed -e "s/import//" | sort | uniq))
+
+for i in "${deps[@]}"
+do
+    go get -u -v "$i"
+done
+
+popd >/dev/null
+
+# Compilation dependencies
+go get -u -v github.com/lonnc/golang-nw
+go install -v github.com/lonnc/golang-nw/cmd/golang-nw-pkg

979eeb4d991c15589e8441abe2c2dfaabecef043
aks <aks@skycoin.net>
2014-01-13 05:21:26 -0800

    transaction test

--- blockchain.go
+++ blockchain.go
@@ -101,27 +101,61 @@ func tests() {
 	genesisAddress = genesisWallet.AA[0].address
 	var BC *sb_coin.BlockChain = sb_coin.NewBlockChain(genesisAddress)
 
+	genesisWallet.RefeshUnspentOutputs(BC)
 	//create 16 wallets with 64 addresses
+	wn := 16 //number of wallets to create
 	WA := new([]Wallet)
-	for i := 0; i < 16; i++ {
+	for i := 0; i < wn; i++ {
 		WA = append(WA, NewWallet(64))
 	}
 
 	//pub, sec := util.GenerateKeyPair()
 	//pub := FromHex("02fa939957e9fc52140e180264e621c2576a1bfe781f88792fb315ca3d1786afb8")
 	//sec := FromHex("f2de015e7096a8b2416ea6232ea3ee2f9b928bf4672bc9801dea6ef9a0aee7a0")
-
-	fmt.Printf("sec: %s \n", ToHex(sec))
-	fmt.Printf("pub: %s \n", ToHex(pub))
-
-	if false {
-		fmt.Printf("l= %v\n", len(BC.Blocks))
-	}
+	//fmt.Printf("sec: %s \n", ToHex(sec))
+	//fmt.Printf("pub: %s \n", ToHex(pub))
 
 	B := BC.NewBlock()
 
 	var T sb_coin.Transaction
 
+	if true {
+		//create transaction by hand
+		ti := sb_coin.TransactionInput
+		ti.SigIdx = uint16(0)
+		ti.UxOut = genesisWallet.Outputs[0].Hash()
+		T.TI = append(T.TI, ti)
+
+		to := sb_coin.TransactionOutput
+		to.DestinationAddress = genesisWallet.AA[0].address
+		to.Value1 = 100*1000000 - wn*1000
+		T.TO = append(T.TO, to)
+
+		for i := 0; i < wn; i++ {
+			to := sb_coin.TransactionOutput
+			a := WA[i].GetRandom()
+			to.DestinationAddress = a.address
+			to.Value1 = 1000
+			to.Value2 = 1024 * 1024
+			T.TO = append(T.TO, to)
+		}
+
+		var sec sb_coin.SecKey
+		sec.Set(genesisAddress.seckey)
+		T.SetSig(0, sec)
+
+	} else {
+		T.PushInput(genesisWallet.Outputs[0].Hash())
+		T.PushOutput(genesisWallet.AA[0].address, 100*1000000-wn*1000, 0)
+		for i := 0; i < wn; i++ {
+			a := WA[i].GetRandom()
+			T.PushOutput(A.address, wn, 1024)
+		}
+
+		var sec sb_coin.SecKey
+		sec.Set(genesisAddress.seckey)
+		T.SetSig(0, sec)
+	}
 	T.UpdateHeader() //sets hash
 
 	/*

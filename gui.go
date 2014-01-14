3350da67ef8dbb5fd2c6c97ea33985599d76bf91
aks <aks@skycoin.net>
2014-01-14 04:05:01 -0800

    fixed

--- gui.go
+++
@@ -1,5 +0,0 @@
-package main
-import "net/http"
-func main() {
-        panic(http.ListenAndServe(":8888", http.FileServer(http.Dir("static/app/"))))
-}
--- wallet.go
+++ wallet.go
@@ -1,5 +1,9 @@
 package main
+
 import "net/http"
+import "fmt"
+
 func main() {
-        panic(http.ListenAndServe(":8080", http.FileServer(http.Dir("static/app/"))))
+	fmt.Printf("Wallet runnign on port 127.0.0.1:8888 \n")
+	panic(http.ListenAndServe(":8888", http.FileServer(http.Dir("static/app/"))))
 }

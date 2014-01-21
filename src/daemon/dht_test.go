69305447c235629cfcbe3f5eb6c3acbe0fdd2b45
TNN <tnnnow@gmail.com>
2014-01-20 21:23:04 -0500

    daemon tests

---
+++ src/daemon/dht_test.go
@@ -0,0 +1,48 @@
+package daemon
+
+import (
+    "github.com/nictuku/dht"
+    "github.com/skycoin/pex"
+    "github.com/stretchr/testify/assert"
+    "testing"
+)
+
+var (
+    port = 6677
+)
+
+func resetDHT() {
+    DHT = nil
+    dhtInfoHash = ""
+}
+
+func TestInitDHT(t *testing.T) {
+    assert.Nil(t, DHT)
+    assert.Equal(t, string(dhtInfoHash), "")
+    InitDHT(port)
+    assert.NotNil(t, DHT)
+    assert.NotEqual(t, string(dhtInfoHash), "")
+    resetDHT()
+}
+
+func TestReceivedDHTPeers(t *testing.T) {
+    Peers = pex.NewPex(maxPeers)
+    m := make(map[dht.InfoHash][]string)
+    peers := make([]string, 0)
+    peers = append(peers, string([]byte{013, 026, 041, 054, 013, 013}))
+    peers = append(peers, string([]byte{013, 026, 041, 055, 013, 013}))
+    m[dht.InfoHash("")] = peers
+    receivedDHTPeers(m)
+    assert.Equal(t, len(Peers.Peerlist), 2)
+    assert.NotNil(t, Peers.Peerlist["11.22.33.45:2827"])
+    assert.NotNil(t, Peers.Peerlist["11.22.33.44:2827"])
+    resetPeers()
+}
+
+func TestRequestDHTPeers(t *testing.T) {
+    resetDHT()
+    assert.Panics(t, RequestDHTPeers)
+    InitDHT(port)
+    assert.NotPanics(t, RequestDHTPeers)
+    resetDHT()
+}
---
+++ src/daemon/peers_test.go
@@ -0,0 +1,69 @@
+package daemon
+
+import (
+    "github.com/skycoin/pex"
+    "github.com/stretchr/testify/assert"
+    "os"
+    "testing"
+)
+
+func TestInitPeers(t *testing.T) {
+    Peers = pex.NewPex(maxPeers)
+    assert.Panics(t, func() { InitPeers("x") })
+    Peers = nil
+
+    // Write dummy peer db
+    fn := "./" + pex.PeerDatabaseFilename
+    os.Remove(fn)
+    os.Remove("./" + pex.BlacklistedDatabaseFilename)
+    f, err := os.Create(fn)
+    defer os.Remove(fn)
+    defer os.Remove("./" + pex.BlacklistedDatabaseFilename)
+    assert.Nil(t, err)
+    if err != nil {
+        t.Fatalf("Error creating %s", fn)
+    }
+    _, err = f.Write([]byte(addr + "\n"))
+    assert.Nil(t, err)
+    f.Close()
+
+    assert.NotPanics(t, func() { InitPeers("./") })
+    assert.NotNil(t, Peers)
+    assert.Equal(t, len(Peers.Peerlist), 1)
+    assert.NotNil(t, Peers.Peerlist[addr])
+}
+
+func TestShutdownPeers(t *testing.T) {
+    SetupPeersShutdown(t)
+    ShutdownPeers("./")
+    ConfirmPeersShutdown(t)
+}
+
+func SetupPeersShutdown(t *testing.T) {
+    os.Remove("./" + pex.BlacklistedDatabaseFilename)
+    os.Remove("./" + pex.PeerDatabaseFilename)
+    fn := "./" + pex.PeerDatabaseFilename
+    _, err := os.Stat(fn)
+    if err == nil {
+        os.Remove(fn)
+    }
+    Peers = pex.NewPex(maxPeers)
+    _, err = Peers.AddPeer(addr)
+    assert.Nil(t, err)
+}
+
+func ConfirmPeersShutdown(t *testing.T) {
+    defer os.Remove("./" + pex.BlacklistedDatabaseFilename)
+    defer os.Remove("./" + pex.PeerDatabaseFilename)
+    assert.Nil(t, Peers)
+
+    f, err := os.Open("./" + pex.PeerDatabaseFilename)
+    assert.Nil(t, err)
+    if err != nil {
+        t.Fatalf("Failed to open %s")
+    }
+    b := make([]byte, len(addr)*2)
+    n, err := f.Read(b)
+    assert.Nil(t, err)
+    assert.Equal(t, string(b[:n]), addr+"\n")
+}

bc6d57761eec3cbde8e2be4774c9dfe51bf4d0d3
TNN <tnnnow@gmail.com>
2014-01-20 23:52:31 -0500

    api method example with network stats

--- src/gui/network.go
+++ src/gui/network.go
@@ -2,19 +2,41 @@
 package gui
 
 import (
+    "github.com/skycoin/skycoin/src/daemon"
     "net/http"
 )
 
-type PeersMessage struct {
-    Count int
-    Peers []string
+type Connection struct {
+    addr          string
+    last_sent     int64
+    last_received int64
 }
 
-func peersPage(w http.ResponseWriter, r *http.Request) {
-    m := PeersMessage{}
-    SendJSON(w, m)
+type Connections struct {
+    connections []Connection
+}
+
+func connectionsPage(w http.ResponseWriter, r *http.Request) {
+    if daemon.Pool == nil {
+        // the daemon is not initialized
+        Error404(w)
+        return
+    }
+
+    conns := make([]Connection, len(daemon.Pool.Pool))
+    for _, v := range daemon.Pool.Pool {
+        conns = append(conns, Connection{
+            addr:          v.Addr(),
+            last_sent:     v.LastSent.Unix(),
+            last_received: v.LastReceived.Unix(),
+        })
+    }
+
+    if SendJSON(w, &Connections{conns}) != nil {
+        Error500(w)
+    }
 }
 
 func RegisterNetworkHandlers(mux *http.ServeMux) {
-    mux.HandleFunc("/peers", peersPage)
+    mux.HandleFunc("/api/network/connections", connectionsPage)
 }

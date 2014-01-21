d1cb8e7e607b72414ff67733cb4d3cabd07f97fb
aks <aks@skycoin.net>
2014-01-21 08:03:40 -0800

    Random transaction

--- cmd/blockchain/blockchain.go
+++ cmd/blockchain/blockchain.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-    "encoding/hex"
-    "fmt"
-    "github.com/skycoin/skycoin/src/coin"
-    "log"
-    "math/rand"
+	"encoding/hex"
+	"fmt"
+	"github.com/skycoin/skycoin/src/coin"
+	"log"
+	"math/rand"
 )
 
 func Run() {
-    tests()
+	tests()
 }
 
 func ToHex(b []byte) string {
-    return hex.EncodeToString(b)
+	return hex.EncodeToString(b)
 }
 
 func FromHex(s string) []byte {
-    b, err := hex.DecodeString(s)
-    if err != nil {
-        log.Panic(err)
-    }
-    return b
+	b, err := hex.DecodeString(s)
+	if err != nil {
+		log.Panic(err)
+	}
+	return b
 }
 
 /*
@@ -32,73 +32,73 @@ func FromHex(s string) []byte {
 */
 
 type Wallet struct {
-    AA      []Address    //address array
-    Outputs []coin.UxOut //unspent outputs
+	AA      []Address    //address array
+	Outputs []coin.UxOut //unspent outputs
 }
 
 func (self *Wallet) GetRandom() Address {
-    index := rand.Int() % len(self.AA)
-    return self.AA[index]
+	index := rand.Int() % len(self.AA)
+	return self.AA[index]
 }
 
 //signs a hash for a given address. nil on failure
 func (self *Wallet) Sign(address coin.Address, hash []byte) []byte {
-    for _, a := range self.AA {
-        if a.address == address {
-            //func GenerateSignature(seckey []byte, msg []byte) []byte
-            sig := coin.GenerateSignature(a.seckey, hash)
-            return sig
-        }
+	for _, a := range self.AA {
+		if a.address == address {
+			//func GenerateSignature(seckey []byte, msg []byte) []byte
+			sig := coin.GenerateSignature(a.seckey, hash)
+			return sig
+		}
 
-    }
-    return nil
+	}
+	return nil
 }
 
 //refresh the unspent outputs for the wallet
 func (self *Wallet) RefeshUnspentOutputs(bc *coin.BlockChain) {
-    var outputs []coin.UxOut
-    for _, a := range self.AA {
-        //get unspent outputs for the address
-        unspentOutputs := bc.GetUnspentOutputs(a.address)
-        outputs = append(outputs, unspentOutputs...)
-    }
-    self.Outputs = outputs
+	var outputs []coin.UxOut
+	for _, a := range self.AA {
+		//get unspent outputs for the address
+		unspentOutputs := bc.GetUnspentOutputs(a.address)
+		outputs = append(outputs, unspentOutputs...)
+	}
+	self.Outputs = outputs
 }
 
 func (self *Wallet) Balance(bc *coin.BlockChain) (uint64, uint64) {
-    self.RefeshUnspentOutputs(bc)
+	self.RefeshUnspentOutputs(bc)
 
-    var balance1 uint64 = 0
-    var balance2 uint64 = 0
+	var balance1 uint64 = 0
+	var balance2 uint64 = 0
 
-    var time uint64 = bc.Head.Header.Time
+	var time uint64 = bc.Head.Header.Time
 
-    for _, ux := range self.Outputs {
-        balance1 += ux.Body.Value1
-        balance2 += ux.CoinHours(time)
-    }
-    return balance1, balance2
+	for _, ux := range self.Outputs {
+		balance1 += ux.Body.Value1
+		balance2 += ux.CoinHours(time)
+	}
+	return balance1, balance2
 }
 
 func NewWallet(n int) Wallet {
-    var w Wallet
-    for i := 0; i < n; i++ {
-        w.AA = append(w.AA, GenerateAddress())
-    }
-    return w
+	var w Wallet
+	for i := 0; i < n; i++ {
+		w.AA = append(w.AA, GenerateAddress())
+	}
+	return w
 }
 
 type Address struct {
-    pubkey  []byte
-    seckey  []byte
-    address coin.Address
+	pubkey  []byte
+	seckey  []byte
+	address coin.Address
 }
 
 func GenerateAddress() Address {
-    var A Address
-    A.pubkey, A.seckey = coin.GenerateKeyPair()
-    A.address = coin.AddressFromRawPubkey(A.pubkey)
-    return A
+	var A Address
+	A.pubkey, A.seckey = coin.GenerateKeyPair()
+	A.address = coin.AddressFromRawPubkey(A.pubkey)
+	return A
 }
 
 //func (self *Address) GetOutputs(bc coin.BlockChain) []coin.UxOut {
@@ -113,95 +113,113 @@ func GenerateAddress() Address {
 //fmt.Printf("pub: %s \n", ToHex(pub))
 
 func WalletBalances(bc *coin.BlockChain, wallets []Wallet) {
-    for i, w := range wallets {
+	for i, w := range wallets {
 
-        b1, b2 := w.Balance(bc)
-        fmt.Printf("%v: %v %v \n", i, b1, b2)
-    }
+		b1, b2 := w.Balance(bc)
+		fmt.Printf("%v: %v %v \n", i, b1, b2)
+	}
 
-    for i, ux := range bc.Unspent {
-        fmt.Printf("%v: %v \n", i, ux.String())
-    }
+	for i, ux := range bc.Unspent {
+		fmt.Printf("%v: %v \n", i, ux.String())
+	}
+}
+
+/*
+   Transaction set
+*/
+type PendingTransactions struct {
+	A []coin.Transaction
+}
+
+func (self *PendingTransactions) Push(T coin.Transaction) {
+	self.A = append(self.A, T)
+}
+
+func (self *PendingTransactions) Rand() coin.Transaction {
+	i := rand.Int() % len(self.A) //random element
+	element := self.A[i]
+	self.A = append(self.A[:i], self.A[i+1:]...) //remove ith element
+	return element
 }
 
 func tests() {
 
-    genesisWallet := NewWallet(1)
-    genesisAddress := genesisWallet.AA[0]
-    var BC *coin.BlockChain = coin.NewBlockChain(genesisAddress.address)
+	genesisWallet := NewWallet(1)
+	genesisAddress := genesisWallet.AA[0]
+	var BC *coin.BlockChain = coin.NewBlockChain(genesisAddress.address)
 
-    genesisWallet.RefeshUnspentOutputs(BC)
-    //create 16 wallets with 64 addresses
-    wn := 16 //number of wallets to create
-    var WA []Wallet
-    for i := 0; i < wn; i++ {
-        WA = append(WA, NewWallet(64))
-    }
+	genesisWallet.RefeshUnspentOutputs(BC)
+	//create 16 wallets with 64 addresses
+	wn := 16 //number of wallets to create
+	var WA []Wallet
+	for i := 0; i < wn; i++ {
+		WA = append(WA, NewWallet(64))
+	}
 
-    B := BC.NewBlock()
+	B := BC.NewBlock()
 
-    var T coin.Transaction
+	var T coin.Transaction
 
-    if true {
-        //create transaction by hand
-        var ti coin.TransactionInput
-        ti.SigIdx = uint16(0)
-        ti.UxOut = genesisWallet.Outputs[0].Hash()
-        T.TI = append(T.TI, ti)
+	if true {
+		//create transaction by hand
+		var ti coin.TransactionInput
+		ti.SigIdx = uint16(0)
+		ti.UxOut = genesisWallet.Outputs[0].Hash()
+		T.TI = append(T.TI, ti)
 
-        var to coin.TransactionOutput
-        to.DestinationAddress = genesisWallet.AA[0].address
-        to.Value1 = uint64(100*1000000 - wn*1000)
-        T.TO = append(T.TO, to)
+		var to coin.TransactionOutput
+		to.DestinationAddress = genesisWallet.AA[0].address
+		to.Value1 = uint64(100*1000000 - wn*1000)
+		T.TO = append(T.TO, to)
 
-        for i := 0; i < wn; i++ {
-            var to coin.TransactionOutput
-            a := WA[i].GetRandom()
-            to.DestinationAddress = a.address
-            to.Value1 = 1000
-            to.Value2 = 1024
-            T.TO = append(T.TO, to)
-        }
+		for i := 0; i < wn; i++ {
+			var to coin.TransactionOutput
+			a := WA[i].GetRandom()
+			to.DestinationAddress = a.address
+			to.Value1 = 1000
+			to.Value2 = 1024
+			T.TO = append(T.TO, to)
+		}
 
-        var sec coin.SecKey
-        sec.Set(genesisAddress.seckey)
-        T.SetSig(0, sec)
+		var sec coin.SecKey
+		sec.Set(genesisAddress.seckey)
+		T.SetSig(0, sec)
 
-    } else {
-        T.PushInput(genesisWallet.Outputs[0].Hash())
-        T.PushOutput(genesisWallet.AA[0].address, uint64(100*1000000-wn*1000), 0)
+	} else {
+		T.PushInput(genesisWallet.Outputs[0].Hash())
+		T.PushOutput(genesisWallet.AA[0].address, uint64(100*1000000-wn*1000), 0)
 
-        for i := 0; i < wn; i++ {
-            a := WA[i].GetRandom()
-            T.PushOutput(a.address, uint64(1000), 1024*1024)
-        }
+		for i := 0; i < wn; i++ {
+			a := WA[i].GetRandom()
+			T.PushOutput(a.address, uint64(1000), 1024*1024)
+		}
 
-        var sec coin.SecKey
-        sec.Set(genesisAddress.seckey)
-        T.SetSig(0, sec)
-    }
-    T.UpdateHeader() //sets hash
+		var sec coin.SecKey
+		sec.Set(genesisAddress.seckey)
+		T.SetSig(0, sec)
+	}
+	T.UpdateHeader() //sets hash
 
-    fmt.Printf("genesis transaction: \n")
-    /*
-       Need to add input
-    */
-    err := BC.AppendTransaction(B, &T)
+	fmt.Printf("genesis transaction: \n")
+	/*
+	   Need to add input
+	*/
+	err := BC.AppendTransaction(B, &T)
 
-    if err != nil {
-        log.Panic(err)
-    }
+	if err != nil {
+		log.Panic(err)
+	}
 
-    WalletBalances(BC, WA)
-    err = BC.ExecuteBlock(B)
-    if err != nil {
-        log.Panic(err)
-    }
+	WalletBalances(BC, WA)
+	err = BC.ExecuteBlock(B)
+	if err != nil {
+		log.Panic(err)
+	}
 
-    WalletBalances(BC, WA)
+	WalletBalances(BC, WA)
 
 }
 
 func main() {
-    tests()
+	tests()
 }

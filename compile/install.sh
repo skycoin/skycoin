4113496b26e07e93cd75691e1580500a256749f6
TNN <tnnnow@gmail.com>
2014-01-22 23:31:08 -0500

    install script

--- compile/install.sh
+++ compile/install.sh
@@ -1,14 +1,12 @@
+#!/usr/bin/env bash
+
 # Install gvm and go1.2
 bash < <(curl -s https://raw.github.com/moovweb/gvm/master/binscripts/gvm-installer)
 gvm install go1.2
 echo "gvm use go1.2" >> ~/.bashrc
 source ~/.bashrc
-
 # Install dependencies
 ./compile/getdeps.sh
-
 # Add to $GOPATH
 ./compile/install-to-gopath.sh
 
-# Run
-./run.sh

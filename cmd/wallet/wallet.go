759dd97996de027adebaf02443365a81ceac07eb
aks <aks@skycoin.net>
2014-01-25 09:10:54 -0800

    Stuff

--- cmd/wallet/wallet.go
+++ cmd/wallet/wallet.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	static_path,_ := filepath.Abs(".static/")
+	static_path,_ := filepath.Abs("static/")
 	gui.LaunchWebInterface("127.0.0.1", 6060, static_path) //does not work for 6666a
 }
 //func LaunchWebInterface(addr string, port int)

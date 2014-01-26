58fb81d304c6d00bec6116664abf29ad2f8154d3
asmercrof <asmercroft@mir.com>
2014-01-26 01:39:52 -0800

    api test

--- cmd/wallet/wallet.go
+++ cmd/wallet/wallet.go
@@ -1,12 +1,62 @@
 package main
 
 import (
-    "github.com/skycoin/skycoin/src/gui"
-    //"path/filepath"
+	"encoding/json"
+	"fmt"
+	"github.com/op/go-logging"
+	"github.com/skycoin/skycoin/src/keyring"
+	//"io"
+	"net/http"
+	//"os"
+	"path/filepath"
+	//"log"
 )
 
-func main() {
-	//static_path,_ := filepath.Abs("static/")
-	gui.LaunchWebInterface("127.0.0.1:6060", "static") //does not work for 6666a
+type Profile struct {
+	Name    string
+	Hobbies []string
 }
-//func LaunchWebInterface(addr string, port int)
+
+type walletData struct {
+	Seed      string
+	Addresses []string
+	History   []string
+}
+
+var (
+	logger = logging.MustGetLogger("skycoin.gui")
+)
+
+var walletFile = walletData{}
+
+func main() {
+	static_path, _ := filepath.Abs("../../static/app/")
+	logger.Debug("Serving %s", static_path)
+
+	http.Handle("/", http.FileServer(http.Dir(static_path)))
+
+	http.HandleFunc("/api/newAdress/", newAddress)
+
+	http.ListenAndServe(":3003", nil)
+}
+
+func newAddress(w http.ResponseWriter, r *http.Request) {
+
+	logger.Debug("Serving %s", r)
+
+	//js, err := json.Marshal(profile)
+	addr := keyring.GenerateAddress()
+
+	//walletFile.Addresses = append(walletFile.Addresses, addr)
+	fmt.Printf("address= %s \n", addr.Address.String())
+
+	js, err := json.Marshal(addr.Address.String())
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}

b76c751aca34ea8fa4c7c51ad8b060c4bac463df
aks <aks@skycoin.net>
2014-01-25 19:34:06 -0800

    keyring

--- src/keyring/wallet.go
+++ src/keyring/wallet.go
@@ -2,16 +2,12 @@ package keyring
 
 import (
 	"encoding/hex"
-	"errors"
+	//"errors"
 	"fmt"
 	"github.com/skycoin/skycoin/src/coin"
 	"log"
 	"math/rand"
 )
-
-func Run() {
-	tests()
-}
 
 func ToHex(b []byte) string {
 	return hex.EncodeToString(b)
@@ -75,7 +71,7 @@ func (self *Wallet) Balance(bc *coin.BlockChain) (uint64, uint64) {
 	var time uint64 = bc.Head.Header.Time
 
 	for _, ux := range self.Outputs {
-		balance1 += ux.Body.Value1
+		balance1 += ux.Body.Coins
 		balance2 += ux.CoinHours(time)
 	}
 	return balance1, balance2
@@ -164,102 +160,3 @@ func WalletBalances(bc *coin.BlockChain, wallets []Wallet) {
 		fmt.Printf("%v: %v \n", i, ux.String())
 	}
 }
-
-/*
-   Transaction set
-*/
-type PendingTransactions struct {
-	A []coin.Transaction
-}
-
-func (self *PendingTransactions) Push(T coin.Transaction) {
-	self.A = append(self.A, T)
-}
-
-func (self *PendingTransactions) Rand() coin.Transaction {
-	i := rand.Int() % len(self.A) //random element
-	element := self.A[i]
-	self.A = append(self.A[:i], self.A[i+1:]...) //remove ith element
-	return element
-}
-
-/*
-func tests() {
-
-	genesisWallet := NewWallet(1)
-	genesisAddress := genesisWallet.AA[0]
-	var BC *coin.BlockChain = coin.NewBlockChain(genesisAddress.address)
-
-	genesisWallet.RefeshUnspentOutputs(BC)
-	//create 16 wallets with 64 addresses
-	wn := 16 //number of wallets to create
-	var WA []Wallet
-	for i := 0; i < wn; i++ {
-		WA = append(WA, NewWallet(64))
-	}
-
-	B := BC.NewBlock()
-
-	var T coin.Transaction
-
-	if true {
-		//create transaction by hand
-		var ti coin.TransactionInput
-		ti.SigIdx = uint16(0)
-		ti.UxOut = genesisWallet.Outputs[0].Hash()
-		T.TI = append(T.TI, ti)
-
-		var to coin.TransactionOutput
-		to.DestinationAddress = genesisWallet.AA[0].address
-		to.Value1 = uint64(100*1000000 - wn*1000)
-		T.TO = append(T.TO, to)
-
-		for i := 0; i < wn; i++ {
-			var to coin.TransactionOutput
-			a := WA[i].GetRandom()
-			to.DestinationAddress = a.address
-			to.Value1 = 1000
-			to.Value2 = 1024
-			T.TO = append(T.TO, to)
-		}
-
-		var sec coin.SecKey
-		sec.Set(genesisAddress.seckey)
-		T.SetSig(0, sec)
-
-	} else {
-		T.PushInput(genesisWallet.Outputs[0].Hash())
-		T.PushOutput(genesisWallet.AA[0].address, uint64(100*1000000-wn*1000), 0)
-
-		for i := 0; i < wn; i++ {
-			a := WA[i].GetRandom()
-			T.PushOutput(a.address, uint64(1000), 1024*1024)
-		}
-
-		var sec coin.SecKey
-		sec.Set(genesisAddress.seckey)
-		T.SetSig(0, sec)
-	}
-	T.UpdateHeader() //sets hash
-
-	fmt.Printf("genesis transaction: \n")
-
-	//Need to add input
-
-	err := BC.AppendTransaction(B, &T)
-
-	if err != nil {
-		log.Panic(err)
-	}
-
-	WalletBalances(BC, WA)
-	err = BC.ExecuteBlock(B)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	WalletBalances(BC, WA)
-
-}
-
-*/

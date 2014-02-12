a4e00dda18da466ba9e7cedf72fc9eb3d708b6aa
aks <aks@skycoin.net>
2014-02-12 12:24:57 -0800

    last refactor before launch

---
+++ src/rpc/wallet.go
@@ -0,0 +1,285 @@
+package rpc
+
+import (
+    "errors"
+    "fmt"
+    "github.com/skycoin/skycoin/src/coin"
+    "github.com/skycoin/skycoin/src/util"
+    "log"
+)
+
+type WalletEntry struct {
+    Address coin.Address
+    Public  coin.PubKey
+    Secret  coin.SecKey
+}
+
+func NewWalletEntry() WalletEntry {
+    pub, sec := coin.GenerateKeyPair()
+    return WalletEntry{
+        Address: coin.AddressFromPubKey(pub),
+        Public:  pub,
+        Secret:  sec,
+    }
+}
+
+func WalletEntryFromReadable(w *ReadableWalletEntry) WalletEntry {
+    // Wallet entries are shared as a form of identification, the secret key
+    // is not required
+    // TODO -- fix lib/base58 to not panic on invalid input -- should
+    // return error, so we can detect a broken wallet.
+    if w.Address == "" {
+        log.Panic("ReadableWalletEntry has no Address")
+    }
+    var s coin.SecKey
+    if w.Secret != "" {
+        s = coin.SecKeyFromHex(w.Secret)
+    }
+    return WalletEntry{
+        Address: coin.MustDecodeBase58Address(w.Address),
+        Public:  coin.PubKeyFromHex(w.Public),
+        Secret:  s,
+    }
+}
+
+// Loads a WalletEntry from filename, where the file contains a
+// ReadableWalletEntry
+func LoadWalletEntry(filename string) (WalletEntry, error) {
+    w, err := LoadReadableWalletEntry(filename)
+    if err != nil {
+        return WalletEntry{}, err
+    } else {
+        return WalletEntryFromReadable(&w), nil
+    }
+}
+
+// Loads a WalletEntry from filename but panics is unable to load or contents
+// are invalid
+func MustLoadWalletEntry(filename string) WalletEntry {
+    keys, err := LoadWalletEntry(filename)
+    if err != nil {
+        log.Panicf("Failed to load wallet entry: %v", err)
+    }
+    if err := keys.Verify(); err != nil {
+        log.Panicf("Invalid wallet entry: %v", err)
+    }
+    return keys
+}
+
+// Checks that the public key is derivable from the secret key,
+// and that the public key is associated with the address
+func (self *WalletEntry) Verify() error {
+    if coin.PubKeyFromSecKey(self.Secret) != self.Public {
+        return errors.New("Invalid public key for secret key")
+    }
+    return self.VerifyPublic()
+}
+
+// Checks that the public key is associated with the address
+func (self *WalletEntry) VerifyPublic() error {
+    if err := self.Public.Verify(); err != nil {
+        return err
+    } else {
+        return self.Address.Verify(self.Public)
+    }
+}
+
+type Balance struct {
+    Coins uint64 `json:"coins"`
+    Hours uint64 `json:"hours"`
+}
+
+func NewBalance(coins, hours uint64) Balance {
+    return Balance{
+        Coins: coins,
+        Hours: hours,
+    }
+}
+
+func (self Balance) Add(other Balance) Balance {
+    return Balance{
+        Coins: self.Coins + other.Coins,
+        Hours: self.Hours + other.Hours,
+    }
+}
+
+// Subtracts other from self and returns the new Balance.  Will panic if
+// other is greater than balance, because Coins and Hours are unsigned.
+func (self Balance) Sub(other Balance) Balance {
+    if other.GreaterThan(self) {
+        log.Panic("Cannot subtract balances, second balance is too large")
+    }
+    return Balance{
+        Coins: self.Coins - other.Coins,
+        Hours: self.Hours - other.Hours,
+    }
+}
+
+func (self Balance) GreaterThan(other Balance) bool {
+    return self.Coins > other.Coins && self.Hours > other.Hours
+}
+
+func (self Balance) GreaterThanOrEqual(other Balance) bool {
+    return self.Coins >= other.Coins && self.Hours >= other.Hours
+}
+
+func (self Balance) IsZero() bool {
+    return self.Coins == 0 && self.Hours == 0
+}
+
+// Simplest wallet implementation
+type Wallet struct {
+    Entries map[coin.Address]WalletEntry
+}
+
+func NewWallet() *Wallet {
+    return &Wallet{
+        Entries: make(map[coin.Address]WalletEntry),
+    }
+}
+
+func NewWalletFromReadable(r *ReadableWallet) *Wallet {
+    entries := make(map[coin.Address]WalletEntry, len(r.Entries))
+    for _, re := range r.Entries {
+        we := WalletEntryFromReadable(&re)
+        if err := we.Verify(); err != nil {
+            log.Panicf("Invalid wallet entry loaded. Address: %s", re.Address)
+        }
+        entries[we.Address] = we
+    }
+    return &Wallet{
+        Entries: entries,
+    }
+}
+
+// Creates a WalletEntry
+func (self *Wallet) CreateEntry() WalletEntry {
+    e := NewWalletEntry()
+    if err := e.Verify(); err != nil {
+        log.Panic("Creating invalid wallet entry: %v", err)
+    }
+    if err := self.AddEntry(e); err != nil {
+        log.Panic("Somehow, we managed to create a duplicate Address: %v", err)
+    }
+    return e
+}
+
+// Creates new WalletEntries to fill the wallet up to n.  No WalletEntries
+// are created if the Wallet already contains n or more entries.
+func (self *Wallet) populate(n int) {
+    for i := len(self.Entries); i < n; i++ {
+        self.CreateEntry()
+    }
+}
+
+// Returns all coin.Addresses in this Wallet
+func (self *Wallet) GetAddresses() []coin.Address {
+    addrs := make([]coin.Address, 0, len(self.Entries))
+    for a, _ := range self.Entries {
+        addrs = append(addrs, a)
+    }
+    return addrs
+}
+
+// Returns the WalletEntry for a coin.Address
+func (self *Wallet) GetEntry(a coin.Address) (WalletEntry, bool) {
+    we, exists := self.Entries[a]
+    return we, exists
+}
+
+// Adds a WalletEntry to the wallet. Returns an error if the coin.Address is
+// already in the wallet
+func (self *Wallet) AddEntry(e WalletEntry) error {
+    _, exists := self.Entries[e.Address]
+    if exists {
+        return fmt.Errorf("Wallet entry already exists for address %s",
+            e.Address.String())
+    } else {
+        self.Entries[e.Address] = e
+        return nil
+    }
+}
+
+// Saves to filename
+func (self *Wallet) Save(filename string) error {
+    r := NewReadableWallet(self)
+    return r.Save(filename)
+}
+
+// Loads from filename
+func (self *Wallet) Load(filename string) error {
+    r := &ReadableWallet{}
+    if err := r.Load(filename); err != nil {
+        return err
+    }
+    *self = *(NewWalletFromReadable(r))
+    return nil
+}
+
+type ReadableWalletEntry struct {
+    Address string `json:"address"`
+    Public  string `json:"public_key"`
+    Secret  string `json:"secret_key"`
+}
+
+func NewReadableWalletEntry(w *WalletEntry) ReadableWalletEntry {
+    return ReadableWalletEntry{
+        Address: w.Address.String(),
+        Public:  w.Public.Hex(),
+        Secret:  w.Secret.Hex(),
+    }
+}
+
+func LoadReadableWalletEntry(filename string) (ReadableWalletEntry, error) {
+    w := ReadableWalletEntry{}
+    err := util.LoadJSON(filename, &w)
+    return w, err
+}
+
+// Creates a ReadableWalletEntry given a pubkey hex string.  The Secret field
+// is left empty.
+func ReadableWalletEntryFromPubkey(pub string) ReadableWalletEntry {
+    pubkey := coin.PubKeyFromHex(pub)
+    addr := coin.AddressFromPubKey(pubkey)
+    return ReadableWalletEntry{
+        Address: addr.String(),
+        Public:  pub,
+    }
+}
+
+func (self *ReadableWalletEntry) Save(filename string) error {
+    return util.SaveJSONSafe(filename, self, 0600)
+}
+
+// Used for [de]serialization of the Wallet
+type ReadableWallet struct {
+    Entries []ReadableWalletEntry `json:"entries"`
+}
+
+// Converts a Wallet to a ReadableWallet
+func NewReadableWallet(w *Wallet) *ReadableWallet {
+    readable := make([]ReadableWalletEntry, 0, len(w.Entries))
+    for _, e := range w.Entries {
+        readable = append(readable, NewReadableWalletEntry(&e))
+    }
+    return &ReadableWallet{
+        Entries: readable,
+    }
+}
+
+// Loads a ReadableWallet from disk
+func LoadReadableWallet(filename string) (*ReadableWallet, error) {
+    w := &ReadableWallet{}
+    err := w.Load(filename)
+    return w, err
+}
+
+// Saves to filename
+func (self *ReadableWallet) Save(filename string) error {
+    return util.SaveJSON(filename, self, 0600)
+}
+
+// Loads from filename
+func (self *ReadableWallet) Load(filename string) error {
+    return util.LoadJSON(filename, self)
+}
---
+++ src/rpc/wallet_test.go
@@ -0,0 +1,361 @@
+package rpc
+
+import (
+    "github.com/skycoin/skycoin/src/coin"
+    "github.com/stretchr/testify/assert"
+    "os"
+    "testing"
+)
+
+func assertFileMode(t *testing.T, filename string, mode os.FileMode) {
+    stat, err := os.Stat(filename)
+    assert.Nil(t, err)
+    assert.Equal(t, stat.Mode(), mode)
+}
+
+func TestNewWalletEntry(t *testing.T) {
+    we := NewWalletEntry()
+    assert.NotEqual(t, we.Address, coin.Address{})
+    assert.NotEqual(t, we.Public, coin.PubKey{})
+    assert.NotEqual(t, we.Secret, coin.SecKey{})
+    assert.Nil(t, we.Public.Verify())
+    assert.Nil(t, we.Secret.Verify())
+    assert.Nil(t, we.Address.Verify(we.Public))
+}
+
+func TestWalletEntryFromReadable(t *testing.T) {
+    we := NewWalletEntry()
+    rwe := NewReadableWalletEntry(&we)
+    we2 := WalletEntryFromReadable(&rwe)
+    assert.Equal(t, we, we2)
+
+    // No address, panics
+    addr := rwe.Address
+    rwe.Address = ""
+    assert.Panics(t, func() { WalletEntryFromReadable(&rwe) })
+
+    // No secret key is ok
+    rwe.Address = addr
+    rwe.Secret = ""
+    we = WalletEntryFromReadable(&rwe)
+    assert.Equal(t, we.Secret, coin.SecKey{})
+    assert.Equal(t, we.Public, we2.Public)
+    assert.Equal(t, we.Address, we2.Address)
+}
+
+func TestLoadWalletEntry(t *testing.T) {
+    defer cleanupVisor()
+    we := NewWalletEntry()
+    rwe := NewReadableWalletEntry(&we)
+    assert.Nil(t, rwe.Save(testWalletEntryFile))
+    assertFileMode(t, testWalletEntryFile, 0600)
+    assertFileExists(t, testWalletEntryFile)
+    we2, err := LoadWalletEntry(testWalletEntryFile)
+    assert.Nil(t, err)
+    assert.Equal(t, we, we2)
+
+    // No file, returns error
+    cleanupVisor()
+    _, err = LoadWalletEntry(testWalletEntryFile)
+    assert.NotNil(t, err)
+}
+
+func TestMustLoadLoadWalletEntry(t *testing.T) {
+    defer cleanupVisor()
+    // File doesn't exist, panics
+    assertFileNotExists(t, testWalletEntryFile)
+    assert.Panics(t, func() { MustLoadWalletEntry(testWalletEntryFile) })
+    cleanupVisor()
+
+    // Valid file loads
+    we := NewWalletEntry()
+    rwe := NewReadableWalletEntry(&we)
+    assert.Nil(t, rwe.Save(testWalletEntryFile))
+    assertFileMode(t, testWalletEntryFile, 0600)
+    assertFileExists(t, testWalletEntryFile)
+    we2 := MustLoadWalletEntry(testWalletEntryFile)
+    assert.Equal(t, we, we2)
+
+    // Invalid entry panics
+    we.Public = coin.PubKey{}
+    rwe = NewReadableWalletEntry(&we)
+    cleanupVisor()
+    assert.Nil(t, rwe.Save(testWalletEntryFile))
+    assertFileMode(t, testWalletEntryFile, 0600)
+    assertFileExists(t, testWalletEntryFile)
+    assert.Panics(t, func() { MustLoadWalletEntry(testWalletEntryFile) })
+}
+
+func TestWalletEntryVerify(t *testing.T) {
+    defer cleanupVisor()
+    // Valid
+    we := NewWalletEntry()
+    assert.Nil(t, we.Verify())
+    // Invalid secret key
+    we2 := NewWalletEntry()
+    we2.Secret = we.Secret
+    err := we2.Verify()
+    assert.NotNil(t, err)
+    assert.Equal(t, err.Error(), "Invalid public key for secret key")
+    // Invalid address
+    we3 := NewWalletEntry()
+    we3.Address = we.Address
+    assert.NotNil(t, we3.Verify())
+}
+
+func TestWalletEntryVerifyPublic(t *testing.T) {
+    defer cleanupVisor()
+    // Valid
+    we := NewWalletEntry()
+    assert.Nil(t, we.VerifyPublic())
+    // Invalid public
+    we2 := NewWalletEntry()
+    we2.Public = coin.PubKey{}
+    assert.NotNil(t, we2.VerifyPublic())
+    // Invalid address
+    we3 := NewWalletEntry()
+    we3.Address = we.Address
+    assert.NotNil(t, we3.VerifyPublic())
+}
+
+func TestNewBalance(t *testing.T) {
+    b := NewBalance(uint64(10), uint64(20))
+    assert.Equal(t, b.Coins, uint64(10))
+    assert.Equal(t, b.Hours, uint64(20))
+}
+
+func TestBalanceAdd(t *testing.T) {
+    b := NewBalance(uint64(10), uint64(20))
+    c := NewBalance(uint64(15), uint64(25))
+    d := b.Add(c)
+    assert.Equal(t, d.Coins, uint64(25))
+    assert.Equal(t, d.Hours, uint64(45))
+    e := c.Add(b)
+    assert.Equal(t, d, e)
+}
+
+func TestBalanceSub(t *testing.T) {
+    b := NewBalance(uint64(10), uint64(20))
+    c := NewBalance(uint64(15), uint64(25))
+    d := c.Sub(b)
+    assert.Equal(t, d.Coins, uint64(5))
+    assert.Equal(t, d.Hours, uint64(5))
+    assert.Panics(t, func() { b.Sub(c) })
+}
+
+func TestBalanceGreaterThan(t *testing.T) {
+    b := NewBalance(uint64(10), uint64(20))
+    c := NewBalance(uint64(15), uint64(25))
+    assert.True(t, c.GreaterThan(b))
+    assert.False(t, b.GreaterThan(c))
+    // Both have a field greater than the other's
+    b.Hours = uint64(35)
+    assert.False(t, c.GreaterThan(b))
+    assert.False(t, b.GreaterThan(c))
+    b.Hours = uint64(20)
+    b.Coins = uint64(25)
+    assert.False(t, c.GreaterThan(b))
+    assert.False(t, b.GreaterThan(c))
+}
+
+func TestBalanceGreaterThanOrEqual(t *testing.T) {
+    // One fully greater than the other
+    b := NewBalance(uint64(10), uint64(20))
+    c := NewBalance(uint64(15), uint64(25))
+    assert.True(t, c.GreaterThanOrEqual(b))
+    assert.False(t, b.GreaterThanOrEqual(c))
+    // Both equal
+    b.Coins = c.Coins
+    b.Hours = c.Hours
+    assert.True(t, c.GreaterThanOrEqual(b))
+    assert.True(t, b.GreaterThanOrEqual(c))
+    // Both have fields greater than or equal to each other
+    b.Hours = uint64(25)
+    assert.False(t, c.GreaterThan(b))
+    assert.False(t, b.GreaterThan(c))
+    b.Hours = uint64(15)
+    b.Coins = uint64(20)
+    assert.False(t, c.GreaterThan(b))
+    assert.False(t, b.GreaterThan(c))
+}
+
+func TestBalanceIsZero(t *testing.T) {
+    b := NewBalance(uint64(0), uint64(0))
+    assert.True(t, b.IsZero())
+    b.Coins = uint64(1)
+    assert.False(t, b.IsZero())
+    b.Hours = uint64(1)
+    assert.False(t, b.IsZero())
+    b.Coins = uint64(0)
+    assert.False(t, b.IsZero())
+}
+
+func TestNewWallet(t *testing.T) {
+    w := NewWallet()
+    assert.NotNil(t, w.Entries)
+    assert.Equal(t, len(w.Entries), 0)
+}
+
+func TestNewWalletFromReadable(t *testing.T) {
+    w := NewWallet()
+    we := NewWalletEntry()
+    w.Entries[we.Address] = we
+    we2 := NewWalletEntry()
+    w.Entries[we2.Address] = we2
+    rw := NewReadableWallet(w)
+    w2 := NewWalletFromReadable(rw)
+    for a, e := range w2.Entries {
+        assert.Equal(t, a, e.Address)
+        assert.Equal(t, e, w.Entries[a])
+    }
+    assert.Equal(t, len(w.Entries), len(w2.Entries))
+    sec := coin.SecKey{}
+    rw.Entries[0].Secret = sec.Hex()
+    assert.Panics(t, func() { NewWalletFromReadable(rw) })
+}
+
+func TestWalletCreateEntry(t *testing.T) {
+    w := NewWallet()
+    we := w.CreateEntry()
+    // Not testing:
+    //  Can't force NewWalletEntry to make an invalid entry
+    //  Can't force NewWalletEntry to generate a duplicate wallet entry,
+    assert.Nil(t, we.Verify())
+    assert.Equal(t, len(w.Entries), 1)
+    assert.Equal(t, w.Entries[we.Address], we)
+}
+
+func TestWalletPopulate(t *testing.T) {
+    w := NewWallet()
+    // Populating should only grow if not enough entries
+    assert.Equal(t, len(w.Entries), 0)
+    w.populate(10)
+    assert.Equal(t, len(w.Entries), 10)
+    w.populate(10)
+    assert.Equal(t, len(w.Entries), 10)
+    w.populate(15)
+    assert.Equal(t, len(w.Entries), 15)
+    w.populate(10)
+    assert.Equal(t, len(w.Entries), 15)
+}
+
+func TestWalletGetAddresses(t *testing.T) {
+    w := NewWallet()
+    w.populate(10)
+    addrs := w.GetAddresses()
+    assert.Equal(t, len(addrs), 10)
+    addrsMap := make(map[coin.Address]byte, len(addrs))
+    for _, a := range addrs {
+        _, ok := w.Entries[a]
+        assert.True(t, ok)
+        addrsMap[a] = byte(1)
+    }
+    // No duplicates
+    assert.Equal(t, len(addrs), len(addrsMap))
+}
+
+func TestWalletGetEntry(t *testing.T) {
+    w := NewWallet()
+    we := w.CreateEntry()
+    we2, ok := w.GetEntry(we.Address)
+    assert.True(t, ok)
+    assert.Equal(t, we, we2)
+    we2, ok = w.GetEntry(coin.Address{})
+    assert.False(t, ok)
+    assert.NotEqual(t, we, we2)
+}
+
+func TestWalletAddEntry(t *testing.T) {
+    w := NewWallet()
+    assert.Equal(t, len(w.Entries), 0)
+    we := w.CreateEntry()
+    assert.Equal(t, len(w.Entries), 1)
+    // No duplicates inserted
+    assert.NotNil(t, w.AddEntry(we))
+    assert.Equal(t, len(w.Entries), 1)
+
+    we2 := NewWalletEntry()
+    assert.Nil(t, w.AddEntry(we2))
+    assert.Equal(t, len(w.Entries), 2)
+
+    assert.Equal(t, w.Entries[we2.Address], we2)
+    assert.Equal(t, w.Entries[we.Address], we)
+}
+
+func TestWalletSaveLoad(t *testing.T) {
+    defer cleanupVisor()
+    w := NewWallet()
+    we := w.CreateEntry()
+    assert.Nil(t, w.Save(testWalletFile))
+    assertFileMode(t, testWalletFile, 0600)
+    w2 := NewWallet()
+    assert.Nil(t, w2.Load(testWalletFile))
+    assert.Equal(t, w, w2)
+    assert.Equal(t, w2.Entries[we.Address], we)
+
+    cleanupVisor()
+    assertFileNotExists(t, testWalletFile)
+    assert.NotNil(t, w2.Load(testWalletFile))
+}
+
+func TestNewReadableWalletEntry(t *testing.T) {
+    defer cleanupVisor()
+    we := NewWalletEntry()
+    rwe := NewReadableWalletEntry(&we)
+    we2 := WalletEntryFromReadable(&rwe)
+    assert.Equal(t, we, we2)
+}
+
+func TestSaveLoadReadableWalletEntry(t *testing.T) {
+    defer cleanupVisor()
+    we := NewWalletEntry()
+    rwe := NewReadableWalletEntry(&we)
+    assert.Nil(t, rwe.Save(testWalletEntryFile))
+    assertFileMode(t, testWalletEntryFile, 0600)
+    rwe2, err := LoadReadableWalletEntry(testWalletEntryFile)
+    assert.Nil(t, err)
+    assert.Equal(t, rwe, rwe2)
+    we2 := WalletEntryFromReadable(&rwe2)
+    assert.Equal(t, we, we2)
+}
+
+func TestReadableWalletEntryFromPubKey(t *testing.T) {
+    defer cleanupVisor()
+    we := NewWalletEntry()
+    rwe := NewReadableWalletEntry(&we)
+    rwe2 := ReadableWalletEntryFromPubkey(rwe.Public)
+    assert.Equal(t, rwe.Address, rwe2.Address)
+    assert.Equal(t, rwe.Public, rwe2.Public)
+    assert.Equal(t, rwe2.Secret, "")
+    we2 := WalletEntryFromReadable(&rwe2)
+    assert.Nil(t, we2.VerifyPublic())
+}
+
+func TestNewReadableWallet(t *testing.T) {
+    defer cleanupVisor()
+    w := NewWallet()
+    w.populate(10)
+    rw := NewReadableWallet(w)
+    assert.Equal(t, len(w.Entries), 10)
+    w2 := NewWalletFromReadable(rw)
+    assert.Equal(t, w, w2)
+}
+
+func TestSaveLoadReadableWallet(t *testing.T) {
+    defer cleanupVisor()
+    w := NewWallet()
+    w.populate(10)
+    rw := NewReadableWallet(w)
+    assert.Nil(t, rw.Save(testWalletFile))
+    assertFileMode(t, testWalletFile, 0600)
+    rw2 := &ReadableWallet{}
+    assert.Nil(t, rw2.Load(testWalletFile))
+    assert.Equal(t, rw, rw2)
+    w2 := NewWalletFromReadable(rw2)
+    assert.Equal(t, w, w2)
+    rw3, err := LoadReadableWallet(testWalletFile)
+    assert.Nil(t, err)
+    assert.Equal(t, rw, rw3)
+    w3 := NewWalletFromReadable(rw3)
+    assert.Equal(t, w, w3)
+}

0c7de42bc9d56128a3294f9294df02530cda1110
aks <aks@skycoin.com>
2014-03-25 03:41:25 -0700

    documentation, fixed bugs in hashchain example

--- chaintest.go
+++ chaintest.go
@@ -3,14 +3,27 @@ package main
 import (
 	"github.com/skycoin/sync/src/hashchain"
 
-	//"fmt"
-	//"log"
+	"fmt"
+	"log"
 )
 
 func main() {
 
 	_, seckey := hashchain.GenerateDeterministicKeyPair([]byte("seed"))
 	bc := hashchain.NewBlockChain(seckey)
+
+	for i := 0; i < 256; i++ {
+
+		//write this data to the block
+		s := fmt.Sprintf("test data: %v", i)
+
+		block := bc.NewBlock(seckey, uint64(time.Now().Unix()), []byte(s))
+
+		err := bc.ApplyBlock(block)
+		if err != nil {
+			log.Panic(err)
+		}
+	}
 
 	_ = bc
 }

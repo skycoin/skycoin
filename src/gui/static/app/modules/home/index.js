cadb0326107abe69007063019ad2bac94bd2d494
Chen Houwu <chenhouwu@gmail.com>
2015-02-27 11:04:52 +0800

    use ui-router instead of ng-route

--- src/gui/static/app/modules/home/index.js
+++ src/gui/static/app/modules/home/index.js
@@ -1,29 +1,32 @@
 'use strict';
 
 require('./controllers');
-require('angular-route');
 require('angular-qrcode');
 
 // Declare app level module which depends on filters, and services
 module.exports = angular.module('skycoin', [
-  'ngRoute',
+  'ui.router',
   'ui.bootstrap',
-  //'skycoin.filters',
-  //'skycoin.services',
-  //'skycoin.directives',
   'skycoin.controllers',
   'monospaced.qrcode'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    template: require('./partial1.html'),
-    controller: 'mainCtrl'
-  });
-  $routeProvider.when('/view2', {
-    template: require('./partial2.html'),
-    controller: 'mainCtrl'
-  });
-  $routeProvider.otherwise({redirectTo: '/'});
-}]).config(['$locationProvider', function($locationProvider){
-  $locationProvider.html5Mode(true).hashPrefix('');
-}]);
+])
+.config([
+  '$stateProvider', '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('view1', {
+      url: '/view1',
+      template: require('./partial1.html'),
+      controller: 'mainCtrl'
+    });
+    $stateProvider.state('view2', {
+      url: '/view2',
+      template: require('./partial2.html'),
+      controller: 'mainCtrl'
+    });
+    $urlRouterProvider.otherwise('/');
+  }
+])
+.config(['$locationProvider', function($locationProvider){
+    $locationProvider.html5Mode(true).hashPrefix('');
+  }
+]);

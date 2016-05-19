d1416947960e484e8d9fdf60841ae6fd84ff6564
Sean Purser-Haskell <sean.purserhaskell@gmail.com>
2016-05-19 21:13:02 +0800

    - TUN Fix

--- cmd/mesh/tun.go
+++ cmd/mesh/tun.go
@@ -1,46 +1,64 @@
 package main
 
 import(
-	"os"
-	"fmt"
-	"reflect"
+    "os"
+    "fmt"
+    "flag"
+    "reflect"
     "github.com/songgao/water"
+    "github.com/satori/go.uuid"
 )
 
-import "github.com/skycoin/skycoin/src/cipher"
+var route_id = flag.String("route", "", "Route ID (UUID) through which to send traffic")
 
 const DATAGRAMSIZE = 1522
 
 func HostTun() {
-    pubkey, _ := cipher.GenerateKeyPair()
+    route_id, err := uuid.FromString(*route_id)
+    if err != nil {
+        panic(err)
+    }
+
     cmd_stdoutQueue := make(chan interface{})
     cmd_stdinQueue := make(chan interface{})
     SpawnNodeSubprocess(*config_path, cmd_stdoutQueue, cmd_stdinQueue)
 
     // Wait for route establishment before setting up TUN
     for {
-        msg_out := <- cmd_stdoutQueue
-        if reflect.TypeOf(msg_out) == reflect.TypeOf(Stdout_EstablishedRoute{}) {
-        	break
+        msg_recvd := <- cmd_stdoutQueue
+        if reflect.TypeOf(msg_recvd) == reflect.TypeOf(Stdout_EstablishedRoute{}) {
+            break
         }
     }
 
     ifce, err1 := water.NewTUN("")
     if err1 != nil {
-    	fmt.Fprintf(os.Stderr, "Error creating tun interface: %v\n", err1)
-    	return
+        fmt.Fprintf(os.Stderr, "Error creating tun interface: %v\n", err1)
+        return
     }
+
+    go func() {
+        for {
+            msg_recvd := <- cmd_stdoutQueue
+
+            if reflect.TypeOf(msg_recvd) == reflect.TypeOf(Stdout_RecvMessage{}) {
+                _, err := ifce.Write(msg_recvd.(Stdout_RecvMessage).Contents)
+                if err == nil {
+                    fmt.Fprintf(os.Stderr, "Error writing to tun interface: %v\n", err)
+                    break
+                }
+            }
+        }
+    }() 
+
     for {
         buffer := make([]byte, DATAGRAMSIZE)
-        nr, err2 := ifce.Read(buffer)
+        _, err2 := ifce.Read(buffer)
         if err2 != nil {
-    		fmt.Fprintf(os.Stderr, "Error reading from tun interface: %v\n", err2)
+            fmt.Fprintf(os.Stderr, "Error reading from tun interface: %v\n", err2)
             break
         }
-        if nr != DATAGRAMSIZE {
-        	continue
-        }
-        cmd_stdinQueue <- SendDatagram{pubkey, buffer}
+        cmd_stdinQueue <- Stdin_SendMessage{route_id, buffer}
     }
 
 }

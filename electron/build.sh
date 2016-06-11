52e7ca0b2d2fdb069e2530edadf335b7019810da
bztk <bztk>
2016-06-11 16:12:42 -0400

    Fix electron for windows/linux

--- electron/build.sh
+++ electron/build.sh
@@ -14,7 +14,28 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "$SCRIPTDIR" >/dev/null
 
 ./gox.sh "$GOX_OSARCH" "$GOX_OUTPUT"
+if [ $? -ne 0 ]; then
+    echo "gox build failed"
+    exit 1
+fi
+
+rm -r .electron_output
 gulp electron
+if [ $? -ne 0 ]; then
+    echo "gulp electron failed"
+    exit 1
+fi
+
 ./package-release.sh
+if [ $? -ne 0 ]; then
+    echo "package-release.sh failed"
+    exit 1
+fi
+
+./compress-release.sh
+if [ $? -ne 0 ]; then
+    echo "compress-release.sh failed"
+    exit 1
+fi
 
 popd >/dev/null
---
+++ electron/compress-release.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# Compresses packaged electron apps after ./package-release.sh is done
+
+. build-conf.sh
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+FINAL_OUTPUT="release"
+
+pushd "$SCRIPTDIR" >/dev/null
+
+# Compress archives
+pushd "$ELN_OUTPUT" >/dev/null
+
+# OS X
+pushd "darwin-x64" >/dev/null
+OSX_ZIP="Skycoin-$SKY_VERSION.zip"
+if [ -e "$OSX_ZIP" ]; then
+    echo "Removing old $OSX_ZIP"
+    rm "$OSX_ZIP"
+fi
+echo "Zipping $OSX_ZIP"
+zip -r --quiet "$OSX_ZIP" "Skycoin.app"
+mv "$OSX_ZIP" "../$OSX_ZIP"
+popd >/dev/null
+
+# Windows
+WIN64_PRE="skycoin-$SKY_VERSION-win-x64"
+WIN64_ZIP="${WIN64_PRE}.zip"
+
+if [ -e "$WIN64_ZIP" ]; then
+    echo "Removing old $WIN64_ZIP"
+    rm "$WIN64_ZIP"
+fi
+echo "Zipping $WIN64_ZIP"
+mv "win32-x64" "$WIN64_PRE"
+zip -r --quiet "$WIN64_ZIP" "$WIN64_PRE"
+mv "$WIN64_PRE" "win32-x64"
+
+# Linux
+LNX64_PRE="skycoin-$SKY_VERSION-linux-x64"
+LNX64_ZIP="${LNX64_PRE}.tar.gz"
+
+if [ -e "$LNX64_ZIP" ]; then
+    echo "Removing old $LNX64_ZIP"
+    rm "$LNX64_ZIP"
+fi
+echo "Zipping $LNX64_ZIP"
+mv "linux-x64" "$LNX64_PRE"
+tar czf "$LNX64_ZIP" "$LNX64_PRE"
+mv "$LNX64_PRE" "linux-x64"
+
+popd >/dev/null
+
+# Move to final release dir
+mkdir -p "$FINAL_OUTPUT"
+mv "$ELN_OUTPUT/"*.zip "$FINAL_OUTPUT"
+mv "$ELN_OUTPUT/"*.tar.gz "$FINAL_OUTPUT"
+
+popd >/dev/null

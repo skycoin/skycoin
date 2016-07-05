84027f423e382ed577e937dadc4ebdf8afd4f318
Sean Purser-Haskell <sean.purserhaskell@gmail.com>
2016-07-05 22:54:18 +0800

    - Message fragmentation

--- src/mesh2/node.go
+++ src/mesh2/node.go
@@ -1,14 +1,14 @@
 package mesh
 
 import(
-/*
+
 "os"
-	"fmt"
-	*/
+"fmt"
+	
 	"time"
     "sync"
     "errors"
-//    "reflect"
+    "reflect"
     "gopkg.in/op/go-logging.v1")
 
 import(
@@ -23,9 +23,20 @@ type NodeConfig struct {
 	ChaCha20Key	    			[32]byte
 	MaximumForwardingDuration	time.Duration
 	RefreshRouteDuration		time.Duration
+	ExpireMessagesInterval      time.Duration
+	ExpireRoutesInterval		time.Duration
+	TimeToAssembleMessage		time.Duration
+	TransportMessageChannelLength int
+	ReliableConfig				reliable.ReliableTransportConfig
 }
 
-type LocalRouteId uuid.UUID
+func min(a, b uint64) uint64 {
+    if a < b {
+        return a
+    }
+    return b
+}
+
 type RouteId uuid.UUID
 type messageId uuid.UUID
 
@@ -40,17 +51,21 @@ type MeshMessage struct {
     Contents      []byte
 }
 
-type LocalRoute struct {
-	connectedPeer cipher.PubKey
-	routeId       RouteId
-}
-
 type Route struct {
 	forwardToPeer 			cipher.PubKey
 	forwardRewriteSendId 	RouteId
 
 	backwardToPeer 			cipher.PubKey
 	backwardRewriteSendId 	RouteId
+}
+
+type MessageUnderAssembly struct {
+	fragments 				map[uint64]UserMessage
+	sendId    				RouteId
+	sendBack                bool
+	count                   uint64
+	dropped                 bool
+	firstFragmentReceived	time.Time
 }
 
 type Node struct {
@@ -61,11 +76,13 @@ type Node struct {
 
     lock *sync.Mutex
     closeGroup *sync.WaitGroup
+	closing 	chan bool
 
     transports 						map[transport.Transport]bool
-    reliableTransports				map[transport.Transport]reliable.ReliableTransport
-    localRoutesById					map[LocalRouteId]LocalRoute
-    routesById                      map[messageId]Route
+    reliableTransports				map[transport.Transport]*reliable.ReliableTransport
+    messagesBeingAssembled          map[messageId]*MessageUnderAssembly
+    routesById                      map[RouteId]Route
+    localRoutesByTerminatingPeer	map[cipher.PubKey]RouteId
 }
 
 // Fields must be public (capital first letter) for encoder
@@ -84,10 +101,12 @@ type UserMessage struct {
 
 type SetRouteMessage struct {
 	MessageBase
-	SetRouteId     RouteId
-	ForwardToPeer  cipher.PubKey
-	BackwardToPeer cipher.PubKey
-    DurationHint   time.Duration
+	SetRouteId     			RouteId
+	ForwardToPeer 			cipher.PubKey
+	ForwardRewriteSendId 	RouteId
+	BackwardToPeer 			cipher.PubKey
+	BackwardRewriteSendId 	RouteId
+    DurationHint   			time.Duration
 }
 
 // Refreshes the route as it passes thru it
@@ -116,20 +135,189 @@ func NewNode(config NodeConfig) (*Node, error) {
 	ret := &Node{
 		config,
 		nil,			// received
-		make(chan []byte),			// received		
+		make(chan []byte, config.TransportMessageChannelLength),			// received		
 		serialize.NewSerializer(),
 		&sync.Mutex{},	// Lock
 		&sync.WaitGroup{},
+		make(chan bool, 10),
 		make(map[transport.Transport]bool),
-		make(map[transport.Transport]reliable.ReliableTransport),
-		make(map[LocalRouteId]LocalRoute),
-		make(map[messageId]Route),
+		make(map[transport.Transport]*reliable.ReliableTransport),
+		make(map[messageId]*MessageUnderAssembly),
+		make(map[RouteId]Route),
+		make(map[cipher.PubKey]RouteId),
 	}
     ret.serializer.RegisterMessageForSerialization(serialize.MessagePrefix{1}, UserMessage{})
     ret.serializer.RegisterMessageForSerialization(serialize.MessagePrefix{2}, SetRouteMessage{})
     ret.serializer.RegisterMessageForSerialization(serialize.MessagePrefix{3}, RefreshRouteMessage{})
     ret.serializer.RegisterMessageForSerialization(serialize.MessagePrefix{4}, DeleteRouteMessage{})
+
+    go ret.processIncomingMessagesLoop()
+    go ret.expireOldRoutesLoop()
+    go ret.expireOldMessagesLoop()
+
 	return ret, nil
+}
+
+// Returns nil if reassembly didn't happen (incomplete message)
+func (self*Node) reassembleUserMessage(msgIn UserMessage) (contents []byte) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+
+	_, routeExists := self.routesById[msgIn.SendId]
+	if !routeExists {
+        fmt.Fprintf(os.Stderr, "Message %v is to unknown route id %v, ignoring\n", msgIn.MessageId, msgIn.SendId)
+		return nil
+	}
+
+	_, assembledExists := self.messagesBeingAssembled[msgIn.MessageId]
+	if !assembledExists {
+		beingAssembled := &MessageUnderAssembly{
+			make(map[uint64]UserMessage),
+			msgIn.SendId,
+			msgIn.SendBack,
+			msgIn.Count, 
+			false, 
+			time.Now().Add(self.config.TimeToAssembleMessage),
+		}
+		self.messagesBeingAssembled[msgIn.MessageId] = beingAssembled
+	}
+
+	beingAssembled, _ := self.messagesBeingAssembled[msgIn.MessageId]
+
+	if beingAssembled.dropped {
+		return nil
+	}
+
+	if beingAssembled.count != msgIn.Count {
+        fmt.Fprintf(os.Stderr, "Fragments of message %v have different total counts!\n", msgIn.MessageId)
+        beingAssembled.dropped = true
+		return nil
+	}
+
+	if beingAssembled.sendId != msgIn.SendId {
+        fmt.Fprintf(os.Stderr, "Fragments of message %v have different send ids!\n", msgIn.SendId)
+        beingAssembled.dropped = true
+		return nil
+	}
+
+	if beingAssembled.sendBack != msgIn.SendBack {
+        fmt.Fprintf(os.Stderr, "Fragments of message %v have different send directions!\n", msgIn.SendId)
+        beingAssembled.dropped = true
+		return nil
+	}
+
+	_, messageExists := beingAssembled.fragments[msgIn.Index]
+	if messageExists {
+        fmt.Fprintf(os.Stderr, "Fragment %v of message %v is duplicated, dropping message\n", msgIn.Index, msgIn.MessageId)
+		return nil
+	}
+
+	beingAssembled.fragments[msgIn.Index] = msgIn
+
+	if (uint64)(len(beingAssembled.fragments)) == beingAssembled.count {
+		delete(self.messagesBeingAssembled, msgIn.MessageId)
+		reassembled := []byte{}
+		for i := (uint64)(0); i < beingAssembled.count; i++ {
+			reassembled = append(reassembled, beingAssembled.fragments[i].Contents...)
+		}
+		return reassembled
+	}
+
+	return nil
+}
+
+func (self*Node) forwardMessage(forwardTo cipher.PubKey, sendBack bool, contents []byte) {
+	panic("TODO: forward message")
+}
+
+func (self*Node) processUserMessage(msgIn UserMessage) {
+	reassembled := self.reassembleUserMessage(msgIn)
+	self.lock.Lock()
+	route, routeExists := self.routesById[msgIn.SendId]
+	if !routeExists {
+		self.lock.Unlock()
+		fmt.Fprintf(os.Stderr, "Dropping message %v to unknown route id %v\n", msgIn.MessageId, msgIn.SendId)
+		return
+	}
+	self.lock.Unlock()
+
+	forwardTo := route.forwardToPeer
+	if msgIn.SendBack {
+		forwardTo = route.backwardToPeer
+	}
+
+	if forwardTo == (cipher.PubKey{}) {
+		self.outputMessagesReceived <- MeshMessage{msgIn.SendId, reassembled}
+	} else {
+		self.forwardMessage(forwardTo, msgIn.SendBack, reassembled)
+	}
+}
+
+func (self*Node) processMessage(serialized []byte) {
+    msg, deserialize_error := self.serializer.UnserializeMessage(serialized)
+    if deserialize_error != nil {
+        fmt.Fprintf(os.Stderr, "Deserialization error %v\n", deserialize_error)
+        return
+    }
+	// Receive or forward. Refragment on forward!
+    msg_type := reflect.TypeOf(msg) 
+
+	if msg_type == reflect.TypeOf(UserMessage{}) {
+		self.processUserMessage(msg.(UserMessage))
+	} else {
+        fmt.Fprintf(os.Stderr, "Unknown message type received\n")
+        return
+	}
+
+	// TODO: Route establishment etc
+}
+
+func (self*Node) expireOldMessages() {
+	fmt.Fprintf(os.Stderr, "TODO: expireOldMessages\n")
+}
+
+func (self*Node) expireOldMessagesLoop() {
+	for len(self.closing) == 0 {
+		select {
+			case <-time.After(self.config.ExpireMessagesInterval): {
+				self.expireOldMessages()
+			}
+			case <-self.closing: {
+				return
+			}
+		}
+	}
+}
+
+func (self*Node) processIncomingMessagesLoop() {
+   	for len(self.closing) == 0 {
+		select {
+			case msg := <- self.transportsMessagesReceived: {
+				self.processMessage(msg)
+			}
+			case <- self.closing: {
+				return
+			}
+		}
+	}
+}
+
+func (self*Node) expireOldRoutes() {
+	fmt.Fprintf(os.Stderr, "TODO: expireOldRoutes\n")
+
+}
+
+func (self*Node) expireOldRoutesLoop() {
+	for len(self.closing) == 0 {
+		select {
+			case <-time.After(self.config.ExpireRoutesInterval): {
+				self.expireOldRoutes()
+			}
+			case <-self.closing: {
+				return
+			}
+		}
+	}
 }
 
 // Waits for transports to close
@@ -141,18 +329,6 @@ func (self*Node) Close() error {
 
 func (self*Node) GetConfig() NodeConfig {
 	return self.config
-}
-
-func (self*Node) safelyGetTransportToPeer(peerKey cipher.PubKey) transport.Transport {
-	self.lock.Lock()
-	defer self.lock.Unlock()
-	for transport, _ := range(self.transports) {
-		// TODO: Choose transport more intelligently
-		if transport.ConnectedToPeer(peerKey) {
-			return transport
-		}
-	}
-	return nil
 }
 
 // Node takes ownership of the transport, and will call Close() when it is closed
@@ -167,6 +343,7 @@ func (self*Node) RemoveTransport(transport transport.Transport) {
 	self.lock.Lock()
 	defer self.lock.Unlock()
 	delete(self.transports, transport)
+	delete(self.reliableTransports, transport)
 }
 
 func (self*Node) GetTransports() ([]transport.Transport) {
@@ -207,43 +384,127 @@ func  (self*Node) SetReceiveChannel(received chan MeshMessage) {
 }
 
 // toPeer must be the public key of a connected peer
-func (self*Node) AddRoute(id LocalRouteId, toPeer cipher.PubKey) error {
-//Direct, go thru transports
+func (self*Node) AddRoute(id RouteId, toPeer cipher.PubKey) error {
+// add locally to routesById for backward termination
 	return errors.New("todo")
 }
 
 // toPeer must be the public key of a peer connected to the current last node in this route
 // Blocks until the operation is completed
-func (self*Node) ExtendRoute(id LocalRouteId, toPeer cipher.PubKey) error {
+func (self*Node) ExtendRoute(id RouteId, toPeer cipher.PubKey) error {
 // blocks waiting
 	return errors.New("todo")
 }
 
-func (self*Node) RemoveRoute(id LocalRouteId) (error) {
+func (self*Node) RemoveRoute(id RouteId) (error) {
 	return errors.New("todo")
 }
 
-func (self*Node) getMaximumContentLength(transport transport.Transport) uint64 {
-	return 0
+func (self*Node) getMaximumContentLength(toPeer cipher.PubKey, transport transport.Transport) uint64 {	
+	transportSize := transport.GetMaximumMessageSizeToPeer(toPeer)
+	empty := UserMessage{}
+	emptySerialized := self.serializer.SerializeMessage(empty)
+	if (uint)(len(emptySerialized)) >= transportSize {
+		return 0
+	}
+	return (uint64)(len(emptySerialized)) - (uint64)(transportSize)
 }
 
-// Chooses a route automatically. Sends directly without a route if connected to that peer. 
-func (self*Node) SendMessageToPeer(toPeer cipher.PubKey, contents []byte, reliably bool, timeout time.Duration) (err error, routeId RouteId) {
-//fragmentMessage()
-	return nil, NilRouteId
+func (self*Node) fragmentMessage(fullContents []byte, toPeer cipher.PubKey, transport transport.Transport, base MessageBase) []UserMessage {
+	ret_noCount := make([]UserMessage, 0)
+	maxContentLength := self.getMaximumContentLength(toPeer, transport)
+	remainingBytes := fullContents[:]
+	for len(remainingBytes) > 0 {
+		nBytesThisMessage := min(maxContentLength, (uint64)(len(remainingBytes)))
+		bytesThisMessage := remainingBytes[:nBytesThisMessage]
+		remainingBytes = remainingBytes[nBytesThisMessage:]
+		message := UserMessage {
+			base,
+			(messageId)(uuid.NewV4()),
+			(uint64)(len(ret_noCount)),
+			0,
+			bytesThisMessage,
+		}
+		ret_noCount = append(ret_noCount, message)
+	}
+	ret := make([]UserMessage, 0)
+	for _, message := range(ret_noCount) {
+		message.Count = (uint64)(len(ret_noCount))
+		ret = append(ret, message)
+	}
+	return ret
+}
+
+func (self*Node) unsafelyGetTransportToPeer(peerKey cipher.PubKey) transport.Transport {
+	for transport, _ := range(self.transports) {
+		// TODO: Choose transport more intelligently
+		if transport.ConnectedToPeer(peerKey) {
+			return transport
+		}
+	}
+	return nil
+}
+
+func (self*Node) findRouteToPeer(toPeer cipher.PubKey, reliably bool) (directPeer cipher.PubKey, routeId RouteId, transport transport.Transport, err error) {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+	localRouteIdFound, localRouteExists := self.localRoutesByTerminatingPeer[toPeer]
+	if localRouteExists {
+		route, routeStructExists := self.routesById[localRouteIdFound]
+		if !routeStructExists {
+			panic("Local route struct does not exists")
+		}
+		directPeer = route.forwardToPeer
+		routeId = route.forwardRewriteSendId
+	} else {
+		return cipher.PubKey{}, NilRouteId, nil, errors.New(fmt.Sprintf("No route to peer: %v", toPeer))
+	}
+	transport = self.unsafelyGetTransportToPeer(directPeer)
+	if transport == nil {
+		return cipher.PubKey{}, NilRouteId, nil, 
+			    errors.New(fmt.Sprintf("No route or transport to peer %v\n", toPeer))
+	}
+	if reliably {
+		reliableTransport, reliableExists := self.reliableTransports[transport]
+		if !reliableExists {
+			panic("Reliable transport doesn't exist")
+		}
+		transport = reliableTransport
+	}
+	return
+}
+
+// Chooses a route automatically. Sends directly without a route if connected to that peer
+func (self*Node) SendMessageToPeer(toPeer cipher.PubKey, contents []byte, reliably bool) (err error, routeId RouteId) {
+	directPeer, routeId, transport, error := self.findRouteToPeer(toPeer, reliably)
+	if error != nil {
+		return error, NilRouteId
+	}
+	base := MessageBase{
+		routeId,
+		false,		// Sending forward
+	}
+	messages := self.fragmentMessage(contents, directPeer, transport, base)
+	for _, message := range(messages) {
+		serialized := self.serializer.SerializeMessage(message)
+		send_error := transport.SendMessage(directPeer, serialized)
+		if send_error != nil {
+			return send_error, NilRouteId
+		}
+	}
+	return nil, routeId
 }
 
 // Blocks until message is confirmed received if reliably is true
-func (self*Node) SendMessageThruRoute(route_id RouteId, contents []byte, reliably bool, deadline time.Time) (error) {
+func (self*Node) SendMessageThruRoute(route_id RouteId, contents []byte, reliably bool,) (error) {
 //fragmentMessage()
 	return errors.New("todo")
 }
 
 // Blocks until message is confirmed received if reliably is true
-func (self*Node) SendMessageBackThruRoute(replyRoute RouteId, contents []byte, reliably bool, deadline time.Time) (error) {
+func (self*Node) SendMessageBackThruRoute(route_id RouteId, contents []byte, reliably bool) (error) {
 //fragmentMessage()
 	return errors.New("todo")
 }
-
 
 
--- src/mesh2/node_test.go
+++ src/mesh2/node_test.go
@@ -1,11 +1,16 @@
 package mesh
 
 import(
+"os"
+"fmt"
+
 	"time"
 	"testing"
 	"sort")
 
 import(
+	"github.com/skycoin/skycoin/src/mesh2/reliable"
+	"github.com/skycoin/skycoin/src/mesh2/transport"
     "github.com/skycoin/skycoin/src/cipher"
 	"github.com/stretchr/testify/assert")
 
@@ -16,34 +21,58 @@ func sortPubKeys(pubKeys []cipher.PubKey) ([]cipher.PubKey) {
 }
 
 func TestManageTransports(t *testing.T) {
-	transport_a := NewStubTransport(t, 512)
-	transport_b := NewStubTransport(t, 512)
+	transport_a := transport.NewStubTransport(t, 512)
+	transport_b := transport.NewStubTransport(t, 512)
+	test_key_a := cipher.NewPubKey([]byte{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0})
 	node, error := NewNode(NodeConfig{
-			cipher.NewPubKey([]byte{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}),
+			test_key_a,
 			[32]byte{0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, },
 			time.Minute,
 			10 * time.Second,
+			time.Second,
+			time.Second,
+			2*time.Second,
+			10, // Transport message channel length
+			reliable.ReliableTransportConfig{
+				test_key_a,
+				10,
+				6 * time.Second,
+				6 * time.Second,
+				time.Second,
+			},
 		})
 	assert.Nil(t, error)
-	assert.Equal(t, []Transport{}, node.GetTransports())
+	assert.Equal(t, []transport.Transport{}, node.GetTransports())
 	node.AddTransport(transport_a)
-	assert.Equal(t, []Transport{transport_a}, node.GetTransports())
+	assert.Equal(t, []transport.Transport{transport_a}, node.GetTransports())
 	node.AddTransport(transport_b)
-	assert.Equal(t, []Transport{transport_a, transport_b}, node.GetTransports())
+	assert.Equal(t, []transport.Transport{transport_a, transport_b}, node.GetTransports())
 	node.RemoveTransport(transport_a)
-	assert.Equal(t, []Transport{transport_b}, node.GetTransports())
+	assert.Equal(t, []transport.Transport{transport_b}, node.GetTransports())
 	node.RemoveTransport(transport_b)
-	assert.Equal(t, []Transport{}, node.GetTransports())
+	assert.Equal(t, []transport.Transport{}, node.GetTransports())
 }
 
 func TestConnectedPeers(t *testing.T) {
-	transport_a := NewStubTransport(t, 512)
-	transport_b := NewStubTransport(t, 512)
+	transport_a := transport.NewStubTransport(t, 512)
+	transport_b := transport.NewStubTransport(t, 512)
+	test_key_a := cipher.NewPubKey([]byte{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0})
 	node, error := NewNode(NodeConfig{
-			cipher.NewPubKey([]byte{3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}),
+			test_key_a,
 			[32]byte{0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, },
 			time.Minute,
 			10 * time.Second,
+			time.Second,
+			time.Second,
+			2*time.Second,
+			10, // Transport message channel length
+			reliable.ReliableTransportConfig{
+				test_key_a,
+				10,
+				6 * time.Second,
+				6 * time.Second,
+				time.Second,
+			},
 		})
 	assert.Nil(t, error)
 	peer_a := cipher.NewPubKey([]byte{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0})
@@ -82,14 +111,25 @@ func TestConnectedPeers(t *testing.T) {
 	assert.False(t, node.ConnectedToPeer(peer_c))
 }
 
-func SetupNode(t *testing.T) (*Node, *StubTransport) {
-	transport := NewStubTransport(t, 512)
+func SetupNode(t *testing.T) (*Node, *transport.StubTransport) {
+	transport := transport.NewStubTransport(t, 512)
 	newPubKey, _ := cipher.GenerateKeyPair()
 	node, error := NewNode(NodeConfig{
 			newPubKey,
 			[32]byte{0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, },
 			time.Minute,
 			10 * time.Second,
+			time.Second,
+			time.Second,
+			2*time.Second,
+			10, // Transport message channel length
+			reliable.ReliableTransportConfig{
+				newPubKey,
+				10,
+				6 * time.Second,
+				6 * time.Second,
+				time.Second,
+			},
 		})
 	assert.Nil(t, error)
 	node.AddTransport(transport)
@@ -100,12 +140,11 @@ func SetupNode(t *testing.T) (*Node, *StubTransport) {
 // All nodes receive all other nodes' messages, but stub transport filters
 func SetupNodes(n uint, connections [][]int, t *testing.T) (nodes []*Node, to_close chan []byte) {
 	nodes = make([]*Node, n)
-	transports := make([]*StubTransport, n)
+	transports := make([]*transport.StubTransport, n)
 	to_close = make(chan []byte, 20)
 	sentMessages := make(chan []byte, 20)
 	for i := (uint)(0); i < n; i++ {
 		nodes[i], transports[i] = SetupNode(t)
-		nodes[i].AddTransport(transports[i])
 	}
 	for i := (uint)(0); i < n; i++ {
 		transport_from := transports[i]
@@ -119,7 +158,12 @@ func SetupNodes(n uint, connections [][]int, t *testing.T) (nodes []*Node, to_cl
 	return nodes, sentMessages
 }
 
-func sendDirect(t *testing.T, reliable bool) {
+
+func TestEstablishRoute(t *testing.T) {
+	// todo
+}
+
+func sendDirect(t *testing.T, reliable bool, contents []byte) {
 	connections  := [][]int{
 		[]int{1,1,},
 		[]int{1,1,},
@@ -132,13 +176,13 @@ func sendDirect(t *testing.T, reliable bool) {
 		}
 	}()
 
-	contents := []byte{4,66,7,44,33}
+// establish route
 
 	received := make(chan MeshMessage, 10)
 	nodes[1].SetReceiveChannel(received)
 
 	test_key_b := nodes[1].GetConfig().PubKey
-	send_err, route_id := nodes[0].SendMessageToPeer(test_key_b, contents, reliable, time.Second)
+	send_err, route_id := nodes[0].SendMessageToPeer(test_key_b, contents, reliable)
 	assert.Nil(t, send_err)
 	assert.Zero(t, route_id)
 
@@ -152,8 +196,23 @@ func sendDirect(t *testing.T, reliable bool) {
 	}
 }
 
-func TestEstablishRoute(t *testing.T) {
-	
+func TestSendDirectUnreliably(t *testing.T) {
+	contents := []byte{4,66,7,44,33}
+	sendDirect(t, false, contents)
+}
+
+func TestSendDirectReliably(t *testing.T) {
+	contents := []byte{4,66,7,44,33}
+	sendDirect(t, true, contents)
+}
+
+func TestSendLongMessage(t *testing.T) {
+	contents := []byte{}
+	for i := 0; i < 25670 ; i++ {
+		contents = append(contents, (byte)(i))
+	}
+fmt.Fprintf(os.Stderr, "contents %v\n", contents)
+	sendDirect(t, false, contents)
 }
 
 // Route expiry test

a605924fe63fe5fa2ad831beb4264df7b33dbcb6
skycoin <skycoin>
2016-07-06 19:10:26 +0800

    node

--- src/gui/static/dist/app/app.loadWallet.ts
+++ src/gui/static/dist/app/app.loadWallet.ts
@@ -1,5 +1,10 @@
+
+
+//import {Component, OnInit, ViewChild} from 'app/angular2/core';
+//import {ROUTER_DIRECTIVES, OnActivate} from 'app/angular2/router';
 import {Component, OnInit, ViewChild} from 'angular2/core';
 import {ROUTER_DIRECTIVES, OnActivate} from 'angular2/router';
+
 import {Http, HTTP_BINDINGS, Response} from 'angular2/http';
 import {HTTP_PROVIDERS, Headers} from 'angular2/http';
 import {Observable} from 'rxjs/Observable';

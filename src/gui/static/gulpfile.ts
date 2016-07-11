1ab682e13a03a5cd87388171801fabbc7e0181e0
skycoin <skycoin>
2016-07-11 15:08:14 +0800

    gui update

--- src/gui/static/gulpfile.ts
+++ src/gui/static/gulpfile.ts
@@ -5,6 +5,9 @@ const del = require("del");
 const tsc = require("gulp-typescript");
 const sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+const tsProject = tsc.createProject("tsconfig.json");
+var replace = require('gulp-replace');
 
 /**
  * Remove build directory.
@@ -17,9 +20,9 @@ gulp.task('clean', (cb) => {
  * Compile TypeScript sources and create sourcemaps in build directory.
  */
 gulp.task("compile", () => {
-    var tsResult = gulp.src([
+    /*var tsResult = gulp.src([
         'node_modules/angular2/typings/browser.d.ts',
-        'src/**/*.ts'
+        'src/!**!/!*.ts'
     ]).pipe(sourcemaps.init()) // This means sourcemaps will be generated
         .pipe(tsc({
             sortOutput: true,
@@ -28,7 +31,17 @@ gulp.task("compile", () => {
 
     return tsResult.js
         .pipe(concat('script.js')) // You can use other plugins that also support gulp-sourcemaps
-        .pipe(gulp.dest('dist/app'));
+        //.pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/app'));*/
+
+    let tsResult = gulp.src("src/**/*.ts")
+        .pipe(sourcemaps.init())
+        .pipe(tsc(tsProject));
+    return tsResult.js
+        .pipe(replace('.ts', '.js'))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("dist"));
 });
 
 /**

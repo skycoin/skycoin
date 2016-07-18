b7de95a508a80e466864d2ada989ed7b669ad843
Li Xing <polarislee1984@outlook.com>
2016-07-18 16:44:43 +0800

    updated by polarislee
    Usage
    
    1. remove node_modules directory
    2. npm install
    3. gulp clean
    4. gulp build

--- src/gui/static/gulpfile.js
+++ src/gui/static/gulpfile.js
@@ -20,21 +20,6 @@ gulp.task('clean', (cb) => {
  * Compile TypeScript sources and create sourcemaps in build directory.
  */
 gulp.task("compile", () => {
-    /*var tsResult = gulp.src([
-        'node_modules/angular2/typings/browser.d.ts',
-        'src/!**!/!*.ts'
-    ]).pipe(sourcemaps.init()) // This means sourcemaps will be generated
-        .pipe(tsc({
-            sortOutput: true,
-            experimentalDecorators:true
-        }));
-
-    return tsResult.js
-        .pipe(concat('script.js')) // You can use other plugins that also support gulp-sourcemaps
-        //.pipe(uglify())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist/app'));*/
-
     let tsResult = gulp.src("src/**/*.ts")
         .pipe(sourcemaps.init())
         .pipe(tsc(tsProject));

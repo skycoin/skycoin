6a655d2685e58f381369c46b0c847a623932ae0a
iketheadore <heaven.mao@gmail.com>
2016-08-15 22:31:25 +0800

    update chacha20.go in cipher

--- src/cipher/chacha20.go
+++ src/cipher/chacha20.go
@@ -2,49 +2,16 @@ package cipher
 
 import (
 	"errors"
-	chacha20_1 "github.com/codahale/chacha20"
-	chacha20_0 "github.com/tang0th/go-chacha20"
-	"log"
+
+	"github.com/skycoin/skycoin/src/cipher/chacha20"
 )
 
-//32 byte input key
-func ChaCha20Encrypt(in []byte, Key []byte) []byte {
-
-	if len(Key) != 32 {
-		log.Panic("Key is 32 bytes")
+func Chacha20Encrypt(data []byte, key []byte, nonce []byte) (d []byte, err error) {
+	if len(key) != 32 {
+		return []byte{}, errors.New("Key is 32 bytes")
 	}
-	out := make([]byte, len(in))
-	//TODO, using fixed nonce
-	chacha20_0.XORKeyStream(out, in, []byte("nonce123"), Key[:])
-	return out
-}
-
-//32 byte input key
-func ChaCha20Decrypt(in []byte, Key []byte) []byte {
-
-	if len(Key) != 32 {
-		log.Panic("Key is 32 bytes")
-	}
-	out := make([]byte, len(in))
-	//TODO, using fixed nonce
-	chacha20_0.XORKeyStream(out, in, []byte("nonce123"), Key)
-	return out
-}
-
-/*
-Duplicate
-*/
-
-func Chacha20Encrypt(data []byte, pubkey PubKey, seckey SecKey, nonce []byte) (d []byte, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = errors.New("encrypt faild")
-		}
-	}()
-
-	key := ECDH(pubkey, seckey)
 	e := make([]byte, len(data))
-	c, err := chacha20_1.New(key, nonce)
+	c, err := chacha20.New(key, nonce)
 	if err != nil {
 		return []byte{}, err
 	}
@@ -52,16 +19,12 @@ func Chacha20Encrypt(data []byte, pubkey PubKey, seckey SecKey, nonce []byte) (d
 	return e, nil
 }
 
-func Chacha20Decrypt(data []byte, pubkey PubKey, seckey SecKey, nonce []byte) (d []byte, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = errors.New("decrypt faild")
-		}
-	}()
-
-	key := ECDH(pubkey, seckey)
+func Chacha20Decrypt(data []byte, key []byte, nonce []byte) (d []byte, err error) {
+	if len(key) != 32 {
+		return []byte{}, errors.New("Key is 32 bytes")
+	}
 	e := make([]byte, len(data))
-	c, err := chacha20_1.New(key, nonce)
+	c, err := chacha20.New(key, nonce)
 	if err != nil {
 		return []byte{}, err
 	}

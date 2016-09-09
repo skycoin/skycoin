843bde911543f0c21033dd6526cce799f6efeeab
iketheadore <heaven.mao@gmail.com>
2016-09-09 11:33:13 +0800

    add bucket struct for maintaining bolt bucket

--- src/visor/blockdb/blockdb.go
+++ src/visor/blockdb/blockdb.go
@@ -15,21 +15,17 @@ import (
 	"github.com/skycoin/skycoin/src/util"
 )
 
-/*
-	Create 3 buckets. One for
-	- blocks
-	- block signatures
-	- unspent output set
-*/
-
 var db *bolt.DB
 var dbDir string //use src/util to resolve
 
-var bucketBlocks = []byte("blocks")
-var bucketBlockSigs = []byte("blocksigs")
-var bucketUtxos = []byte("utxos")
+// Create 3 buckets. One for
+// - blocks
+// - block signatures
+// - unspent output set
 
-// var blocksBucket
+var bucketBlocks *bucket
+var bucketBlockSigs *bucket
+var bucketUtxos *bucket
 
 // Start the blockdb.
 func Start() {
@@ -43,17 +39,20 @@ func Start() {
 	}
 
 	// create blocks bucket.
-	if err := createBucket(bucketBlocks); err != nil {
+	bucketBlocks, err = newBucket([]byte("blocks"))
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	// create block signature bucket.
-	if err := createBucket(bucketBlockSigs); err != nil {
+	bucketBlockSigs, err = newBucket([]byte("blocksigs"))
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	// create unspent output bucket.
-	if err := createBucket(bucketUtxos); err != nil {
+	bucketUtxos, err = newBucket([]byte("utxos"))
+	if err != nil {
 		log.Fatal(err)
 	}
 }
@@ -63,41 +62,49 @@ func Stop() {
 	db.Close()
 }
 
-func createBucket(name []byte) error {
-	return db.Update(func(tx *bolt.Tx) error {
+type bucket struct {
+	Name []byte
+}
+
+func newBucket(name []byte) (*bucket, error) {
+	err := db.Update(func(tx *bolt.Tx) error {
 		if _, err := tx.CreateBucketIfNotExists(name); err != nil {
 			return err
 		}
 		return nil
 	})
+	if err != nil {
+		return nil, err
+	}
+	return &bucket{name}, nil
 }
 
-func bktSetValue(name []byte, key []byte, value []byte) error {
-	return db.Update(func(tx *bolt.Tx) error {
-		return tx.Bucket(name).Put(key, value)
-	})
-}
-
-func bktGetValue(name []byte, key []byte) []byte {
+func (b bucket) Get(key []byte) []byte {
 	var value []byte
 	db.View(func(tx *bolt.Tx) error {
-		value = tx.Bucket(name).Get(key)
+		value = tx.Bucket(b.Name).Get(key)
 		return nil
 	})
 	return value
 }
 
-// SaveBlock save coin block.
-func SaveBlock(block coin.Block) error {
+func (b bucket) Set(key []byte, value []byte) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		return tx.Bucket(b.Name).Put(key, value)
+	})
+}
+
+// SetBlock save coin block.
+func SetBlock(block coin.Block) error {
 	//write to DB
 	hash := block.HashHeader()      //the key
 	bin := encoder.Serialize(block) //the value
-	return bktSetValue(bucketBlocks, hash[:], bin)
+	return bucketBlocks.Set(hash[:], bin)
 }
 
 // GetBlock by block hash.
 func GetBlock(hash cipher.SHA256) *coin.Block {
-	bin := bktGetValue(bucketBlocks, hash[:])
+	bin := bucketBlocks.Get(hash[:])
 	block := coin.Block{}
 	if err := encoder.DeserializeRaw(bin, &block); err != nil {
 		return nil
@@ -123,7 +130,7 @@ func SetBlockSignature(hash cipher.SHA256, preHash cipher.SHA256, Sig cipher.Sig
 	}
 
 	bin := encoder.Serialize(bs) //value
-	if err := bktSetValue(bucketBlockSigs, hash[:], bin); err != nil {
+	if err := bucketBlockSigs.Set(hash[:], bin); err != nil {
 		return nil, err
 	}
 	return &bs, nil
@@ -133,7 +140,7 @@ func SetBlockSignature(hash cipher.SHA256, preHash cipher.SHA256, Sig cipher.Sig
 func GetBlockSignature(hash cipher.SHA256) *BlockSignature {
 	var bs = BlockSignature{}
 	//get object
-	bin := bktGetValue(bucketBlockSigs, hash[:])
+	bin := bucketBlockSigs.Get(hash[:])
 	if err := encoder.DeserializeRaw(bin, &bs); err != nil {
 		return nil
 	}

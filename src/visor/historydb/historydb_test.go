a0088d8e72a8ed076a97205eb33552dcbb83a8b8
iketheadore <heaven.mao@gmail.com>
2016-09-23 11:18:45 +0800

    Update historydb test

--- src/visor/historydb/historydb_test.go
+++ src/visor/historydb/historydb_test.go
@@ -1,4 +1,4 @@
-package historydb
+package historydb_test
 
 import (
 	"fmt"
@@ -8,15 +8,23 @@ import (
 	"testing"
 	"time"
 
+	"github.com/boltdb/bolt"
+	"github.com/skycoin/skycoin/src/aether/encoder"
 	"github.com/skycoin/skycoin/src/cipher"
 	"github.com/skycoin/skycoin/src/coin"
 	"github.com/skycoin/skycoin/src/util"
+	"github.com/skycoin/skycoin/src/visor/historydb"
+	"github.com/stretchr/testify/assert"
 )
 
 var (
 	genPublic, genSecret = cipher.GenerateKeyPair()
 	genAddress           = cipher.AddressFromPubKey(genPublic)
 	testMaxSize          = 1024 * 1024
+	transactionBkt       = []byte("transactions")
+	outputBkt            = []byte("outputs")
+	addressInBkt         = []byte("address_in")
+	addressOutBkt        = []byte("address_out")
 )
 
 var _genTime uint64 = 1000
@@ -31,8 +39,8 @@ var _ = func() int64 {
 	return t
 }()
 
-func setup(t *testing.T) (*HistoryDB, func(), error) {
-	dbName := fmt.Sprintf("%d.db", rand.Int31n(10000))
+func setup(t *testing.T) (*bolt.DB, func(), error) {
+	dbName := fmt.Sprintf("%ddb", rand.Int31n(10000))
 	teardown := func() {}
 	tmpDir := filepath.Join(os.TempDir(), dbName)
 	if err := os.MkdirAll(tmpDir, 0777); err != nil {
@@ -40,16 +48,18 @@ func setup(t *testing.T) (*HistoryDB, func(), error) {
 	}
 
 	util.DataDir = tmpDir
-	hdb := &HistoryDB{}
-	hdb.Start()
+	db, err := historydb.NewDB()
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	teardown = func() {
-		hdb.Stop()
+		db.Close()
 		if err := os.RemoveAll(tmpDir); err != nil {
 			panic(err)
 		}
 	}
-	return nil, teardown, nil
+	return db, teardown, nil
 }
 
 type FakeTree struct {
@@ -81,8 +91,8 @@ func (ft *FakeTree) GetBlockInDepth(dep uint64, filter func(hps []coin.HashPair)
 	return ft.blocks[int(dep)]
 }
 
-func TestProcessBlock(t *testing.T) {
-	hisDB, teardown, err := setup(t)
+func TestProcessGenesisBlock(t *testing.T) {
+	db, teardown, err := setup(t)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -92,5 +102,32 @@ func TestProcessBlock(t *testing.T) {
 	bc := coin.NewBlockchain(&ft, nil)
 	gb := bc.CreateGenesisBlock(genAddress, _genCoins, _genTime)
 
-	hisDB.ProcessBlock(&gb)
+	hisDB, err := historydb.New(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := hisDB.ProcessBlock(&gb); err != nil {
+		t.Fatal(err)
+	}
+
+	// check transactions bucket.
+	var tx historydb.Transaction
+	txHash := gb.Body.Transactions[0].Hash()
+	if err := getBucketValue(db, transactionBkt, txHash[:], &tx); err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, tx.Transaction, gb)
+}
+
+func getBucketValue(db *bolt.DB, name []byte, key []byte, value interface{}) error {
+	return db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(name)
+		bin := b.Get(key)
+		if bin == nil {
+			value = nil
+			return nil
+		}
+		return encoder.DeserializeRaw(bin, value)
+	})
 }

7c8bd861d44b3cc7d67ca8c1c1dfa69edd20f53f
iketheadore <heaven.mao@gmail.com>
2016-09-24 13:21:27 +0800

    Store block in ProcessBlock

--- src/visor/historydb/historydb.go
+++ src/visor/historydb/historydb.go
@@ -86,6 +86,9 @@ func (hd *HistoryDB) GetUxout(uxID cipher.SHA256) (*UxOut, error) {
 // ProcessBlock will index the transaction, outputs,etc.
 func (hd *HistoryDB) ProcessBlock(b *coin.Block) error {
 	// store the block
+	if err := hd.blocks.Add(b); err != nil {
+		return err
+	}
 
 	// index the transactions
 	for _, t := range b.Body.Transactions {

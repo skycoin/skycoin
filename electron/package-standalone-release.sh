43054c2c1786cf31e64d735100c89cc5d0887d76
bztk <bztk>
2016-09-27 11:40:37 -0400

    Make scripts more robust and support single arch

--- electron/package-standalone-release.sh
+++ electron/package-standalone-release.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e -o pipefail
 
 # Builds the release without electron
 
@@ -16,33 +17,48 @@ OSX64_SRC="${OSX64}/src"
 WIN64_SRC="${WIN64}/src"
 LNX64_SRC="${LNX64}/src"
 
-echo "Creating target directories in $STL_OUTPUT"
+DESTSRCS=()
 
-# create target directories
-for var in "${OSX64}" "${WIN64}" "${LNX64}"; do
-    if [ -e "${var}" ]; then
-        rm -r "${var}"
+function copy_if_exists {
+    if [ -z "$1" -o -z "$2" -o -z "$3" -o -z "$4" ]; then
+        echo "copy_if_exists requires 4 args"
+        exit 1
     fi
-    mkdir -p "${var}"
-done
+
+    BIN="${GOX_OUTPUT}/${1}"
+    DESTDIR="$2"
+    DESTBIN="${DESTDIR}/${3}"
+    DESTSRC="$4"
+
+    if [ -f "$BIN" ]; then
+        if [ -e "$DESTDIR" ]; then
+            rm -r "$DESTDIR"
+        fi
+        mkdir -p "$DESTDIR"
+
+        # Copy binary to electron app
+        echo "Copying $BIN to $DESTBIN"
+        cp "$BIN" "$DESTBIN"
+
+        # Copy static resources to electron app
+        echo "Copying $GUI_DIST_DIR to $DESTDIR"
+        cp -R "$GUI_DIST_DIR" "$DESTDIR"
+
+        echo "Adding $DESTSRC to package-source.sh list"
+        DESTSRCS+=("$DESTSRC")
+    fi
+}
 
 echo "Copying skycoin binaries"
 
 # copy binaries
-cp "${GOX_OUTPUT}/skycoin_darwin_amd64" "${OSX64}/skycoin"
-cp "${GOX_OUTPUT}/skycoin_windows_amd64.exe" "${WIN64}/skycoin.exe"
-cp "${GOX_OUTPUT}/skycoin_linux_amd64" "${LNX64}/skycoin"
-
-echo "Copying static resources"
-
-# copy static resources
-for var in "${OSX64}" "${WIN64}" "${LNX64}"; do
-    cp -R "${GUI_DIST_DIR}" "${var}"
-done
+copy_if_exists "skycoin_darwin_amd64" "$OSX64" "skycoin" "$OSX64_SRC"
+copy_if_exists "skycoin_windows_amd64.exe" "$WIN64" "skycoin.exe" "$WIN64_SRC"
+copy_if_exists "skycoin_linux_amd64" "$LNX64" "skycoin" "$LNX64_SRC"
 
 # Copy the source for reference
 # tar it with filters, move it, then untar in order to do this
 echo "Copying source snapshot"
 
-./package-source.sh "${OSX64_SRC}" "${WIN64_SRC}" "${LNX64_SRC}"
+./package-source.sh "${DESTSRCS[@]}"
 

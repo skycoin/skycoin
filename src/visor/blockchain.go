6fbdf137a44f30661c2f82ae8457898d5502bf75
iketheadore <heaven.mao@gmail.com>
2016-09-27 16:15:01 +0800

    Move verifyTransactionInputs function to coin's transaction.

--- src/visor/blockchain.go
+++ src/visor/blockchain.go
@@ -293,7 +293,7 @@ func (bc Blockchain) VerifyTransaction(tx coin.Transaction) error {
 	}
 	// Checks whether ux inputs exist,
 	// Check that signatures are allowed to spend inputs
-	if err := verifyTransactionInputs(tx, uxIn); err != nil {
+	if err := tx.VerifyInput(uxIn); err != nil {
 		return err
 	}
 
@@ -314,7 +314,7 @@ func (bc Blockchain) VerifyTransaction(tx coin.Transaction) error {
 	}
 
 	// Check that no coins are lost, and sufficient coins and hours are spent
-	err = verifyTransactionSpending(bc.Time(), tx, uxIn, uxOut)
+	err = coin.VerifyTransactionSpending(bc.Time(), uxIn, uxOut)
 	if err != nil {
 		return err
 	}
@@ -560,71 +560,6 @@ func (bc Blockchain) VerifySigs(pubKey cipher.PubKey, sigs *blockdb.BlockSigs) e
 		}
 	}
 
-	return nil
-}
-
-/* Unassigned operators */
-
-// Validates the inputs to a transaction by checking signatures. Assumes txn
-// has valid number of signatures for inputs.
-func verifyTransactionInputs(tx coin.Transaction, uxIn coin.UxArray) error {
-	if DebugLevel2 {
-		if len(tx.In) != len(tx.Sigs) || len(tx.In) != len(uxIn) {
-			log.Panic("tx.In != tx.Sigs != uxIn")
-		}
-		if tx.InnerHash != tx.HashInner() {
-			log.Panic("Invalid Tx Header Hash")
-		}
-	}
-
-	// Check signatures against unspent address
-	for i := range tx.In {
-		hash := cipher.AddSHA256(tx.InnerHash, tx.In[i]) //use inner hash, not outer hash
-		err := cipher.ChkSig(uxIn[i].Body.Address, hash, tx.Sigs[i])
-		if err != nil {
-			return errors.New("Signature not valid for output being spent")
-		}
-	}
-	if DebugLevel2 {
-		// Check that hashes match.
-		// This would imply a bug with UnspentPool.GetMultiple
-		if len(tx.In) != len(uxIn) {
-			log.Panic("tx.In does not match uxIn")
-		}
-		for i := range tx.In {
-			if tx.In[i] != uxIn[i].Hash() {
-				log.Panic("impossible error: Ux hash mismatch")
-			}
-		}
-	}
-	return nil
-}
-
-// Checks that coins will not be destroyed and that enough coins are hours
-// are being spent for the outputs
-func verifyTransactionSpending(headTime uint64, tx coin.Transaction,
-	uxIn coin.UxArray, uxOut coin.UxArray) error {
-	coinsIn := uint64(0)
-	hoursIn := uint64(0)
-	for i := range uxIn {
-		coinsIn += uxIn[i].Body.Coins
-		hoursIn += uxIn[i].CoinHours(headTime)
-	}
-	coinsOut := uint64(0)
-	hoursOut := uint64(0)
-	for i := range uxOut {
-		coinsOut += uxOut[i].Body.Coins
-		hoursOut += uxOut[i].Body.Hours
-	}
-	if coinsIn < coinsOut {
-		return errors.New("Insufficient coins")
-	}
-	if coinsIn > coinsOut {
-		return errors.New("Transactions may not create or destroy coins")
-	}
-	if hoursIn < hoursOut {
-		return errors.New("Insufficient coin hours")
-	}
 	return nil
 }
 

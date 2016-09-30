b2c8e4a5567b5df3eafeae944cbc7f107b4f1e52
iketheadore <heaven.mao@gmail.com>
2016-09-30 22:57:44 +0800

    Update error handling for uxout handler

--- src/gui/uxout.go
+++ src/gui/uxout.go
@@ -5,10 +5,11 @@ import (
 
 	"github.com/skycoin/skycoin/src/cipher"
 	"github.com/skycoin/skycoin/src/daemon"
-	"github.com/skycoin/skycoin/src/visor"
+	wh "github.com/skycoin/skycoin/src/util/http" //http,json helpers
+	"github.com/skycoin/skycoin/src/visor/historydb"
 )
 
-type UxOutJson struct {
+type uxOutJson struct {
 	Time          uint64 `json:"time"`
 	SrcBkSeq      uint64 `json:"src_block_seq"`
 	SrcTx         string `json:"src_tx"`
@@ -19,39 +20,104 @@ type UxOutJson struct {
 	SpentTxID     string `json:"spent_tx"`        // id of tx which spent this output.
 }
 
+func newUxOutJson(out *historydb.UxOut) *uxOutJson {
+	return &uxOutJson{
+		Time:          out.Out.Head.Time,
+		SrcBkSeq:      out.Out.Head.BkSeq,
+		SrcTx:         out.Out.Body.SrcTransaction.Hex(),
+		OwnerAddress:  out.Out.Body.Address.String(),
+		Coins:         out.Out.Body.Coins,
+		Hours:         out.Out.Body.Hours,
+		SpentBlockSeq: out.SpentBlockSeq,
+		SpentTxID:     out.SpentTxID.Hex(),
+	}
+}
+
 func RegisterUxOutHandlers(mux *http.ServeMux, gateway *daemon.Gateway) {
 	// get uxout by id.
 	mux.HandleFunc("/uxout", getUxOutByID(gateway))
+	// get address in uxouts
+	mux.HandleFunc("/address_in_uxouts", getRecvUxOutOfAddr(gateway))
+	// get address out uxouts
+	mux.HandleFunc("/address_out_uxouts", getSpentOutUxOutOfAddr(gateway))
 }
 
 func getUxOutByID(gateway *daemon.Gateway) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var rlt struct {
-			Success bool                 `json:"success"`
-			Reason  string               `json:"reason, omitempty"`
-			Uxout   visor.ReadableOutput `json:"output, omitempty"`
+		uxid := r.FormValue("uxid")
+		if uxid == "" {
+			wh.Error400(w, "uxid is empty")
+			return
 		}
 
-		for {
-			uxid := r.FormValue("uxid")
-			if uxid == "" {
-				rlt.Reason = "uxid is empty"
-				break
-			}
-			id, err := cipher.SHA256FromHex(uxid)
-			if err != nil {
-				rlt.Reason = err.Error()
-				break
-			}
-			uxout, err := gateway.V.GetUxOutByID(id)
-			if err != nil {
-				rlt.Reason = err.Error()
-				break
-			}
-			rlt.Success = true
-			rlt.Uxout = visor.NewReadableOutput(uxout.Out)
-			break
+		id, err := cipher.SHA256FromHex(uxid)
+		if err != nil {
+			wh.Error400(w, err.Error())
+			return
 		}
-		wh.SendOr404(w, &rlt)
+
+		uxout, err := gateway.V.GetUxOutByID(id)
+		if err != nil {
+			wh.Error400(w, err.Error())
+			return
+		}
+
+		wh.SendOr404(w, newUxOutJson(uxout))
+	}
+}
+
+func getRecvUxOutOfAddr(gateway *daemon.Gateway) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		addr := r.FormValue("address")
+		if addr == "" {
+			wh.Error400(w, "address is empty")
+			return
+		}
+
+		cipherAddr, err := cipher.DecodeBase58Address(addr)
+		if err != nil {
+			wh.Error400(w, err.Error())
+			return
+		}
+
+		uxs, err := gateway.V.GetRecvUxOutOfAddr(cipherAddr)
+		if err != nil {
+			wh.Error400(w, err.Error())
+			return
+		}
+
+		uxOuts := make([]*uxOutJson, len(uxs))
+		for i, ux := range uxs {
+			uxOuts[i] = newUxOutJson(ux)
+		}
+		wh.SendOr404(w, &uxOuts)
+	}
+}
+
+func getSpentOutUxOutOfAddr(gateway *daemon.Gateway) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		addr := r.FormValue("address")
+		if addr == "" {
+			wh.Error400(w, "address is empty")
+			return
+		}
+
+		cipherAddr, err := cipher.DecodeBase58Address(addr)
+		if err != nil {
+			wh.Error400(w, err.Error())
+			return
+		}
+
+		uxs, err := gateway.V.GetSpentUxOutOfAddr(cipherAddr)
+		if err != nil {
+			wh.Error400(w, err.Error())
+			return
+		}
+
+		uxOuts := make([]*uxOutJson, len(uxs))
+		for i, ux := range uxs {
+			uxOuts[i] = newUxOutJson(ux)
+		}
+		wh.SendOr404(w, &uxOuts)
 	}
 }

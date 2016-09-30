ead52cbabdeae4b6178b945a8c842e951e8fd77a
iketheadore <heaven.mao@gmail.com>
2016-09-30 11:05:26 +0800

    Add api for getting block

--- src/gui/blockchain.go
+++ src/gui/blockchain.go
@@ -6,7 +6,9 @@ import (
 	"net/http"
 	"strconv"
 
-	wh "github.com/skycoin/skycoin/src/util/http" //http,json helpers
+	"github.com/skycoin/skycoin/src/cipher"
+	wh "github.com/skycoin/skycoin/src/util/http"
+	"github.com/skycoin/skycoin/src/visor" //http,json helpers
 
 	"github.com/skycoin/skycoin/src/daemon"
 )
@@ -55,7 +57,78 @@ func blockchainProgressHandler(gateway *daemon.Gateway) http.HandlerFunc {
 
 func RegisterBlockchainHandlers(mux *http.ServeMux, gateway *daemon.Gateway) {
 	mux.HandleFunc("/blockchain", blockchainHandler(gateway))
-	mux.HandleFunc("/blockchain/block", blockchainBlockHandler(gateway))
+	// get block by hash
+	mux.HandleFunc("/blockchain/block/hash", getBlockByHash(gateway))
+	// get block by seq
+	mux.HandleFunc("/blockchain/block/seq", getBlockBySeq(gateway))
 	mux.HandleFunc("/blockchain/blocks", blockchainBlocksHandler(gateway))
 	mux.HandleFunc("/blockchain/progress", blockchainProgressHandler(gateway))
+}
+
+func getBlockByHash(gate *daemon.Gateway) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		hash := r.FormValue("hash")
+		var rlt struct {
+			Success bool                `json:"success"`
+			Reason  string              `json:"reason,omitempty"`
+			Block   visor.ReadableBlock `json:"block,omitempty"`
+		}
+		for {
+			if hash == "" {
+				rlt.Reason = "block hash is empty"
+				break
+			}
+
+			h, err := cipher.SHA256FromHex(hash)
+			if err != nil {
+				rlt.Reason = err.Error()
+				break
+			}
+
+			b := gate.V.GetBlockByHash(h)
+			if b == nil {
+				rlt.Reason = fmt.Sprintf("block of hash:%s does not exist", hash)
+				break
+			}
+
+			rlt.Success = true
+			rlt.Block = visor.NewReadableBlock(b)
+			break
+		}
+		wh.SendOr404(w, &rlt)
+	}
+}
+
+func getBlockBySeq(gate *daemon.Gateway) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		seq := r.FormValue("seq")
+		var rlt struct {
+			Success bool                `json:"success"`
+			Reason  string              `json:"reason,omitempty"`
+			Block   visor.ReadableBlock `json:"block,omitempty"`
+		}
+		for {
+			if seq == "" {
+				rlt.Reason = "block seq is empty"
+				break
+			}
+
+			uSeq, err := strconv.ParseUint(seq, 10, 64)
+			if err != nil {
+				rlt.Reason = err.Error()
+				break
+			}
+
+			b := gate.V.GetBlockBySeq(uSeq)
+			if b == nil {
+				rlt.Reason = fmt.Sprintf("block in seq:%s does not exist", seq)
+				break
+			}
+
+			rlt.Success = true
+			rlt.Block = visor.NewReadableBlock(b)
+			break
+		}
+		wh.SendOr404(w, &rlt)
+	}
 }

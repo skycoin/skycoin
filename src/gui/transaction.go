2786b91308f8cd63a4fe488a2a5551343daefe46
iketheadore <heaven.mao@gmail.com>
2016-09-30 15:56:41 +0800

    Update transaction corresponding api handlers
    Change the endpoint of /transactions to /pendingTxs; add /lastTxs endpoint, which is used to query the last 20 transactions; update the getTransactionByID handler.

--- src/gui/transaction.go
+++ src/gui/transaction.go
@@ -14,8 +14,10 @@ import (
 )
 
 func RegisterTxHandlers(mux *http.ServeMux, gateway *daemon.Gateway) {
-	//get set of pending transaction
-	mux.HandleFunc("/transactions", getTransactions(gateway))
+	// get set of pending transactions
+	mux.HandleFunc("/pendingTxs", getPendingTxs(gateway))
+	// get latest confirmed transactions
+	mux.HandleFunc("/lastTxs", getLastTxs(gateway))
 	// get txn by txid.
 	mux.HandleFunc("/transaction", getTransactionByID(gateway))
 	//inject a transaction into network
@@ -25,109 +27,94 @@ func RegisterTxHandlers(mux *http.ServeMux, gateway *daemon.Gateway) {
 }
 
 // Returns pending transactions
-// TODO: FIX!!! Iterates all blocks since begining
-// Gets list of transactions
-// TODO: this will slow down exponentially as blockchain size increases
-// TODO: split function for determining if transaction is confirmed, into another function
-// TODO: only iterate, last 50 blocks, to determine if transaction is confirmed
-// TODO: use transaction ID hash, not readable, to confirm transaction
-func getTransactions(gateway *daemon.Gateway) http.HandlerFunc {
+func getPendingTxs(gateway *daemon.Gateway) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		V := gateway.V
-		isConfirmed := r.URL.Query().Get("confirm")
-
-		//default case
-		if isConfirmed != "1" {
-			ret := make([]*visor.ReadableUnconfirmedTxn, 0, len(V.Unconfirmed.Txns))
-			for _, unconfirmedTxn := range V.Unconfirmed.Txns {
-				readable := visor.NewReadableUnconfirmedTxn(&unconfirmedTxn)
-				ret = append(ret, &readable)
-			}
-			wh.SendOr404(w, ret)
+		ret := make([]*visor.ReadableUnconfirmedTxn, 0, len(V.Unconfirmed.Txns))
+		for _, unconfirmedTxn := range V.Unconfirmed.Txns {
+			readable := visor.NewReadableUnconfirmedTxn(&unconfirmedTxn)
+			ret = append(ret, &readable)
 		}
 
-		//WARNING: TODO: This iterates all blocks and all transactions
-		//TODO: need way to determine if transaction is "confirmed", without iterating all blocks
-		if isConfirmed == "1" {
-			// blks := V.Blockchain.Blocks
-
-			//only look at last 50 blocks, for checking if transaction is confirmed
-			// const max_history = 50
-			// x1 := len(blks)               // start
-			// x2 := len(blks) - max_history //stop
-			// if x2 < 0 {
-			// 	x2 = 0
-			// }
-			blks := V.Blockchain.GetLastBlocks(50)
-			// blks = blks[x2:x1] //only look at last 50 blocks
-
-			totalTxns := []coin.Transaction{}
-			//WARNING: Iterates all blocks, since start
-			//TODO: use transaction hash, not input/output
-			for _, b := range blks {
-				totalTxns = append(totalTxns, b.Body.Transactions...)
-			}
-
-			rdTxns := make([]visor.ReadableTransaction, len(totalTxns))
-			for i, txn := range totalTxns {
-				rdTxns[i] = visor.NewReadableTransaction(&txn)
-			}
-
-			rltTxns := []visor.ReadableTransaction{}
-			// addr := r.URL.Query().Get("addr")
-			input := r.URL.Query().Get("input")
-			output := r.URL.Query().Get("output")
-
-			if input != "" {
-				uxids := getUxidsOfAddr(input, rdTxns)
-				for _, uxid := range uxids {
-					for _, txn := range rdTxns {
-						for _, in := range txn.In {
-							if in == uxid {
-								rltTxns = append(rltTxns, txn)
-								break
-							}
-						}
-					}
-				}
-			}
-
-			if output != "" {
-				outTxns := []visor.ReadableTransaction{}
-				if input != "" {
-					outTxns = rltTxns
-				} else {
-					outTxns = rdTxns
-				}
-
-				txs := []visor.ReadableTransaction{}
-				for _, txn := range outTxns {
-					for _, out := range txn.Out {
-						if out.Address == output {
-							txs = append(txs, txn)
-							break
-						}
-					}
-				}
-				rltTxns = txs
-			}
-			wh.SendOr404(w, rltTxns)
+		var rlt struct {
+			Success bool                            `json:"success"`
+			Txns    []*visor.ReadableUnconfirmedTxn `json:"unconfirm_txs"`
+		} {
+			Success: true,
+			Txns: ret,
 		}
 
+		wh.SendOr404(w, &rlt)
 	}
 }
 
-func getUxidsOfAddr(addr string, rdTxns []visor.ReadableTransaction) []string {
-	uxids := []string{}
-	for _, txn := range rdTxns {
-		for _, out := range txn.Out {
-			if out.Address == addr {
-				uxids = append(uxids, out.Hash)
-			}
+// getLastTxs get the last confirmed txs.
+func getLastTxs(gateway *daemon.Gateway) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var rlt struct {
+			Success bool                      `json:"success"`
+			Reason  string                    `json:"reason, omitempty"`
+			Txs     []visor.TransactionResult `json:"transactions, omitempty"`
 		}
+		for {
+			txs, err := gateway.V.GetLastTxs()
+			if err != nil {
+				rlt.Reason = err.Error()
+				break
+			}
+			rlt.Success = true
+			rlt.Txs = make([]visor.TransactionResult, len(txs))
+			for i, tx := range txs {
+				head := gateway.V.GetHeadBlock()
+				height := head.Seq() - tx.BlockSeq + 1
+				rlt.Txs[i] = visor.TransactionResult{
+					Transaction: visor.NewReadableTransaction(&tx.Tx),
+					Status:      visor.NewConfirmedTransactionStatus(height),
+				}
+			}
+			break
+		}
+		wh.SendOr404(w, &rlt)
 	}
-	return uxids
 }
+
+func getTransactionByID(gate *daemon.Gateway) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var rlt struct {
+			Success bool                    `json:"success"`
+			Reason  string                  `json:"reason,omitempty"`
+			Tx      visor.TransactionResult `json:"transaction, omitempty"`
+		}
+		for {
+			txid := r.FormValue("txid")
+			if txid == "" {
+				rlt.Reason = "txid is empty"
+				break
+			}
+
+			h, err := cipher.SHA256FromHex(txid)
+			if err != nil {
+				rlt.Reason = err.Error()
+				break
+			}
+
+			tx, err := gate.V.GetTransaction(h)
+			if err != nil {
+				rlt.Reason = err.Error()
+				break
+			}
+
+			rlt.Success = true
+			rlt.Tx = visor.TransactionResult{
+				Transaction: visor.NewReadableTransaction(&tx.Txn),
+				Status:      tx.Status,
+			}
+			break
+		}
+		wh.SendOr404(w, &rlt)
+	}
+}
+
 
 //Implement
 func injectTransaction(gateway *daemon.Gateway) http.HandlerFunc {
@@ -170,26 +157,6 @@ func injectTransaction(gateway *daemon.Gateway) http.HandlerFunc {
 
 		//ret := gateway.Visor.GetUnspentOutputReadables(gateway.V)
 		wh.SendOr404(w, rlt)
-	}
-}
-
-func getTransactionByID(gate *daemon.Gateway) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		txid := r.URL.Query().Get("txid")
-		if txid == "" {
-			return
-		}
-
-		h, err := cipher.SHA256FromHex(txid)
-		if err != nil {
-			return
-		}
-		tx := gate.V.GetTransaction(h)
-		rlt := visor.TransactionResult{
-			Transaction: visor.NewReadableTransaction(&tx.Txn),
-			Status:      tx.Status,
-		}
-		wh.SendOr404(w, &rlt)
 	}
 }
 

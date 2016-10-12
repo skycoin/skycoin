f85aac7ddbcad98c0c2249aaae8fd93c3e322ca2
iketheadore <heaven.mao@gmail.com>
2016-10-12 19:55:44 +0800

    Add generateWallet command frame

---
+++ src/api/cli/generate_wallet.go
@@ -0,0 +1,56 @@
+package cli
+
+import (
+	gcli "github.com/urfave/cli"
+)
+
+func init() {
+	cmd := gcli.Command{
+		Name: "generateWallet",
+		Description: `Generate a new wallet from seed. 
+
+                      Use caution when using the “-p” command. If you have 
+                      command history enabled your wallet encryption password 
+                      can be recovered from the history log. If you do not include 
+                      the “-p” option you will be prompted to enter your password after 
+                      you enter your command.
+                       
+                      All results are returned in JSON format. 
+                      `,
+		Usage: "skycoin generateWallet [options]",
+		Flags: []gcli.Flag{
+			gcli.StringFlag{
+				Name:  "s",
+				Usage: "Your seed.",
+			},
+			gcli.StringFlag{
+				Name:  "r",
+				Usage: "A random alpha numeric seed will be generated for you.",
+			},
+			gcli.StringFlag{
+				Name:  "rd",
+				Usage: "A random seed consisting of 12 dictionary words will be generated for you.",
+			},
+			gcli.IntFlag{
+				Name:  "m",
+				Usage: "[numberOfAddresses] Number of addresses to generate. By default 1 address is generated.",
+			},
+			gcli.StringFlag{
+				Name:  "p",
+				Usage: "Password used to encrypt the wallet locally.",
+			},
+			gcli.StringFlag{
+				Name:  "n",
+				Usage: `[walletName] Name of wallet. The final format will be "yourName.wlt". If no wallet name is specified a generic name will be selected.`,
+			},
+			gcli.StringFlag{
+				Name:  "l",
+				Usage: "[label] Label used to idetify your wallet.",
+			},
+		},
+		Action: func(c *gcli.Context) error {
+			return nil
+		},
+	}
+	Commands = append(Commands, cmd)
+}

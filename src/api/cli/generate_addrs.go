f34633110fe0bcd42abf07c24ff911a5d89510f9
iketheadore <heaven.mao@gmail.com>
2016-10-12 20:24:10 +0800

    Add generate addresses command frame

---
+++ src/api/cli/generate_addrs.go
@@ -0,0 +1,37 @@
+package cli
+
+import (
+	gcli "github.com/urfave/cli"
+)
+
+func init() {
+	cmd := gcli.Command{
+		Name: "generateAddresses",
+		Description: `Generate additional addresses for a wallet.  
+        
+                      Use caution when using the “-p” command. If you have command 
+                      history enabled your wallet encryption password can be recovered 
+                      from the history log. If you do not include the “-p” option you 
+                      will be prompted to enter your password after you enter your command. 
+                      `,
+		Usage: "skycoin generateAddresses [options]",
+		Flags: []gcli.Flag{
+			gcli.StringFlag{
+				Name: "m",
+				Usage: "[numberOfAddresses]	Number of addresses to generate. By default 1 address is generated.",
+			},
+			gcli.StringFlag{
+				Name:  "p",
+				Usage: "[password] Password used to encrypt the wallet locally.",
+			},
+			gcli.StringFlag{
+				Name:  "j,json",
+				Usage: "Returns the results in JSON format.",
+			},
+		},
+		Action: func(c *gcli.Context) error {
+			return nil
+		},
+	}
+	Commands = append(Commands, cmd)
+}

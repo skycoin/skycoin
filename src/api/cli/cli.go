5fb9daea0db6e481c3c8206338cd157208cd56de
iketheadore <heaven.mao@gmail.com>
2016-10-13 11:35:26 +0800

    Add skycoinNodeAddr variable in cli

--- src/api/cli/cli.go
+++ src/api/cli/cli.go
@@ -4,7 +4,12 @@ import gcli "gopkg.in/urfave/cli.v1"
 
 // Commands all cmds that we support
 var Commands []gcli.Command
+var skycoinNodeAddr = "http://localhost:6420"
 
 func stringPtr(v string) *string {
 	return &v
+}
+
+func httpGet(url string, v interface{}) error {
+	return nil
 }

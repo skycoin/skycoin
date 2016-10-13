a4835bff4fb063f6411143522f8d925322d4cfb5
iketheadore <heaven.mao@gmail.com>
2016-10-13 11:36:15 +0800

    Add lastBlocks command action

---
+++ src/api/cli/last_blocks.go
@@ -0,0 +1,45 @@
+package cli
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	gcli "gopkg.in/urfave/cli.v1"
+)
+
+func init() {
+	cmd := gcli.Command{
+		Name:        "lastBlocks",
+		Usage:       "Displays the content of the most recently N generated blocks.",
+		Description: "All results returned in JSON format.",
+		ArgsUsage:   "[option] [numberOfBlocks]",
+		Action:      getLastBlocks,
+	}
+	Commands = append(Commands, cmd)
+}
+
+func getLastBlocks(c *gcli.Context) error {
+	num := c.Args().First()
+	if num == "" {
+		num = "1"
+	}
+
+	n, err := strconv.Atoi(num)
+	if err != nil {
+		return err
+	}
+	url := fmt.Sprintf("%s/last_blocks?num=%d", skycoinNodeAddr, n)
+	rsp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer rsp.Body.Close()
+	d, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(d))
+	return nil
+}

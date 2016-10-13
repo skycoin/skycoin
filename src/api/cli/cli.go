c21fc80efc9119a636502a876af26312645245b7
iketheadore <heaven.mao@gmail.com>
2016-10-13 14:41:37 +0800

    Chane skycoinNodeAddr var to nodeAddress

--- src/api/cli/cli.go
+++ src/api/cli/cli.go
@@ -1,10 +1,20 @@
 package cli
 
-import gcli "gopkg.in/urfave/cli.v1"
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/skycoin/skycoin/src/util"
+
+	gcli "gopkg.in/urfave/cli.v1"
+)
 
 // Commands all cmds that we support
 var Commands []gcli.Command
-var skycoinNodeAddr = "http://localhost:6420"
+var (
+	nodeAddress = os.Getenv("SKYCOIN_NODE_ADDR")
+	walletDir   = os.Getenv("SKYCOIN_WLT_DIR")
+)
 
 func stringPtr(v string) *string {
 	return &v
@@ -12,4 +22,15 @@ func stringPtr(v string) *string {
 
 func httpGet(url string, v interface{}) error {
 	return nil
+}
+
+func init() {
+	if nodeAddress == "" {
+		nodeAddress = "http://localhost:6421"
+	}
+
+	if walletDir == "" {
+		home := util.UserHome()
+		walletDir = filepath.Join(home, ".skycoin-cli/wallet")
+	}
 }

4b27a452eb7f6269d71720ebe7e8da09e169e315
iketheadore <heaven.mao@gmail.com>
2016-10-20 12:26:46 +0800

    Add transaction action

--- src/api/cli/transaction.go
+++ src/api/cli/transaction.go
@@ -1,19 +1,51 @@
 package cli
 
-import gcli "gopkg.in/urfave/cli.v1"
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/skycoin/skycoin/src/cipher"
+
+	gcli "gopkg.in/urfave/cli.v1"
+)
 
 func init() {
 	cmd := gcli.Command{
 		Name:      "transaction",
 		Usage:     "Lists details of specific transaction",
 		ArgsUsage: "[option] [transaction id]",
-		Flags: []gcli.Flag{
-			gcli.StringFlag{
-				Name:  "j,json",
-				Usage: "Returns the results in JSON format.",
-			},
-		},
+		// Flags:     []gcli.Flag{
+		// gcli.StringFlag{
+		// 	Name:  "j,json",
+		// 	Usage: "Returns the results in JSON format.",
+		// },
+		// },
 		Action: func(c *gcli.Context) error {
+			txid := c.Args().First()
+			if txid == "" {
+				return errors.New("txid is empty")
+			}
+
+			// validate the txid
+			_, err := cipher.SHA256FromHex(txid)
+			if err != nil {
+				return errors.New("error txid")
+			}
+
+			url := fmt.Sprintf("%v/transaction?txid=%v", nodeAddress, txid)
+			rsp, err := http.Get(url)
+			if err != nil {
+				return errConnectNodeFailed
+			}
+
+			defer rsp.Body.Close()
+			d, err := ioutil.ReadAll(rsp.Body)
+			if err != nil {
+				return err
+			}
+			fmt.Println(string(d))
 			return nil
 		},
 	}

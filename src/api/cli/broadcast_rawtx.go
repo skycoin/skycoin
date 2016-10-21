c6b71812844ae661c4e6de0f3ccc56efb7ca81ae
iketheadore <heaven.mao@gmail.com>
2016-10-21 16:11:25 +0800

    Add broadcast transaction action

--- src/api/cli/broadcast_rawtx.go
+++ src/api/cli/broadcast_rawtx.go
@@ -1,6 +1,15 @@
 package cli
 
-import gcli "gopkg.in/urfave/cli.v1"
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	gcli "gopkg.in/urfave/cli.v1"
+)
 
 func init() {
 	cmd := gcli.Command{
@@ -8,6 +17,30 @@ func init() {
 		Usage:     "Broadcast a raw transaction to the network.",
 		ArgsUsage: "[transaction]",
 		Action: func(c *gcli.Context) error {
+			rawtx := c.Args().First()
+			if rawtx == "" {
+				return errors.New("raw transaction is empty")
+			}
+			var tx = struct {
+				Rawtx string `json:"rawtx"`
+			}{
+				rawtx,
+			}
+			d, err := json.Marshal(tx)
+			if err != nil {
+				return err
+			}
+			url := fmt.Sprintf("%s/injectTransaction", nodeAddress)
+			rsp, err := http.Post(url, "application/json", bytes.NewBuffer(d))
+			if err != nil {
+				return errConnectNodeFailed
+			}
+			defer rsp.Body.Close()
+			v, err := ioutil.ReadAll(rsp.Body)
+			if err != nil {
+				return err
+			}
+			fmt.Println(string(v))
 			return nil
 		},
 	}

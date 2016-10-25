f7387bd1e5f8edfde203ab08c041b63a1f231167
iketheadore <heaven.mao@gmail.com>
2016-10-25 18:02:30 +0800

    Adjust usage message

--- src/api/cli/generate_addrs.go
+++ src/api/cli/generate_addrs.go
@@ -21,18 +21,20 @@ func init() {
 		Usage:     "[options]",
 		Description: `
         Use caution when using the “-p” command. If you have command 
-        history enabled your wallet encryption password can be recovered from the history log. 
-        If you do not include the “-p” option you will be prompted to enter your password after 
-        you enter your command.`,
+        history enabled your wallet encryption password can be recovered from the 
+		history log. If you do not include the “-p” option you will be prompted to 
+		enter your password after you enter your command.`,
 		Flags: []gcli.Flag{
 			gcli.IntFlag{
 				Name:  "m",
 				Value: 1,
-				Usage: "[numberOfAddresses]	Number of addresses to generate. By default 1 address is generated.",
+				Usage: `[numberOfAddresses]	Number of addresses to generate. 
+						By default 1 address is generated.`,
 			},
 			gcli.StringFlag{
-				Name:  "w",
-				Usage: "[wallet file or path] In wallet. If no path is specified your default wallet path will be used.",
+				Name: "w",
+				Usage: `[wallet file or path] In wallet. If no path is specified your default 
+					wallet path will be used.`,
 			},
 			// gcli.StringFlag{
 			// 	Name:  "p",

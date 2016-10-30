eaa89a276b30144285ae4d09c45dfdd9a9bb9023
iketheadore <heaven.mao@gmail.com>
2016-10-30 23:22:53 +0800

    Add getBlocksHandler

--- src/api/webrpc/block.go
+++ src/api/webrpc/block.go
@@ -6,7 +6,7 @@ import (
 )
 
 // get last blocks
-func getLastBlocks(req Request, gateway Gatewayer) Response {
+func getLastBlocksHandler(req Request, gateway Gatewayer) Response {
 	// validate the req params
 	n, err := strconv.ParseUint(req.Params["num"], 10, 64)
 	if err != nil {
@@ -17,6 +17,38 @@ func getLastBlocks(req Request, gateway Gatewayer) Response {
 	}
 
 	blocks := gateway.GetLastBlocks(n)
+	d, err := json.Marshal(blocks)
+	if err != nil {
+		logger.Errorf("%v", err)
+		return makeErrorResponse("", &RPCError{
+			Code:    errCodeInternalError,
+			Message: errMsgInternalErr,
+		})
+	}
+
+	return makeSuccessResponse(req.ID, string(d))
+}
+
+func getBlocksHandler(req Request, gateway Gatewayer) Response {
+	// validate the params
+	start, end := req.Params["start"], req.Params["end"]
+	s, err := strconv.ParseUint(start, 10, 64)
+	if err != nil {
+		return makeErrorResponse("", &RPCError{
+			Code:    errCodeInvalidParams,
+			Message: errMsgInvalidParams,
+		})
+	}
+
+	e, err := strconv.ParseUint(end, 10, 64)
+	if err != nil {
+		return makeErrorResponse("", &RPCError{
+			Code:    errCodeInvalidParams,
+			Message: errMsgInvalidParams,
+		})
+	}
+
+	blocks := gateway.GetBlocks(s, e)
 	d, err := json.Marshal(blocks)
 	if err != nil {
 		logger.Errorf("%v", err)

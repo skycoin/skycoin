1876424d1e612de76ab669b27be9631b4c464973
iketheadore <heaven.mao@gmail.com>
2016-11-02 11:03:00 +0800

    Add debug log

--- src/api/webrpc/transaction.go
+++ src/api/webrpc/transaction.go
@@ -1,7 +1,13 @@
 package webrpc
 
-import "github.com/skycoin/skycoin/src/cipher"
-import "github.com/skycoin/skycoin/src/visor"
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/skycoin/skycoin/src/cipher"
+	"github.com/skycoin/skycoin/src/coin"
+	"github.com/skycoin/skycoin/src/visor"
+)
 
 type TxnResult struct {
 	Transaction *visor.TransactionResult
@@ -10,6 +16,7 @@ type TxnResult struct {
 func getTransactionHandler(req Request, gateway Gatewayer) Response {
 	var txid []string
 	if err := req.DecodeParams(&txid); err != nil {
+		logger.Criticalf("decode params failed:%v", err)
 		return makeErrorResponse(errCodeInvalidParams, errMsgInvalidParams)
 	}
 
@@ -19,9 +26,9 @@ func getTransactionHandler(req Request, gateway Gatewayer) Response {
 
 	t, err := cipher.SHA256FromHex(txid[0])
 	if err != nil {
-		return makeErrorResponse(errCodeInvalidParams, "invalid transaction id")
+		logger.Criticalf("decode txid err: %v", err)
+		return makeErrorResponse(errCodeInvalidParams, "invalid transaction hash")
 	}
-
 	txn, err := gateway.GetTransaction(t)
 	if err != nil {
 		logger.Debugf("%v", err)

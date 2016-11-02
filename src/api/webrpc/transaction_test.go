cef5e72a41fa9edacaff42ecb0850776e4535c8b
iketheadore <heaven.mao@gmail.com>
2016-11-02 18:39:27 +0800

    Add inject transaction test code

--- src/api/webrpc/transaction_test.go
+++ src/api/webrpc/transaction_test.go
@@ -144,3 +144,92 @@ func Test_getTransactionHandler(t *testing.T) {
 		}
 	}
 }
+
+var rawTx = `dc0000000010e05181fd4023f865a84359bf72a304e687b6f00e42f93ad9a4b8ee5a64aabc01000000dcb5b236eecd97a36c7d0a0b8ed68bb5df6274433a51fddf911f02f3926d20bf6eaabdc21529b7696f498545b06cc7e69f2f08b4dc5fa823c5b3f03da06794a300010000006d8a9c89177ce5e9d3b4b59fff67c00f0471fdebdfbb368377841b03fc7d688b02000000005771eeda2e253697cf5368f16fe05210d5cd319040420f0000000000af010000000000000060dfa95881cdc827b45a6d49b11dbc152ecd4de600093d0000000000af01000000000000`
+var txid = "3e52703a21bf9462799f52ab0cedb314efcf7c43aadb815429cd79f35f040954"
+
+func Test_injectTransactionHandler(t *testing.T) {
+	type args struct {
+		req     Request
+		gateway Gatewayer
+	}
+	tests := []struct {
+		name string
+		args args
+		want Response
+	}{
+		// TODO: Add test cases.
+		{
+			"normal",
+			args{
+				req: Request{
+					ID:      "1",
+					Jsonrpc: jsonRPC,
+					Method:  "inject_transaction",
+					Params:  []byte(fmt.Sprintf("[%q]", rawTx)),
+				},
+				gateway: &fakeGateway{
+					injectRawTxMap: map[string]bool{
+						txid: true,
+					},
+				},
+			},
+			makeSuccessResponse("1", InjectResult{txid}),
+		},
+		{
+			"invalid params: invalid raw transaction",
+			args{
+				req: Request{
+					ID:      "1",
+					Jsonrpc: jsonRPC,
+					Method:  "inject_transaction",
+					Params:  []byte(`["abcdefghijk"]`),
+				},
+			},
+			makeErrorResponse(errCodeInvalidParams, "invalid raw transaction:encoding/hex: odd length hex string"),
+		},
+		{
+			"invalid params type",
+			args{
+				req: Request{
+					ID:      "1",
+					Jsonrpc: jsonRPC,
+					Method:  "inject_transaction",
+					Params:  []byte("abcdefghijk"),
+				},
+			},
+			makeErrorResponse(errCodeInvalidParams, errMsgInvalidParams),
+		},
+		{
+			"invalid params: more than one raw transaction",
+			args{
+				req: Request{
+					ID:      "1",
+					Jsonrpc: jsonRPC,
+					Method:  "inject_transaction",
+					Params:  []byte(fmt.Sprintf("[%q,%q]", rawTx, rawTx)),
+				},
+			},
+			makeErrorResponse(errCodeInvalidParams, errMsgInvalidParams),
+		},
+		{
+			"internal error",
+			args{
+				req: Request{
+					ID:      "1",
+					Jsonrpc: jsonRPC,
+					Method:  "inject_transaction",
+					Params:  []byte(fmt.Sprintf("[%q]", rawTx)),
+				},
+				gateway: &fakeGateway{},
+			},
+			makeErrorResponse(errCodeInternalError, "inject transaction failed:inject transaction failed"),
+		},
+	}
+
+	for _, tt := range tests {
+		if got := injectTransactionHandler(tt.args.req, tt.args.gateway); !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("%q. injectTransactionHandler() = %+v, want %+v", tt.name, got, tt.want)
+		}
+	}
+}
--- src/api/webrpc/webrpc_test.go
+++ src/api/webrpc/webrpc_test.go
@@ -2,8 +2,10 @@ package webrpc
 
 import (
 	"encoding/json"
+	"errors"
 
 	"github.com/skycoin/skycoin/src/cipher"
+	"github.com/skycoin/skycoin/src/coin"
 	"github.com/skycoin/skycoin/src/visor"
 )
 
@@ -17,7 +19,8 @@ func setup() (*rpcHandler, func()) {
 }
 
 type fakeGateway struct {
-	transactions map[string]string
+	transactions   map[string]string
+	injectRawTxMap map[string]bool // key: transacion hash, value indicates whether the injectTransaction should return error.
 }
 
 func (fg fakeGateway) GetLastBlocks(num uint64) *visor.ReadableBlocks {
@@ -64,4 +67,12 @@ func (fg fakeGateway) GetTransaction(txid cipher.SHA256) (*visor.TransactionResu
 		return decodeTransaction(str), nil
 	}
 	return nil, nil
+}
+
+func (fg fakeGateway) InjectTransaction(txn coin.Transaction) (coin.Transaction, error) {
+	if _, v := fg.injectRawTxMap[txn.Hash().Hex()]; v {
+		return txn, nil
+	}
+
+	return txn, errors.New("inject transaction failed")
 }

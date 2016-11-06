bc1fd43836a7d90163547f9bbbeec1435451a932
iketheadore <heaven.mao@gmail.com>
2016-11-06 11:47:16 +0800

    Update errAddress

--- src/api/cli/cli.go
+++ src/api/cli/cli.go
@@ -27,7 +27,7 @@ var (
 	errConnectNodeFailed = errors.New("connect to node failed")
 	errWalletName        = fmt.Errorf("error wallet file name, must has %v extension", walletExt)
 	errLoadWallet        = errors.New("load wallet failed")
-	errAddress           = errors.New("error address")
+	errAddress           = errors.New("invalidate address")
 	errReadResponse      = errors.New("read response body failed")
 	errJSONMarshal       = errors.New("json marshal failed")
 	errJSONUnmarshal     = errors.New("json unmarshal failed")

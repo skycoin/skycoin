30a412cba4e09aa714982b6a68c7b1029022a0d5
iketheadore <heaven.mao@gmail.com>
2016-11-07 16:21:03 +0800

    Change the json flag txid in ReadableOutput to hash
    This flag should be hash, it's not the txid.

--- src/visor/readable.go
+++ src/visor/readable.go
@@ -158,7 +158,7 @@ func NewReadableTransactionOutput(t *coin.TransactionOutput, txid cipher.SHA256)
 	Add a verbose version
 */
 type ReadableOutput struct {
-	Hash              string `json:"txid"` //hash uniquely identifies transaction
+	Hash              string `json:"hash"` //hash uniquely identifies transaction
 	SourceTransaction string `json:"src_tx"`
 	Address           string `json:"address"`
 	Coins             string `json:"coins"`

55dace730e434a9d4bd62bf8a317bb9ed6c9846b
iketheadore <heaven.mao@gmail.com>
2016-11-13 17:13:37 +0800

    Fix error about undefined Criticalf method in log

--- src/api/webrpc/transaction.go
+++ src/api/webrpc/transaction.go
@@ -20,7 +20,7 @@ type InjectResult struct {
 func getTransactionHandler(req Request, gateway Gatewayer) Response {
 	var txid []string
 	if err := req.DecodeParams(&txid); err != nil {
-		logger.Criticalf("decode params failed:%v", err)
+		logger.Critical("decode params failed:%v", err)
 		return makeErrorResponse(errCodeInvalidParams, errMsgInvalidParams)
 	}
 
@@ -30,7 +30,7 @@ func getTransactionHandler(req Request, gateway Gatewayer) Response {
 
 	t, err := cipher.SHA256FromHex(txid[0])
 	if err != nil {
-		logger.Criticalf("decode txid err: %v", err)
+		logger.Critical("decode txid err: %v", err)
 		return makeErrorResponse(errCodeInvalidParams, "invalid transaction hash")
 	}
 	txn, err := gateway.GetTransaction(t)
@@ -45,7 +45,7 @@ func getTransactionHandler(req Request, gateway Gatewayer) Response {
 func injectTransactionHandler(req Request, gateway Gatewayer) Response {
 	var rawtx []string
 	if err := req.DecodeParams(&rawtx); err != nil {
-		logger.Criticalf("decode params failed:%v", err)
+		logger.Critical("decode params failed:%v", err)
 		return makeErrorResponse(errCodeInvalidParams, errMsgInvalidParams)
 	}
 

941e75e62e049fe92c954334d4672e88be61720b
iketheadore <heaven.mao@gmail.com>
2016-12-15 11:39:03 +0800

    Add ostype checking in electron build scripts

--- electron/compress-standalone-release.sh
+++ electron/compress-standalone-release.sh
@@ -47,8 +47,11 @@ if [ -e "$LNX64_STL" ]; then
         rm "$LNX64_STL_ZIP"
     fi
     echo "Zipping $LNX64_STL_ZIP"
-    # tar czf "$LNX64_STL_ZIP" --owner=0 --group=0 "$LNX64_STL"
-    tar czf "$LNX64_STL_ZIP"  "$LNX64_STL"
+    if [[ "$OSTYPE" == "linux"* ]]; then
+        tar czf "$LNX64_STL_ZIP" --owner=0 --group=0 "$LNX64_STL"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        tar czf "$LNX64_STL_ZIP"  "$LNX64_STL"
+    fi
     FINALS+=("$LNX64_STL_ZIP")
 fi
 

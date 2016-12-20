88d52acdb259fdbaffa83b83aff9d3b35f4ca808
iketheadore <heaven.mao@gmail.com>
2016-12-20 09:23:48 +0800

    Add addrHistory data struct

--- src/api/cli/wallet_history.go
+++ src/api/cli/wallet_history.go
@@ -7,9 +7,21 @@ import (
 
 	"strings"
 
+	"bytes"
+	"encoding/json"
+
+	"github.com/skycoin/skycoin/src/api/webrpc"
 	"github.com/skycoin/skycoin/src/wallet"
 	gcli "github.com/urfave/cli"
 )
+
+type addrHistory struct {
+	Txid      string `json:"txid"`
+	Address   string `json:"address"`
+	Amount    int64  `json:"amount"`
+	Timestamp int    `json:"timestamp"`
+	Status    int    `json:"status"`
+}
 
 func walletHisCMD() gcli.Command {
 	name := "walletHistory"
@@ -51,9 +63,67 @@ func walletHisCMD() gcli.Command {
 				return err
 			}
 
+			uxouts, err := getAddrUxOuts(addrs)
+			if err != nil {
+				return err
+			}
+
+			addrHis := []addrHistory{}
+
+			// Uxid          string `json:"uxid"`
+			// Time          uint64 `json:"time"`
+			// SrcBkSeq      uint64 `json:"src_block_seq"`
+			// SrcTx         string `json:"src_tx"`
+			// OwnerAddress  string `json:"owner_address"`
+			// Coins         uint64 `json:"coins"`
+			// Hours         uint64 `json:"hours"`
+			// SpentBlockSeq uint64 `json:"spent_block_seq"` // block seq that spent the output.
+			// SpentTxID     string `json:"spent_tx"`        // id of tx which spent this output.
+			for _, ux := range uxouts {
+				for _, u := range ux.UxOuts {
+					addrHis = append(addrHis, addrHistory{
+						Address:   ux.Address,
+						Txid:      u.SrcTx,
+						Amount:    int64(u.Coins),
+						Timestamp: int(u.Time),
+						Status:    1,
+					})
+
+					if u.SpentBlockSeq != 0 {
+						// get spent transaction timestamp.
+					}
+				}
+
+			}
+
 			return nil
 		},
 	}
+}
+
+func getAddrUxOuts(addrs []string) ([]webrpc.AddrUxoutResult, error) {
+	req, err := webrpc.NewRequest("get_address_uxouts", addrs, "1")
+	if err != nil {
+		return nil, fmt.Errorf("create rpc request failed:%v", err)
+	}
+
+	rsp, err := webrpc.Do(req, cfg.RPCAddress)
+	if err != nil {
+		return nil, fmt.Errorf("do rpc request failed:%v", err)
+	}
+
+	if rsp.Error != nil {
+		return nil, fmt.Errorf("do rpc request failed:%+v", *rsp.Error)
+	}
+
+	fmt.Println(string(rsp.Result))
+
+	uxouts := []webrpc.AddrUxoutResult{}
+	if err := json.NewDecoder(bytes.NewReader(rsp.Result)).Decode(&uxouts); err != nil {
+		return nil, fmt.Errorf("decode result failed, err:%v", err)
+	}
+
+	return uxouts, nil
 }
 
 func getAddresses(f string) ([]string, error) {

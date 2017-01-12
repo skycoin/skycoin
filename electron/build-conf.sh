9b40d50abb2dcc602b552718f3db700879fb38c3
iketheadore <heaven.mao@gmail.com>
2017-01-12 17:58:31 +0800

    Change ELV_VERSION from v1.2.2 to v1.4.13

--- electron/build-conf.sh
+++ electron/build-conf.sh
@@ -3,12 +3,13 @@ set -e -o pipefail
 
 # These values are also in gulpfile.js and package.json and must be equal
 SKY_VERSION="0.8.1"
-ELN_VERSION="v1.2.2"
+ELN_VERSION="v1.4.13"
 ELN_OUTPUT_BASE=".electron_output"
 ELN_OUTPUT="${ELN_OUTPUT_BASE}/${ELN_VERSION}"
 
 GOX_OSARCH="linux/amd64 windows/amd64 darwin/amd64"
 # GOX_OSARCH="darwin/amd64"
+# GOX_OSARCH="windows/amd64"
 GOX_OUTPUT=".gox_output"
 
 STL_OUTPUT=".standalone_output"

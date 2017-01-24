b1daa400a6605a93aa1c09fb5556e36de1ddcf85
iketheadore <heaven.mao@gmail.com>
2017-01-24 13:14:54 +0800

    Use electron-builder to build app installer

--- electron/build-electron-release.sh
+++ electron/build-electron-release.sh
@@ -16,27 +16,27 @@ set -e -o pipefail
 
 SKIP_COMPILATION=${SKIP_COMPILATION:-0}
 
-GULP_PLATFORM=""
-if [ -n "$1" ]; then
-    GOX_OSARCH="$1"
-    case "$1" in
-    "linux/amd64")
-        GULP_PLATFORM="linux-x64"
-        ;;
-    "linux/arm")
-        GULP_PLATFORM="linux-arm"
-        ;;
-    "windows/amd64")
-        GULP_PLATFORM="win32-x64"
-        ;;
-    "windows/386")
-        GULP_PLATFORM="win32-ia32"
-        ;;
-    "darwin/amd64")
-        GULP_PLATFORM="darwin-x64"
-        ;;
-    esac
-fi
+# GULP_PLATFORM=""
+# if [ -n "$1" ]; then
+#     GOX_OSARCH="$1"
+#     case "$1" in
+#     "linux/amd64")
+#         GULP_PLATFORM="linux-x64"
+#         ;;
+#     "linux/arm")
+#         GULP_PLATFORM="linux-arm"
+#         ;;
+#     "windows/amd64")
+#         GULP_PLATFORM="win32-x64"
+#         ;;
+#     "windows/386")
+#         GULP_PLATFORM="win32-ia32"
+#         ;;
+#     "darwin/amd64")
+#         GULP_PLATFORM="darwin-x64"
+#         ;;
+#     esac
+# fi
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -50,18 +50,50 @@ if [ -e "$ELN_OUTPUT" ]; then
     rm -r "$ELN_OUTPUT"
 fi
 
-if [ -n "$GULP_PLATFORM" ]; then
-    gulp electron --platform "$GULP_PLATFORM"
-else
-    gulp electron
+npm run dist-win
+npm run dist-ml
+# if [ -n "$GULP_PLATFORM" ]; then
+#     gulp electron --platform "$GULP_PLATFORM"
+# else
+#     gulp electron
+# fi
+
+pushd "$FINAL_OUTPUT" >/dev/null
+if [ -e "mac" ]; then
+    pushd "mac" >/dev/null
+    mv "Skycoin-${SKY_VERSION}.dmg" "../skycoin-${SKY_VERSION}-gui-osx-x64.dmg"
+    popd >/dev/null
+    rm -rf "mac"
 fi
 
-echo "--------------------------"
-echo "Packaging electron release"
-./package-electron-release.sh
+IMG="skycoin-${SKY_VERSION}-x86_64.AppImage"
+DEST_IMG="skycoin-${SKY_VERSION}-gui-linux-x64.AppImage"
+if [ -e $IMG ]; then
+    mv "$IMG" "$DEST_IMG"
+    chmod +x "$DEST_IMG"
+fi
 
-echo "----------------------------"
-echo "Compressing electron release"
-./compress-electron-release.sh
+# mv "skycoin-${SKY_VERSION}-x86_64.AppImage" "skycoin-${SKY_VERSION}-gui-linux-x64.AppImage"
+# chmod +x "skycoin-${SKY_VERSION}-gui-linux-x64.AppImage"
+EXE="Skycoin Setup ${SKY_VERSION}.exe"
+if [ -e "$EXE" ]; then
+    mv "$EXE" "skycoin-${SKY_VERSION}-gui-win-installer.exe"
+fi
+
+# mv "Skycoin Setup ${SKY_VERSION}.exe" "skycoin-${SKY_VERSION}-gui-win-insaller.exe"
+
+# clean unpacked folders
+rm -rf *-unpacked
+
+popd >/dev/null
+
+
+# echo "--------------------------"
+# echo "Packaging electron release"
+# ./package-electron-release.sh
+
+# echo "----------------------------"
+# echo "Compressing electron release"
+# ./compress-electron-release.sh
 
 popd >/dev/null

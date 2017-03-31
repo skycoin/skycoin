2a631feab085588b179a8bab67ad2264ef1385f9
Vyacheslav Zgordan <zgordan.vv@gmail.com>
2017-03-31 04:20:21 +0000

    SOCKS5 server is working, closing of obsolete connections is added

--- src/mesh/app/socks_server.go
+++ src/mesh/app/socks_server.go
@@ -2,21 +2,23 @@ package app
 
 import (
 	"fmt"
-	//	"io"
+	"io"
 	"net"
 	"sync"
 	"time"
 
-	"github.com/armon/go-socks5"
 	"golang.org/x/net/proxy"
 
 	"github.com/skycoin/skycoin/src/cipher"
+	"github.com/skycoin/skycoin/src/mesh/go-socks5"
 	"github.com/skycoin/skycoin/src/mesh/messages"
 )
 
 type SocksServer struct {
 	app
-	socksConn net.Conn
+	socksAddress string
+	dialer       proxy.Dialer
+	connections  map[string]net.Conn
 }
 
 func NewSocksServer(meshnet messages.Network, address cipher.PubKey, socksAddress string) (*SocksServer, error) {
@@ -25,6 +27,7 @@ func NewSocksServer(meshnet messages.Network, address cipher.PubKey, socksAddres
 	socksServer.lock = &sync.Mutex{}
 	socksServer.timeout = time.Duration(messages.GetConfig().AppTimeout)
 	socksServer.socksAddress = socksAddress
+	socksServer.connections = map[string]net.Conn{}
 
 	conn, err := meshnet.NewConnection(address)
 	if err != nil {
@@ -38,6 +41,9 @@ func NewSocksServer(meshnet messages.Network, address cipher.PubKey, socksAddres
 		return nil, err
 	}
 
+	go socksServer.Serve()
+	fmt.Println("ready to accept requests")
+
 	return socksServer, nil
 }
 
@@ -45,19 +51,60 @@ func (self *SocksServer) Consume(msg []byte) {
 	appMsg := messages.AppMessage{}
 	err := messages.Deserialize(msg, &appMsg)
 	if err != nil {
-		fmt.Printf("Cannot consume a message: %s\n", err.Error())
+		fmt.Printf("Cannot deserialize application message: %s\n", err.Error())
 		return
 	}
 
-	originalRequest := appMsg.Payload
+	socksMessageS := appMsg.Payload
+	socksMessage := messages.SocksMessage{}
 
-	n, err := self.socksConn.Write(originalRequest)
-	fmt.Println("sent to SOCKS:", n)
+	err = messages.Deserialize(socksMessageS, &socksMessage)
+	if err != nil {
+		fmt.Printf("Cannot deserialize socks message: %s\n", err.Error())
+		return
+	}
+
+	remoteAddr := socksMessage.RemoteAddr
+	needClose := socksMessage.NeedClose
+
+	socksConn, ok := self.connections[remoteAddr]
+
+	if !ok && needClose {
+		return
+	}
+
+	if !ok && !needClose {
+		socksConn, err = self.dialer.Dial("tcp", self.socksAddress)
+		if err != nil {
+			fmt.Println("Cannot dial to socks server on ", self.socksAddress)
+			return
+		}
+		self.connections[remoteAddr] = socksConn
+		go self.getFromSocks(socksConn, remoteAddr)
+	}
+
+	if needClose {
+		fmt.Printf("Closing connection %s according to a signal from client\n", remoteAddr)
+		socksConn.Close()
+		delete(self.connections, remoteAddr)
+		return
+	}
+
+	data := socksMessage.Data
+	_, err = socksConn.Write(data)
+	if err != nil {
+		fmt.Println("Cannot write to connection:", socksConn.LocalAddr().String(), socksConn.RemoteAddr().String())
+		closingMessage := messages.SocksMessage{
+			nil,
+			remoteAddr,
+			true,
+		}
+		closingMessageS := messages.Serialize(messages.MsgSocksMessage, closingMessage)
+		self.Send(closingMessageS)
+	}
 }
 
 func (self *SocksServer) Serve() {
-
-	socksAddress := self.socksAddress
 
 	go func() {
 		conf := &socks5.Config{}
@@ -66,46 +113,48 @@ func (self *SocksServer) Serve() {
 			panic(err)
 		}
 
-		if err := server.ListenAndServe("tcp", socksAddress); err != nil {
+		if err := server.ListenAndServe("tcp", self.socksAddress); err != nil {
 			panic(err)
 		}
 	}()
-
-	dialer, err := proxy.SOCKS5("tcp", socksAddress, nil, proxy.Direct)
+	dialer, err := proxy.SOCKS5("tcp", self.socksAddress, nil, proxy.Direct)
 	if err != nil {
 		panic(err)
 	}
-
-	for {
-		socksConn, err := dialer.Dial("tcp", socksAddress)
-		if err == nil {
-			self.socksConn = socksConn
-			break
-		}
-	}
-
-	fmt.Println("ready to accept requests")
-	self.getFromSocks()
+	self.dialer = dialer
 }
 
-func (self *SocksServer) getFromSocks() {
+func (self *SocksServer) getFromSocks(conn net.Conn, remoteAddr string) {
 
 	for {
-		fmt.Println("Waiting from SOCKS")
 		buffer := make([]byte, config.SocksPacketSize)
-		n, err := self.socksConn.Read(buffer)
+		n, err := conn.Read(buffer)
 		if err != nil {
-			//			if err != io.EOF {
-			panic(err)
-			//			}
+			return
+			if err == io.EOF {
+				continue
+			} else {
+				conn.Close()
+				return
+			}
 		}
-		fmt.Println("got from SOCKS:", n)
-		response := &messages.AppMessage{
-			0,
-			false,
+
+		socksMessage := messages.SocksMessage{
 			buffer[:n],
+			remoteAddr,
+			false,
 		}
-		responseSerialized := messages.Serialize(messages.MsgAppMessage, response)
-		self.send(responseSerialized)
+		socksMessageS := messages.Serialize(messages.MsgSocksMessage, socksMessage)
+		self.Send(socksMessageS)
 	}
+}
+
+func (self *SocksServer) Send(data []byte) {
+	message := &messages.AppMessage{
+		0,
+		false,
+		data,
+	}
+	messageS := messages.Serialize(messages.MsgAppMessage, message)
+	self.send(messageS)
 }

a3aad03fd67e6baaf133ff1c6b3b2aa027f1cea2
iketheadore <heaven.mao@gmail.com>
2017-04-19 16:48:12 +0800

    Add Help methods in bucket

--- src/visor/bucket/bucket.go
+++ src/visor/bucket/bucket.go
@@ -9,7 +9,7 @@ type Bucket struct {
 	db   *bolt.DB
 }
 
-// NewBucket create bucket of specific name.
+// New create bucket of specific name.
 func New(name []byte, db *bolt.DB) (*Bucket, error) {
 	err := db.Update(func(tx *bolt.Tx) error {
 		if _, err := tx.CreateBucketIfNotExists(name); err != nil {
@@ -47,7 +47,6 @@ func (b Bucket) Find(filter func(key, value []byte) bool) []byte {
 		bt := tx.Bucket(b.Name)
 
 		c := bt.Cursor()
-
 		for k, v := c.First(); k != nil; k, v = c.Next() {
 			if filter(k, v) {
 				value = v
@@ -59,15 +58,91 @@ func (b Bucket) Find(filter func(key, value []byte) bool) []byte {
 	return value
 }
 
-// Count return the number of key/value pairs
-// func (b Bucket) Count() uint64 {
-// 	var count uint64
-// 	b.db.View(func(tx *bolt.Tx) error {
-// 		bt := tx.Bucket(b.Name)
-// 		return bt.ForEach(func(k, v []byte) error {
-// 			count++
-// 			return nil
-// 		})
-// 	})
-// 	return count
-// }
+// Update use callback func to update the value of given key
+func (b *Bucket) Update(key []byte, f func([]byte) ([]byte, error)) error {
+	return b.db.Update(func(tx *bolt.Tx) error {
+		// get the value of given key
+		bkt := tx.Bucket(b.Name)
+		v := bkt.Get(key)
+		var err error
+		v, err = f(v)
+		if err != nil {
+			return err
+		}
+
+		return bkt.Put(key, v)
+	})
+}
+
+// Delete removes value of given key
+func (b *Bucket) Delete(key []byte) error {
+	return b.db.Update(func(tx *bolt.Tx) error {
+		return tx.Bucket(b.Name).Delete(key)
+	})
+}
+
+// GetAll returns all values
+func (b *Bucket) GetAll() map[interface{}][]byte {
+	values := map[interface{}][]byte{}
+	b.db.View(func(tx *bolt.Tx) error {
+		bkt := tx.Bucket(b.Name)
+		c := bkt.Cursor()
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			values[string(k)] = v
+		}
+		return nil
+	})
+	return values
+}
+
+// RangeUpdate updates range of the values
+func (b *Bucket) RangeUpdate(f func(k, v []byte) ([]byte, error)) error {
+	return b.db.Update(func(tx *bolt.Tx) error {
+		bkt := tx.Bucket(b.Name)
+		c := bkt.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			v, err := f(k, v)
+			if err != nil {
+				return err
+			}
+
+			if err := bkt.Put(k, v); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+}
+
+// IsExist check if the value exist of the given key
+func (b *Bucket) IsExist(k []byte) bool {
+	var exist bool
+	b.db.View(func(tx *bolt.Tx) error {
+		v := tx.Bucket(b.Name).Get(k)
+		if v != nil {
+			exist = true
+		}
+		return nil
+	})
+	return exist
+}
+
+// ForEach iterate the whole bucket
+func (b *Bucket) ForEach(f func(k, v []byte) error) error {
+	return b.db.View(func(tx *bolt.Tx) error {
+		return tx.Bucket(b.Name).ForEach(f)
+	})
+}
+
+// Len returns the number of key value pairs
+func (b *Bucket) Len() (len int) {
+	b.db.View(func(tx *bolt.Tx) error {
+		c := tx.Bucket(b.Name).Cursor()
+		for k, _ := c.First(); k != nil; k, _ = c.Next() {
+			len++
+		}
+		return nil
+	})
+	return
+}

e8572436462c156349ffdfddb4557194b5743dad
iketheadore <2589926+iketheadore@users.noreply.github.com>
2017-08-25 12:29:09 +0800

    Separate notes from wallet
    The notes is not used for now, low priority to refactor it

--- src/gui/wallet.go
+++ src/gui/wallet.go
@@ -4,7 +4,6 @@ package gui
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"strconv"
 	"strings"
 
@@ -17,41 +16,6 @@ import (
 
 	wh "github.com/skycoin/skycoin/src/util/http" //http,json helpers
 )
-
-// NotesRPC note rpc
-type NotesRPC struct {
-	Notes           wallet.Notes
-	WalletDirectory string
-}
-
-// Ng global note
-var Ng *NotesRPC
-
-// InitWalletRPC init wallet rpc
-func InitWalletRPC(walletDir string, options ...wallet.Option) {
-	Ng = NewNotesRPC(walletDir)
-}
-
-// NewNotesRPC new notes rpc
-func NewNotesRPC(walletDir string) *NotesRPC {
-	rpc := &NotesRPC{}
-	if err := os.MkdirAll(walletDir, os.FileMode(0700)); err != nil {
-		logger.Panicf("Failed to create notes directory %s: %v", walletDir, err)
-	}
-	rpc.WalletDirectory = walletDir
-	w, err := wallet.LoadNotes(rpc.WalletDirectory)
-	if err != nil {
-		logger.Panicf("Failed to load all notes: %v", err)
-	}
-	wallet.CreateNoteFileIfNotExist(walletDir)
-	rpc.Notes = w
-	return rpc
-}
-
-// GetNotesReadable returns readable notes
-func (nt *NotesRPC) GetNotesReadable() wallet.ReadableNotes {
-	return nt.Notes.ToReadable()
-}
 
 // SpendResult represents the result of spending
 type SpendResult struct {
@@ -159,22 +123,6 @@ func walletSpendHandler(gateway *daemon.Gateway) http.HandlerFunc {
 		}
 
 		wh.SendOr404(w, ret)
-	}
-}
-
-// Create a wallet Name is set by creation date
-func notesCreate(gateway *daemon.Gateway) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		logger.Info("API request made to create a note")
-		note := r.FormValue("note")
-		transactionID := r.FormValue("transaction_id")
-		newNote := wallet.Note{
-			TxID:  transactionID,
-			Value: note,
-		}
-		Ng.Notes.SaveNote(Ng.WalletDirectory, newNote)
-		rlt := Ng.GetNotesReadable()
-		wh.SendOr500(w, rlt)
 	}
 }
 
@@ -317,15 +265,6 @@ func walletGet(gateway *daemon.Gateway) http.HandlerFunc {
 		}
 
 		wh.SendOr404(w, wlt)
-	}
-}
-
-// Returns a wallet by ID if GET.  Creates or updates a wallet if POST.
-func notesHandler(gateway *daemon.Gateway) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		//ret := wallet.Wallets.ToPublicReadable()
-		ret := Ng.GetNotesReadable()
-		wh.SendOr404(w, ret)
 	}
 }
 
@@ -475,7 +414,4 @@ func RegisterWalletHandlers(mux *http.ServeMux, gateway *daemon.Gateway) {
 	mux.Handle("/wallet/newSeed", newWalletSeed(gateway))
 
 	// generate wallet seed
-	mux.Handle("/notes", notesHandler(gateway))
-
-	mux.Handle("/notes/create", notesCreate(gateway))
 }

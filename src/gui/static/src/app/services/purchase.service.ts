4811d2f4b04e6d4d7862e2a559a77ef9ca770a10
Unknown <xano3425@gmail.com>
2017-09-05 19:45:30 +0200

    Add behaviour subject for purchaseOrders

--- src/gui/static/src/app/services/purchase.service.ts
+++ src/gui/static/src/app/services/purchase.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class PurchaseService {
 
+  private purchaseOrders: Subject<any[]> = new BehaviorSubject<any[]>([]);
+
+  all() {
+    return this.purchaseOrders.asObservable();
+  }
 }

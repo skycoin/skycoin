1607afff19b353dc6d01e2a50024e5541558352a
Unknown <xano3425@gmail.com>
2017-09-05 19:48:02 +0200

    Add generatic purchaseOrder getter and setter

--- src/gui/static/src/app/services/purchase.service.ts
+++ src/gui/static/src/app/services/purchase.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class PurchaseService {
 
   private purchaseOrders: Subject<any[]> = new BehaviorSubject<any[]>([]);
+  // private purchaseUrl: string = 'https://event.skycoin.net/api/';
+  private purchaseUrl: string = 'http://localhost:7071/api';
+  // private purchaseUrl: string = '/teller/';
 
   constructor(
     private http: Http,
   ) {
+    this.retrievePurchaseOrders();
   }
 
   all() {
@@ -24,5 +29,17 @@ export class PurchaseService {
   private post(url, parameters = {}) {
     return this.http.post(this.purchaseUrl + url, parameters)
       .map((res: any) => res.json())
+  }
+
+  private retrievePurchaseOrders() {
+    const orders = JSON.parse(window.localStorage.getItem('purchaseOrders'));
+    if (orders) {
+      this.purchaseOrders.next(orders);
+    }
+  }
+
+  private updatePurchaseOrders(collection: any[]) {
+    this.purchaseOrders.next(collection);
+    window.localStorage.setItem('purchaseOrders', JSON.stringify(collection));
   }
 }

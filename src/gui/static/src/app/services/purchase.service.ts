8629e7a43209103f1f1bed052f342126ddf7572a
Unknown <xano3425@gmail.com>
2017-09-05 19:46:44 +0200

    Add get set from http to purchase service

--- src/gui/static/src/app/services/purchase.service.ts
+++ src/gui/static/src/app/services/purchase.service.ts
@@ -7,7 +7,22 @@ export class PurchaseService {
 
   private purchaseOrders: Subject<any[]> = new BehaviorSubject<any[]>([]);
 
+  constructor(
+    private http: Http,
+  ) {
+  }
+
   all() {
     return this.purchaseOrders.asObservable();
+  }
+
+  private get(url) {
+    return this.http.get(this.purchaseUrl + url)
+      .map((res: any) => res.json())
+  }
+
+  private post(url, parameters = {}) {
+    return this.http.post(this.purchaseUrl + url, parameters)
+      .map((res: any) => res.json())
   }
 }

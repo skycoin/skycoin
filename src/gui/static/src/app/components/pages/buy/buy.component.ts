44d92589f7169f64df259ae4291d347f01cb49b4
Unknown <xano3425@gmail.com>
2017-09-05 20:16:50 +0200

    Add generate button

--- src/gui/static/src/app/components/pages/buy/buy.component.ts
+++ src/gui/static/src/app/components/pages/buy/buy.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MdDialog, MdDialogConfig } from '@angular/material';
+import { AddDepositAddressComponent } from './add-deposit-address/add-deposit-address.component';
 
 @Component({
   selector: 'app-buy',
   templateUrl: './buy.component.html',
   styleUrls: ['./buy.component.css']
 })
-export class BuyComponent implements OnInit {
+export class BuyComponent {
 
   orders = [];
+  constructor(
+    public purchaseService: PurchaseService,
+    private dialog: MdDialog,
+  ) { }
+
+  addDepositAddress() {
+    const config = new MdDialogConfig();
+    config.width = '500px';
+    this.dialog.open(AddDepositAddressComponent, config);
+  }
 }

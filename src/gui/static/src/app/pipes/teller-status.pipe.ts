c310733ccd0f51302faac7b638639d089ca7aaab
Xa No <xano3425@gmail.com>
2017-09-17 22:35:23 +0200

    Process textual changes

--- src/gui/static/src/app/pipes/teller-status.pipe.ts
+++ src/gui/static/src/app/pipes/teller-status.pipe.ts
@@ -8,9 +8,9 @@ export class TellerStatusPipe implements PipeTransform {
   transform(value: any): any {
     switch (value) {
       case 'waiting_deposit':
-        return 'Waiting for deposit';
+        return 'Waiting for Bitcoin deposit';
       case 'waiting_send':
-        return 'Waiting for sending out';
+        return 'Waiting to send Skycoin';
       case 'waiting_confirm':
         return 'Waiting for confirmation';
       case 'done':

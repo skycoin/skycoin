55c133e186b9279a67ee1fba46e26a83580cd587
gz-c <token@protonmail.com>
2017-10-30 12:12:06 +0800

    Return error from TransactionDeserialize, dont panic

--- src/api/webrpc/client.go
+++ src/api/webrpc/client.go
@@ -2,25 +2,30 @@ package webrpc
 
 import (
 	"bytes"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
 
+	"github.com/skycoin/skycoin/src/coin"
 	"github.com/skycoin/skycoin/src/visor"
 )
 
-var ErrJSONUnmarshal = errors.New("json unmarshal failed")
+// ErrJSONUnmarshal is returned if JSON unmarshal fails
+var ErrJSONUnmarshal = errors.New("JSON unmarshal failed")
 
+// Client is an RPC client
 type Client struct {
 	Addr     string
-	reqIdCtr int
+	reqIDCtr int
 }
 
+// Do makes an RPC request
 func (c *Client) Do(obj interface{}, method string, params interface{}) error {
-	c.reqIdCtr++
-	req, err := NewRequest(method, params, strconv.Itoa(c.reqIdCtr))
+	c.reqIDCtr++
+	req, err := NewRequest(method, params, strconv.Itoa(c.reqIDCtr))
 	if err != nil {
 		return err
 	}
@@ -34,9 +39,11 @@ func (c *Client) Do(obj interface{}, method string, params interface{}) error {
 		return rsp.Error
 	}
 
-	return decodeJson(rsp.Result, obj)
+	return decodeJSON(rsp.Result, obj)
 }
 
+// GetUnspentOutputs returns unspent outputs for a set of addresses
+// TODO -- what is the difference between this and GetAddressUxOuts?
 func (c *Client) GetUnspentOutputs(addrs []string) (*OutputsResult, error) {
 	outputs := OutputsResult{}
 	if err := c.Do(&outputs, "get_outputs", addrs); err != nil {
@@ -46,8 +53,8 @@ func (c *Client) GetUnspentOutputs(addrs []string) (*OutputsResult, error) {
 	return &outputs, nil
 }
 
-// Returns TxId
-func (c *Client) InjectTransaction(rawtx string) (string, error) {
+// InjectTransactionString injects a hex-encoded transaction string to the network
+func (c *Client) InjectTransactionString(rawtx string) (string, error) {
 	params := []string{rawtx}
 	rlt := TxIDJson{}
 
@@ -58,6 +65,14 @@ func (c *Client) InjectTransaction(rawtx string) (string, error) {
 	return rlt.Txid, nil
 }
 
+// InjectTransaction injects a *coin.Transaction to the network
+func (c *Client) InjectTransaction(tx *coin.Transaction) (string, error) {
+	d := tx.Serialize()
+	rawTx := hex.EncodeToString(d)
+	return c.InjectTransactionString(rawTx)
+}
+
+// GetStatus returns status info for a skycoin node
 func (c *Client) GetStatus() (*StatusResult, error) {
 	status := StatusResult{}
 	if err := c.Do(&status, "get_status", nil); err != nil {
@@ -67,6 +82,7 @@ func (c *Client) GetStatus() (*StatusResult, error) {
 	return &status, nil
 }
 
+// GetTransactionByID returns a transaction given a txid
 func (c *Client) GetTransactionByID(txid string) (*TxnResult, error) {
 	txn := TxnResult{}
 	if err := c.Do(&txn, "get_transaction", []string{txid}); err != nil {
@@ -76,6 +92,8 @@ func (c *Client) GetTransactionByID(txid string) (*TxnResult, error) {
 	return &txn, nil
 }
 
+// GetAddressUxOuts returns unspent outputs for a set of addresses
+// TODO -- what is the difference between this and GetUnspentOutputs?
 func (c *Client) GetAddressUxOuts(addrs []string) ([]AddrUxoutResult, error) {
 	uxouts := []AddrUxoutResult{}
 	if err := c.Do(&uxouts, "get_address_uxouts", addrs); err != nil {
@@ -85,6 +103,7 @@ func (c *Client) GetAddressUxOuts(addrs []string) ([]AddrUxoutResult, error) {
 	return uxouts, nil
 }
 
+// GetBlocks returns a range of blocks
 func (c *Client) GetBlocks(start, end uint64) (*visor.ReadableBlocks, error) {
 	param := []uint64{start, end}
 	blocks := visor.ReadableBlocks{}
@@ -96,6 +115,7 @@ func (c *Client) GetBlocks(start, end uint64) (*visor.ReadableBlocks, error) {
 	return &blocks, nil
 }
 
+// GetBlocksBySeq returns blocks for a set of block sequences (heights)
 func (c *Client) GetBlocksBySeq(ss []uint64) (*visor.ReadableBlocks, error) {
 	blocks := visor.ReadableBlocks{}
 
@@ -106,6 +126,7 @@ func (c *Client) GetBlocksBySeq(ss []uint64) (*visor.ReadableBlocks, error) {
 	return &blocks, nil
 }
 
+// GetLastBlocks returns the last n blocks
 func (c *Client) GetLastBlocks(n uint64) (*visor.ReadableBlocks, error) {
 	param := []uint64{n}
 	blocks := visor.ReadableBlocks{}
@@ -135,7 +156,7 @@ func Do(req *Request, rpcAddress string) (*Response, error) {
 	return &res, nil
 }
 
-func decodeJson(data []byte, obj interface{}) error {
+func decodeJSON(data []byte, obj interface{}) error {
 	if err := json.NewDecoder(bytes.NewBuffer(data)).Decode(obj); err != nil {
 		return ErrJSONUnmarshal
 	}

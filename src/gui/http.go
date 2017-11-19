1919822ac1915f00182e8cba2a0181e214288cea
samos <hanyh2004@gmail.com>
2017-11-19 12:07:28 +0800

    add log

--- src/gui/http.go
+++ src/gui/http.go
@@ -1,12 +1,15 @@
 package gui
 
 import (
+	"bytes"
 	"crypto/tls"
 	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/skycoin/skycoin/src/cipher"
@@ -119,6 +122,8 @@ func NewGUIMux(appLoc string, daemon *daemon.Daemon) *http.ServeMux {
 		}
 		mux.Handle(route, http.FileServer(http.Dir(appLoc)))
 	}
+
+	mux.HandleFunc("/logs", getLogsHandler(&daemon.LogBuff))
 
 	mux.HandleFunc("/version", versionHandler(daemon.Gateway))
 
@@ -250,5 +255,86 @@ func versionHandler(gateway *daemon.Gateway) http.HandlerFunc {
 		}
 
 		wh.SendOr404(w, gateway.GetBuildInfo())
+	}
+}
+
+/*
+attrActualLog remove color char in log
+origin: "\u001b[36m[skycoin.daemon:DEBUG] Trying to connect to 47.88.33.156:6000\u001b[0m",
+*/
+func attrActualLog(logInfo string) string {
+	//return logInfo
+	var actualLog string
+	actualLog = logInfo
+	if strings.HasPrefix(logInfo, "[skycoin") {
+		if strings.Contains(logInfo, "\u001b") {
+			actualLog = logInfo[0 : len(logInfo)-5]
+		}
+	} else {
+		if len(logInfo) > 5 {
+			if strings.Contains(logInfo, "\u001b") {
+				actualLog = logInfo[5 : len(logInfo)-5]
+			}
+		} else {
+			actualLog = "here:" + actualLog
+		}
+	}
+	return actualLog
+}
+func getLogsHandler(logbuf *bytes.Buffer) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			wh.Error405(w)
+			return
+		}
+
+		var err error
+		defaultLineNum := 10 // default line numbers
+		linenum := defaultLineNum
+		if lines := r.FormValue("lines"); lines != "" {
+			linenum, err = strconv.Atoi(lines)
+			if err != nil {
+				linenum = defaultLineNum
+			}
+		}
+		keyword := r.FormValue("include")
+		excludeKeyword := r.FormValue("exclude")
+		regstr := r.FormValue("reg")
+		logs := []string{}
+
+		reg, err := regexp.Compile(regstr)
+		if err != nil {
+			logger.Info("reg express error:%v", err)
+			regstr = ""
+		}
+
+		for {
+			logInfo, err := logbuf.ReadString(byte('\n'))
+			if err != nil {
+				//logger.Info("read logbuffer err %v", err) EOF
+				break
+			}
+			if excludeKeyword != "" && strings.Contains(logInfo, excludeKeyword) {
+				continue
+			}
+			if keyword != "" && !strings.Contains(logInfo, keyword) {
+				continue
+			}
+
+			if regstr != "" {
+				if reg.MatchString(logInfo) {
+					logs = append(logs, attrActualLog(logInfo))
+				}
+			} else {
+				logs = append(logs, attrActualLog(logInfo))
+			}
+
+			if len(logs) >= linenum || len(logInfo) == 0 {
+				logger.Debug("log logbuffer size %d", len(logs))
+				break
+			}
+		}
+
+		wh.SendOr404(w, logs)
 	}
 }

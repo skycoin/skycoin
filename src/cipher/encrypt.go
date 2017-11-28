7ae99148c1c85621d12ec7a8f73f6c4eaaf61508
iketheadore <2589926+iketheadore@users.noreply.github.com>
2017-11-28 12:26:29 +0800

    Add data hash for password verifaction

--- src/cipher/encrypt.go
+++ src/cipher/encrypt.go
@@ -17,20 +17,26 @@ const (
 )
 
 // Encrypt encrypts the data with password
-// 1> Add 32 bits length prefix to indicate the length of data
-// 2> Split data into 256 bits(32 bytes) blocks (pad to 32 bytes with nulls at end)
-// 3> Each block is encrypted by XORing the unencrypted block with SHA256(SHA256(password), SHA256(index, SHA256(nonce))
+// 1> SHA256 the data
+// 2> Add 32 bits length prefix to indicate the length of data, including the data hash
+// 3> Split data into 256 bits(32 bytes) blocks (pad to 32 bytes with nulls at end)
+// 4> Each block is encrypted by XORing the unencrypted block with SHA256(SHA256(password), SHA256(index, SHA256(nonce))
 // 	  - index is 0 for the first block of 32 bytes, 1 for the second block of 32 bytes, 2 for third block
-// 4> SHA256 the nonce with comma seperated, hex encoded blocks of 32 bytes(256 bits)
-// 5> Encode <checksum(32 bytes)><nonce(32 bytes)><block0.Hex(), block1.Hex()...> with base64
+// 5> SHA256 the nonce with comma seperated, hex encoded blocks of 32 bytes(256 bits)
+// 6> Encode <checksum(32 bytes)><nonce(32 bytes)><block0.Hex(), block1.Hex()...> with base64
 func Encrypt(data []byte, password []byte) ([]byte, error) {
 	// set data length prefix
-	prefix := make([]byte, lenPrefixSize)
-	binary.PutUvarint(prefix, uint64(len(data)))
-	pdata := append(prefix, data...)
+	dataHash := SumSHA256(data)
+	length := make([]byte, lenPrefixSize)
+	binary.PutUvarint(length, uint64(len(data)+32)) // the length including the data hash
+	var pbuf bytes.Buffer
+	pbuf.Write(length)
+	pbuf.Write(dataHash[:])
+	pbuf.Write(data)
+	pdata := pbuf.Bytes()
 
 	// split the data into 256 bit blocks(pad with null to 32 bytes)
-	l := len(pdata)
+	l := len(pdata) // hash length + data length
 	n := l / blockSize
 	var blocks [][blockSize]byte
 	for i := 0; i < n; i++ {
@@ -131,14 +137,21 @@ func Decrypt(data []byte, password []byte) ([]byte, error) {
 	buf = bytes.NewBuffer(decodeData)
 	l, err := binary.ReadUvarint(bytes.NewReader(decodeData[:lenPrefixSize]))
 	if err != nil {
-		return nil, fmt.Errorf("read prefix length failed: %v", err)
+		return nil, fmt.Errorf("invalid password, read prefix length failed: %v", err)
 	}
 
-	if l > uint64(len(decodeData[4:])) {
-		return nil, fmt.Errorf("prefix length > data length")
+	if l > uint64(len(decodeData[lenPrefixSize:])) {
+		return nil, errors.New("invalid password, prefix length > data length")
 	}
 
-	return decodeData[lenPrefixSize : l+lenPrefixSize], nil
+	var dataHash SHA256
+	copy(dataHash[:], decodeData[lenPrefixSize:lenPrefixSize+32])
+	rawData := decodeData[lenPrefixSize+32 : lenPrefixSize+l]
+	if dataHash.Hex() != SumSHA256(rawData).Hex() {
+		return nil, errors.New("decrypt data failed, invalid password")
+	}
+
+	return rawData, nil
 }
 
 // hash(password, hash(index, hash(nonce)))

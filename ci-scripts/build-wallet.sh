c91d3f6c8a6b04c257d3b62bc8e44c5f1a3724bb
iketheadore <2589926+iketheadore@users.noreply.github.com>
2017-12-12 17:38:38 +0800

    Add linux/arm build in travis

--- ci-scripts/build-wallet.sh
+++ ci-scripts/build-wallet.sh
@@ -3,7 +3,7 @@
 echo "start to build wallets..."
 pushd "electron"
 yarn
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then ./build.sh 'linux/amd64' ;fi
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then ./build.sh 'linux/amd64 linux/arm' ;fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then ./build.sh 'darwin/amd64' ;fi
 ls release/
 popd

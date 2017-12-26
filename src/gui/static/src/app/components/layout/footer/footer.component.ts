c433da79db0045617599abe67c2d9fc2d379b6e2
montycrypto <cryptomonty@gmail.com>
2017-12-26 21:31:49 +0100

    Restyle footer

--- src/gui/static/src/app/components/layout/footer/footer.component.ts
+++ src/gui/static/src/app/components/layout/footer/footer.component.ts
@@ -1,19 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(
+    private router: Router,
+  ) { }
 
   openWalletsPage() {
-    // this.router(WalletsPage);
+    this.router.navigate(['/wallets']);
   }
 
   openSendPage() {

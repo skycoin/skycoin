c4cddf3b7742f123e27e5f239c7009962561787e
montycrypto <cryptomonty@gmail.com>
2017-12-28 19:57:27 +0100

    Implement new design for Blockchain

--- src/gui/static/src/app/components/layout/header/top-bar/top-bar.component.ts
+++ src/gui/static/src/app/components/layout/header/top-bar/top-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent {
+  @Input() title: string;
 
   get showBack() {
     return this.router.url !== '/wallets';

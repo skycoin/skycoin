05d7ff8b686bf53f36c6d51a13dacbcb099db149
iketheadore <2589926+iketheadore@users.noreply.github.com>
2018-01-01 18:22:34 +0800

    Refactor the cipher.Encrypt and cipher.Decrypt
    Remove the base64 encoding during the encryption and decryption
    
    Change the data format from <length><hash><data> to <hash><length><data>, the previous format, if we decrypt the data with wrong password, the length could be any value, and we can't check if the password is valid before getting the hash and data throught the length. After we change to the second format, we can check if the hash is matched directly, and if it's not matched, it's wrong password, if matched, then we could read out length, and get the data.

--- src/cipher/encrypt.go
+++ src/cipher/encrypt.go
@@ -2,17 +2,21 @@ package cipher
 
 import (
 	"bytes"
-	"encoding/base64"
 	"encoding/binary"
 	"errors"
-	"strings"
+	"fmt"
+	"io"
 )
 
 const (
 	// the data size of each block
 	blockSize = 32 // 32 bytes
+	// nonce data size
+	nonceSize = 32 // 32 bytes
+	// checksum data size
+	checksumSize = 32 // 32 bytes
 	// the data length prefix size
-	lenPrefixSize = 4 // 4 bytes
+	prefixLengthSize = 4 // 4 bytes
 )
 
 var (
@@ -21,58 +25,66 @@ var (
 )
 
 // Encrypt encrypts the data with password
-// 1> SHA256 the data
-// 2> Add 32 bits length prefix to indicate the length of data, including the data hash
-// 3> Split data into 256 bits(32 bytes) blocks (pad to 32 bytes with nulls at end)
+//
+// 1> Add 32 bits length prefix to indicate the length of data. <length(4 bytes)><data>
+// 2> Pad the length + data to 32 bytes will nulls at end
+// 2> SHA256(<length(4 bytes)><data><padding>) and prefix the hash. <hash(32 bytes)><length(4 bytes)><data><padding>
+// 3> Split the whole data(hash+length+data+padding) into 256 bits(32 bytes) blocks
 // 4> Each block is encrypted by XORing the unencrypted block with SHA256(SHA256(password), SHA256(index, SHA256(nonce))
 // 	  - index is 0 for the first block of 32 bytes, 1 for the second block of 32 bytes, 2 for third block
-// 5> SHA256 the nonce with comma seperated, hex encoded blocks of 32 bytes(256 bits)
-// 6> Encode <checksum(32 bytes)><nonce(32 bytes)><block0.Hex(), block1.Hex()...> with base64
+// 5> Prefix nonce and SHA256 the nonce with blocks to get checksum, and prefix the checksum
+// 6> Finally, the data format is: <checksum(32 bytes)><nonce(32 bytes)><block0.Hex(), block1.Hex()...>
 func Encrypt(data []byte, password []byte) ([]byte, error) {
 	// set data length prefix
-	dataHash := SumSHA256(data)
-	length := make([]byte, lenPrefixSize)
-	binary.PutUvarint(length, uint64(len(data)+32)) // the length including the data hash
+	length := make([]byte, prefixLengthSize)
+	binary.PutUvarint(length, uint64(len(data))) // the length
 	var pbuf bytes.Buffer
 	pbuf.Write(length)
-	pbuf.Write(dataHash[:])
 	pbuf.Write(data)
 	pdata := pbuf.Bytes()
 
-	// split the data into 256 bit blocks(pad with null to 32 bytes)
-	l := len(pdata) // hash length + data length
+	// Pad length + data with null to 32 bytes
+	l := len(pdata) // hash + length + data
 	n := l / blockSize
-	var blocks [][blockSize]byte
+	m := l % blockSize
+	if m > 0 {
+		paddingNull := make([]byte, blockSize-m)
+		pdata = append(pdata, paddingNull...)
+		n++
+	}
+
+	// Hash(length+data+padding)
+	dataHash := SumSHA256(pdata)
+
+	// Initialize blocks with data hash
+	blocks := [][blockSize]byte{dataHash}
 	for i := 0; i < n; i++ {
 		var b [blockSize]byte
 		copy(b[:], pdata[i*blockSize:(i+1)*blockSize])
 		blocks = append(blocks, b)
 	}
 
-	// append last block if exist
-	if l%blockSize > 0 {
-		b := [blockSize]byte{}
-		copy(b[:], pdata[n*blockSize:])
-		blocks = append(blocks, b)
-	}
-
-	nonce := RandByte(blockSize)
-	var encryptedBlocks []string
-	hashPassword := SumSHA256(password)
+	// Generates a nonce
+	nonce := RandByte(nonceSize)
+	// Hash the nonce
 	hashNonce := SumSHA256(nonce)
-	// encode the blocks
+	// Hash the password
+	hashPassword := SumSHA256(password)
+
+	var encryptedData []byte
+	// Encodes the blocks
 	for i := range blocks {
-		// hash(password, hash(index, hash(nonce)))
+		// Hash(password, hash(index, hash(nonce)))
 		h := hashPwdIndexNonce(hashPassword, int64(i), hashNonce)
+		// Converts the block to SHA256
 		bh := SHA256(blocks[i])
 		encryptedHash := bh.Xor(h)
-		// encode the encrypted hash in base64
-		s := base64.StdEncoding.EncodeToString(encryptedHash[:])
-		encryptedBlocks = append(encryptedBlocks, s)
+		encryptedData = append(encryptedData, encryptedHash[:]...)
 	}
 
-	encryptedData := strings.Join(encryptedBlocks, ",")
-	nonceAndDataBytes := append(nonce, []byte(encryptedData)...)
+	// Prefix the nonce
+	nonceAndDataBytes := append(nonce, encryptedData...)
+	// Calculates the checksum
 	checkSum := SumSHA256(nonceAndDataBytes)
 	var buf bytes.Buffer
 	_, err := buf.Write(checkSum[:])
@@ -92,71 +104,101 @@ func Encrypt(data []byte, password []byte) ([]byte, error) {
 func Decrypt(data []byte, password []byte) ([]byte, error) {
 	buf := bytes.NewBuffer(data)
 
-	checkSumBytes := make([]byte, blockSize)
+	// Gets checksum
+	checkSumBytes := make([]byte, checksumSize)
 	n, err := buf.Read(checkSumBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	if n != blockSize {
+	if n != checksumSize {
 		return nil, errors.New("invalid checksum length")
 	}
 
 	var checkSum SHA256
 	copy(checkSum[:], checkSumBytes)
 
-	// verify the checksum
+	// Checks the checksum
 	csh := SumSHA256(buf.Bytes())
 	if csh != checkSum {
 		return nil, errors.New("invalid checksum")
 	}
 
-	nonce := make([]byte, blockSize)
+	// Gets the nonce
+	nonce := make([]byte, nonceSize)
 	n, err = buf.Read(nonce)
 	if err != nil {
 		return nil, err
 	}
 
-	if n != blockSize {
+	if n != nonceSize {
 		return nil, errors.New("invalid nonce length")
 	}
 
-	encryptedBlocks := strings.Split(buf.String(), ",")
 	var decodeData []byte
 	hashPassword := SumSHA256(password)
 	hashNonce := SumSHA256(nonce)
-	for i := range encryptedBlocks {
-		b, err := base64.StdEncoding.DecodeString(encryptedBlocks[i])
-		if err != nil {
-			return nil, err
+	i := 0
+	for {
+		var block SHA256
+		n, err := buf.Read(block[:])
+		if err == io.EOF {
+			break
 		}
 
-		if len(b) != blockSize {
-			return nil, errors.New("invalid encrypted block string")
+		if n != blockSize {
+			return nil, errors.New("invalid block size, must be multiple of 32 bytes")
 		}
 
-		var bh SHA256
-		copy(bh[:], b[:])
-
-		dataHash := bh.Xor(hashPwdIndexNonce(hashPassword, int64(i), hashNonce))
+		// Decodes the block
+		dataHash := block.Xor(hashPwdIndexNonce(hashPassword, int64(i), hashNonce))
 		decodeData = append(decodeData, dataHash[:]...)
+		i++
 	}
 
 	buf = bytes.NewBuffer(decodeData)
-	l, err := binary.ReadUvarint(bytes.NewReader(decodeData[:lenPrefixSize]))
-	if err != nil {
-		return nil, ErrInvalidPassword
-	}
 
-	if l > uint64(len(decodeData[lenPrefixSize:])) || l < 32 {
-		return nil, ErrInvalidPassword
-	}
-
+	// Gets the hash
 	var dataHash SHA256
-	copy(dataHash[:], decodeData[lenPrefixSize:lenPrefixSize+32])
-	rawData := decodeData[lenPrefixSize+32 : lenPrefixSize+l]
-	if dataHash != SumSHA256(rawData) {
+	n, err = buf.Read(dataHash[:])
+	if err != nil {
+		return nil, fmt.Errorf("read data hash failed: %v", err)
+	}
+
+	if n != 32 {
+		return nil, errors.New("read data hash failed: read length != 32")
+	}
+
+	// Checks the hash
+	if dataHash != SumSHA256(buf.Bytes()) {
 		return nil, ErrInvalidPassword
+	}
+
+	// Reads out the prefix length
+	lb := make([]byte, prefixLengthSize)
+	n, err = buf.Read(lb)
+	if err != nil {
+		return nil, err
+	}
+
+	if n != prefixLengthSize {
+		return nil, errors.New("read prefix length failed")
+	}
+
+	l, _ := binary.Uvarint(lb)
+	if l > uint64(buf.Len()) {
+		return nil, errors.New("invalid prefix length")
+	}
+
+	// Reads out the raw data
+	rawData := make([]byte, l)
+	n, err = buf.Read(rawData)
+	if err != nil {
+		return nil, err
+	}
+
+	if uint64(n) != l {
+		return nil, fmt.Errorf("read data failed, expect %d bytes, but get %d bytes", l, n)
 	}
 
 	return rawData, nil

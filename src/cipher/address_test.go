73bd1e798a83c956a1dd6fe725ac0f9d20fef2a9
therealssj <mehul.guptagm@gmail.com>
2018-01-02 16:23:59 +0530

    Refactor base58 tests

--- src/cipher/address_test.go
+++ src/cipher/address_test.go
@@ -1,23 +1,79 @@
 package cipher
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"github.com/therealssj/base58"
 )
+
+const btcAlphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// Generate random string of length n
+func RandStringBytes(n int) []byte {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = btcAlphabet[rand.Intn(len(btcAlphabet))]
+	}
+
+	return b
+}
 
 func TestMustDecodeBase58Address(t *testing.T) {
 	p, _ := GenerateKeyPair()
 	a := AddressFromPubKey(p)
 	require.NoError(t, a.Verify(p))
 
-	require.Panics(t, func() { MustDecodeBase58Address("") })
-	require.Panics(t, func() { MustDecodeBase58Address("cascs") })
+	// Test addresses for invalid length upto 40 bytes
+	// Correct length is 25 ( hash(20), version(1) and checksum(4) )
+	for i := 0; i < 30; i++ {
+		// Test each length 3 times
+		for j := 0; j < 3; j++ {
+			randBytes := RandStringBytes(i)
+			addr, _ := base58.Encode(randBytes)
+			require.Panics(t, func() { MustDecodeBase58Address(addr) })
+		}
+		if i == 24 {
+			i++
+		}
+	}
+
+	// Test length 25 for checksum and version
+	var checksum = make([]byte, 4)
+	for j := 0; j < 50; j++ {
+		randBytes := RandStringBytes(25)
+		copy(checksum[0:4], randBytes[21:25])
+
+		// check invalid version
+		a := Address{}
+		copy(a.Key[0:20], randBytes[0:20])
+		chksum := a.Checksum()
+		copy(randBytes[21:25], chksum[0:4])
+		addr, _ := base58.Encode(randBytes)
+		require.Panics(t, func() { MustDecodeBase58Address(addr) })
+
+		// check valid version
+		randBytes[20] = 0
+		copy(a.Key[0:20], randBytes[0:20])
+		chksum = a.Checksum()
+		copy(randBytes[21:25], chksum[0:4])
+		addr, _ = base58.Encode(randBytes)
+		require.NotPanics(t, func() { MustDecodeBase58Address(addr) })
+
+		// check invalid checksum
+		copy(randBytes[21:25], checksum[0:4])
+		addr, _ = base58.Encode(randBytes)
+		require.Panics(t, func() { MustDecodeBase58Address(addr) })
+	}
+
 	b := a.Bytes()
-	h, _ := base58.Encode(b[:len(b)/2])
-	require.Panics(t, func() { MustDecodeBase58Address(h) })
-	h, _ = base58.Encode(b)
+	h, _ := base58.Encode(b)
 	require.NotPanics(t, func() { MustDecodeBase58Address(h) })
 	a2 := MustDecodeBase58Address(h)
 	require.Equal(t, a, a2)
@@ -48,13 +104,55 @@ func TestDecodeBase58Address(t *testing.T) {
 	a := AddressFromPubKey(p)
 	require.NoError(t, a.Verify(p))
 
-	a2, err := DecodeBase58Address("")
-	require.Error(t, err)
-	a2, err = DecodeBase58Address("cascs")
-	require.Error(t, err)
+	// Test addresses for invalid length upto 40 bytes
+	// Correct length is 25 ( hash(20), version(1) and checksum(4) )
+	for i := 0; i < 30; i++ {
+		// Test each length 3 times
+		for j := 0; j < 3; j++ {
+			randBytes := RandStringBytes(i)
+			addr, _ := base58.Encode(randBytes)
+			_, err := DecodeBase58Address(addr)
+			require.Error(t, err)
+		}
+		if i == 24 {
+			i++
+		}
+	}
+
+	// Test length 25 for checksum and version
+	var checksum = make([]byte, 4)
+	for j := 0; j < 50; j++ {
+		randBytes := RandStringBytes(25)
+		copy(checksum[0:4], randBytes[21:25])
+
+		// check invalid version
+		a := Address{}
+		copy(a.Key[0:20], randBytes[0:20])
+		chksum := a.Checksum()
+		copy(randBytes[21:25], chksum[0:4])
+		addr, _ := base58.Encode(randBytes)
+		_, err := DecodeBase58Address(addr)
+		require.Error(t, err)
+
+		// check valid version
+		randBytes[20] = 0
+		copy(a.Key[0:20], randBytes[0:20])
+		chksum = a.Checksum()
+		copy(randBytes[21:25], chksum[0:4])
+		addr, _ = base58.Encode(randBytes)
+		_, err = DecodeBase58Address(addr)
+		require.NoError(t, err)
+
+		// check invalid checksum
+		copy(randBytes[21:25], checksum[0:4])
+		addr, _ = base58.Encode(randBytes)
+		_, err = DecodeBase58Address(addr)
+		require.Error(t, err)
+	}
+
 	b := a.Bytes()
 	h, _ := base58.Encode(b[:len(b)/2])
-	a2, err = DecodeBase58Address(h)
+	a2, err := DecodeBase58Address(h)
 	require.Error(t, err)
 	h, _ = base58.Encode(b)
 	a2, err = DecodeBase58Address(h)
@@ -140,34 +238,34 @@ func TestAddressString(t *testing.T) {
 	require.Equal(t, a2, a3)
 }
 
-func TestBitcoinAddress1(t *testing.T) {
-	seckey := MustSecKeyFromHex("1111111111111111111111111111111111111111111111111111111111111111")
-	pubkey := PubKeyFromSecKey(seckey)
-	pubkeyStr := "034f355bdcb7cc0af728ef3cceb9615d90684bb5b2ca5f859ab0f0b704075871aa"
-	require.Equal(t, pubkeyStr, pubkey.Hex())
-	bitcoinStr := "1Q1pE5vPGEEMqRcVRMbtBK842Y6Pzo6nK9"
-	bitcoinAddr := BitcoinAddressFromPubkey(pubkey)
-	require.Equal(t, bitcoinStr, bitcoinAddr)
-}
+func TestBitcoinAddresses(t *testing.T) {
+	var hexstr = []string{
+		"1111111111111111111111111111111111111111111111111111111111111111",
+		"dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
+		"47f7616ea6f9b923076625b4488115de1ef1187f760e65f89eb6f4f7ff04b012",
+	}
 
-func TestBitcoinAddress2(t *testing.T) {
-	seckey := MustSecKeyFromHex("dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
-	pubkey := PubKeyFromSecKey(seckey)
-	pubkeyStr := "02ed83704c95d829046f1ac27806211132102c34e9ac7ffa1b71110658e5b9d1bd"
-	require.Equal(t, pubkeyStr, pubkey.Hex())
-	bitcoinStr := "1NKRhS7iYUGTaAfaR5z8BueAJesqaTyc4a"
-	bitcoinAddr := BitcoinAddressFromPubkey(pubkey)
-	require.Equal(t, bitcoinStr, bitcoinAddr)
-}
+	var pubkeys = []string{
+		"034f355bdcb7cc0af728ef3cceb9615d90684bb5b2ca5f859ab0f0b704075871aa",
+		"02ed83704c95d829046f1ac27806211132102c34e9ac7ffa1b71110658e5b9d1bd",
+		"032596957532fc37e40486b910802ff45eeaa924548c0e1c080ef804e523ec3ed3",
+	}
 
-func TestBitcoinAddress3(t *testing.T) {
-	seckey := MustSecKeyFromHex("47f7616ea6f9b923076625b4488115de1ef1187f760e65f89eb6f4f7ff04b012")
-	pubkey := PubKeyFromSecKey(seckey)
-	pubkeyStr := "032596957532fc37e40486b910802ff45eeaa924548c0e1c080ef804e523ec3ed3"
-	require.Equal(t, pubkeyStr, pubkey.Hex())
-	bitcoinStr := "19ck9VKC6KjGxR9LJg4DNMRc45qFrJguvV"
-	bitcoinAddr := BitcoinAddressFromPubkey(pubkey)
-	require.Equal(t, bitcoinStr, bitcoinAddr)
+	var btcaddrs = []string{
+		"1Q1pE5vPGEEMqRcVRMbtBK842Y6Pzo6nK9",
+		"1NKRhS7iYUGTaAfaR5z8BueAJesqaTyc4a",
+		"19ck9VKC6KjGxR9LJg4DNMRc45qFrJguvV",
+	}
+
+	for i := range hexstr {
+		seckey := MustSecKeyFromHex(hexstr[i])
+		pubkey := PubKeyFromSecKey(seckey)
+		pubkeyStr := pubkeys[i]
+		require.Equal(t, pubkeyStr, pubkey.Hex())
+		bitcoinStr := btcaddrs[i]
+		bitcoinAddr := BitcoinAddressFromPubkey(pubkey)
+		require.Equal(t, bitcoinStr, bitcoinAddr)
+	}
 }
 
 func TestBitcoinWIPRoundTrio(t *testing.T) {

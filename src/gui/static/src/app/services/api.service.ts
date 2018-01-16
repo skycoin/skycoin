46db4d044cec51910b322eb98b9cd8c51b5636ed
Samuel Visscher <samuel@visscher.io>
2018-01-16 18:55:52 +0100

    fix css, js and naming issues

--- src/gui/static/src/app/services/api.service.ts
+++ src/gui/static/src/app/services/api.service.ts
@@ -9,8 +9,8 @@ import { Address, GetWalletsResponseEntry, GetWalletsResponseWallet, PostWalletN
 @Injectable()
 export class ApiService {
 
-  // private url = 'http://127.0.0.1:6420/'; // production
-  private url = '/api/'; // test
+  private url = 'http://127.0.0.1:6420/'; // production
+  // private url = '/api/'; // test
 
   constructor(
     private http: Http,

bfcafbe8ec685ca0665d71634bb5893028ee6930
Mauricio López-Quintana Conesa <dev0005@simelo.tech>
2018-01-17 15:24:56 -0500

    [docker] [mainnet] refs #827 Makes dockerfile from template

--- docker/images/testnet/testnet.go
+++ docker/images/testnet/testnet.go
@@ -2,18 +2,19 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"os"
 	"os/exec"
 	"path"
+	"strings"
+	"text/template"
 )
 
 // DockerParameters Dockerfile parameters
 type DockerParameters struct {
-	Execution    string
-	BuildContext string
-	GitCommit    string
+	SkyCoinParameters string
+	BuildContext      string
+	GitCommit         string
 }
 
 // GetCurrentGitCommit returns the current git commit SHA
@@ -33,28 +34,54 @@ func GetCurrentGitCommit() string {
 	return firstSix
 }
 
-// CreateDockerfiles makes the Dockerfiles needed to build the images
+// CreateDockerFile makes the Dockerfiles needed to build the images
 // for the testnet
-func CreateDockerfiles() {
-	gitCommit := GetCurrentGitCommit()
-	parameters := DockerParameters{
-		Execution:    "run",
-		BuildContext: "run",
-		GitCommit:    gitCommit,
-	}
+func CreateDockerFile(parameters DockerParameters) {
 	dockerfileTemplate := path.Join("templates", "Dockerfile")
 	_, err := os.Stat(dockerfileTemplate)
 	if err != nil {
 		log.Print(err)
 		return
 	}
+	f, err := os.Create("Dockerfile")
+	if err != nil {
+		log.Print(err)
+		return
+	}
 	buildTemplate, err := template.ParseFiles(dockerfileTemplate)
-	err = buildTemplate.Execute(os.Stdout, parameters)
+	err = buildTemplate.Execute(f, parameters)
 	if err != nil {
 		log.Print(err)
 		return
 	}
 }
+
+// GenerateDockerFile generates a Dockerfile with the passed parameters
+func GenerateDockerFiles() {
+	commonParameters := []string{
+		"-launch-browser false",
+	}
+	guiParameters := []string{
+		"--gui-dir=/usr/local/skycoin/static",
+		"--web-interface-addr=0.0.0.0",
+	}
+	noGuiParameters := []string{
+		"-web-interface false",
+		"-web-interface false",
+	}
+	parameters := DockerParameters{
+		SkyCoinParameters: strings.Join(append(commonParameters, noGuiParameters...), "\", \""),
+		BuildContext:      "run",
+		GitCommit:         GetCurrentGitCommit(),
+	}
+	noparameters := DockerParameters{
+		SkyCoinParameters: strings.Join(append(commonParameters, guiParameters...), "\", \""),
+		BuildContext:      "run",
+		GitCommit:         GetCurrentGitCommit(),
+	}
+	CreateDockerFile(parameters)
+	CreateDockerFile(noparameters)
+}
 func main() {
-	CreateDockerfiles()
+	GenerateDockerFiles()
 }

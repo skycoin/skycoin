23dbef5f91393664727951e078136e97bcdd839d
iketheadore <2589926+iketheadore@users.noreply.github.com>
2018-01-20 00:41:53 +0800

    Move scrypt chacha20poly1305 into a subpackage of cipher

---
+++ src/cipher/scryptChacha20poly1305/scryptChacha20poly1305.go
@@ -0,0 +1,129 @@
+package scryptChacha20poly1305
+
+import (
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/json"
+	"errors"
+	"math"
+
+	"github.com/skycoin/skycoin/src/cipher"
+	"golang.org/x/crypto/chacha20poly1305"
+	"golang.org/x/crypto/scrypt"
+)
+
+const (
+	metaLengthSize = 2 // meta data length field size in bytes
+	saltSize       = 8 // salt bytes number
+)
+
+// ScryptChacha20poly1305 provides methods for encryption/decryption with scrypt and chacha20poly1305
+type ScryptChacha20poly1305 struct {
+	n      int
+	r      int
+	p      int
+	keyLen int
+}
+
+// New creates a ScryptChacha20poly1305 instance
+func New(N, r, p, keyLen int) *ScryptChacha20poly1305 {
+	return &ScryptChacha20poly1305{
+		n:      N,
+		r:      r,
+		p:      p,
+		keyLen: keyLen,
+	}
+}
+
+type meta struct {
+	N      int
+	R      int
+	P      int
+	KeyLen int
+	Salt   []byte
+	Nonce  []byte
+}
+
+// Encrypt encrypts data with password,
+// 1. Scrypt derives the key from password
+// 2. Chacha20poly1305 generates AEAD from the derived key
+// 4. Puts scrypt paramenters, salt and nonce into metadata, json serialize it and get the serialized metadata length
+// 5. AEAD.Seal encrypts the data, and use [length][metadata] as additional data
+// 6. Final format: base64([[length][metadata]][ciphertext]), length is 2 bytes.
+func (s *ScryptChacha20poly1305) Encrypt(data, password []byte) ([]byte, error) {
+	// Scyrpt derives key from password
+	salt := cipher.RandByte(saltSize)
+	dk, err := scrypt.Key(password, salt, s.n, s.r, s.p, s.keyLen)
+	if err != nil {
+		return nil, err
+	}
+
+	// Prepare metadata
+	m := meta{s.n, s.r, s.p, s.keyLen, salt, cipher.RandByte(chacha20poly1305.NonceSize)}
+	// json serialize the metadata
+	ms, err := json.Marshal(m)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(ms) > math.MaxUint16 {
+		return nil, errors.New("metadata length beyond the math.MaxUint16")
+	}
+
+	length := make([]byte, metaLengthSize)
+	binary.LittleEndian.PutUint16(length, uint16(len(ms)))
+
+	// Additional data for AEAD
+	ad := append(length, ms...)
+	aead, err := chacha20poly1305.New(dk)
+	if err != nil {
+		return nil, err
+	}
+
+	ciphertext := aead.Seal(nil, m.Nonce, data, ad)
+
+	// Base64 encode the [[length][metadata]][ciphertext]
+	return []byte(base64.StdEncoding.EncodeToString(append(ad, ciphertext...))), nil
+}
+
+// Decrypt decrypts the data with password
+// 1. Base64 decodes the data
+// 2. Reads the first [metaLengthSize] bytes data to get the metadata length, and reads out the metadata.
+// 3. Scrypt derives key from password and paramenters in metadata
+// 4. Chacha20poly1305 geneates AEAD
+// 5. AEAD decrypts ciphertext with nonce in metadata and [length][metadata] as additional data.
+func (s *ScryptChacha20poly1305) Decrypt(data, password []byte) ([]byte, error) {
+	encData, err := base64.StdEncoding.DecodeString(string(data))
+	if err != nil {
+		return nil, err
+	}
+
+	length := binary.LittleEndian.Uint16(encData[:metaLengthSize])
+	if length > math.MaxUint16 {
+		return nil, errors.New("metadata length beyond math.MaxUint64")
+	}
+
+	if int(metaLengthSize+length) > len(encData) {
+		return nil, errors.New("invalid metadata length")
+	}
+
+	var m meta
+	if err := json.Unmarshal(encData[metaLengthSize:metaLengthSize+length], &m); err != nil {
+		return nil, err
+	}
+
+	ad := encData[:metaLengthSize+length]
+	// Scrypt derives key
+	dk, err := scrypt.Key(password, m.Salt, m.N, m.R, m.P, m.KeyLen)
+	if err != nil {
+		return nil, err
+	}
+
+	// Geneates AEAD
+	aead, err := chacha20poly1305.New(dk)
+	if err != nil {
+		return nil, err
+	}
+
+	return aead.Open(nil, m.Nonce, encData[metaLengthSize+length:], ad)
+}
---
+++ src/cipher/scryptChacha20poly1305/scryptChacha20poly1305_test.go
@@ -0,0 +1,81 @@
+package scryptChacha20poly1305
+
+import (
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestScryptChacha20poly1305Encrypt(t *testing.T) {
+	for i := uint(14); i < 21; i++ {
+		name := fmt.Sprintf("N=1<<%v r=%v p=%v keyLen=%v", i, 8, 1, 32)
+		t.Run(name, func(t *testing.T) {
+			crypto := New(1<<i, 8, 1, 32)
+			encData, err := crypto.Encrypt([]byte("plaintext"), []byte("password"))
+			require.NoError(t, err)
+
+			data, err := base64.StdEncoding.DecodeString(string(encData))
+			require.NoError(t, err)
+			// Checks the prefix
+			ml := binary.LittleEndian.Uint16(data[:metaLengthSize])
+			require.True(t, ml <= math.MaxUint16)
+			require.True(t, int(metaLengthSize+ml) <= len(data))
+			var m meta
+			require.NoError(t, json.Unmarshal(data[metaLengthSize:metaLengthSize+ml], &m))
+			require.Equal(t, m.N, 1<<i)
+			require.Equal(t, m.R, 8)
+			require.Equal(t, m.P, 1)
+			require.Equal(t, m.KeyLen, 32)
+		})
+	}
+}
+
+func TestScryptChacha20poly1305Decrypt(t *testing.T) {
+	tt := []struct {
+		name   string
+		data   []byte
+		encPwd []byte
+		decPwd []byte
+		err    error
+	}{
+		{
+			"ok",
+			[]byte("plaintext"),
+			[]byte("pwd"),
+			[]byte("pwd"),
+			nil,
+		},
+		{
+			"invalid password",
+			[]byte("plaintext"),
+			[]byte("pwd"),
+			[]byte("wrong password"),
+			errors.New("chacha20poly1305: message authentication failed"),
+		},
+	}
+
+	for _, tc := range tt {
+		for i := uint(14); i < 21; i++ {
+			name := fmt.Sprintf("N=1<<%v r=8 p=1 keyLen=32 %v", i, tc.name)
+			t.Run(name, func(t *testing.T) {
+				crypto := New(1<<i, 8, 1, 32)
+				encData, err := crypto.Encrypt(tc.data, tc.encPwd)
+				require.NoError(t, err)
+
+				data, err := crypto.Decrypt(encData, tc.decPwd)
+				require.Equal(t, tc.err, err)
+				if err != nil {
+					return
+				}
+
+				require.Equal(t, tc.data, data)
+			})
+		}
+	}
+}

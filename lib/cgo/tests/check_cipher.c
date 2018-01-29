9c9941bbbb1b508ecdb20c2db6dd1ed377fd66b8
Olemis Lang <olemis+work@gmail.com>
2018-01-29 01:14:49 -0500

    [lib] refs #902 - Valid Skycoin address
    Synthesis: Tested: 2 | Passing: 2 | Failing: 0 | Crashing: 0

--- lib/cgo/tests/check_cipher.c
+++ lib/cgo/tests/check_cipher.c
@@ -2,13 +2,13 @@
 #include <criterion/criterion.h>
 #include "libskycoin.h"
 
-#define SKYCOIN_ADDRESS_VALID "12345678"
+#define SKYCOIN_ADDRESS_VALID "2GgFvqoyk9RjwVzj8tqfcXVXB4orBwoc9qv"
 #define SKYCOIN_ADDRESS_WRONG "12345678"
 
 Test(cipher, test_address_valid) {
   GoString strAddr = {
     SKYCOIN_ADDRESS_VALID,
-    8
+    35
   };
   Address addr;
 

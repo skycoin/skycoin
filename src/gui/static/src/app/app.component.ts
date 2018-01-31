aae740968c9cc0af93853d6b686917c284384dd5
therealssj <mehul.guptagm@gmail.com>
2018-02-01 01:17:32 +0530

    fix merge conflict

--- src/gui/static/src/app/app.component.ts
+++ src/gui/static/src/app/app.component.ts
@@ -1,11 +1,5 @@
-import { Component } from '@angular/core';
-import { WalletService } from './services/wallet.service';
-import { BlockchainService } from './services/blockchain.service';
+import {Component} from '@angular/core';
 import 'rxjs/add/operator/takeWhile';
-import { ApiService } from './services/api.service';
-import { Http, Headers } from '@angular/http';
-import {Observable} from 'rxjs/Observable';
-
 
 @Component({
   selector: 'app-root',
@@ -13,46 +7,4 @@ import {Observable} from 'rxjs/Observable';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  current: number;
-  highest: number;
-  version: string;
-  releaseVersion: string;
-  updateAvailable: boolean;
-
-  constructor(
-    public walletService: WalletService,
-    private apiService: ApiService,
-    private blockchainService: BlockchainService,
-    private http: Http,
-  ) {}
-
-  ngOnInit() {
-    this.setVersion();
-  }
-
-  private setVersion() {
-    // Set build version
-    this.apiService.get('version')
-      .subscribe(output =>  this.version = output.version);
-
-    // Set latest release version from github
-    this.http.get('https://api.github.com/repos/skycoin/skycoin/tags')
-      .map((res: any) => res.json())
-      .catch((error: any) => Observable.throw(error || 'Unable to fetch latest release version from github.'))
-      .subscribe(response =>  {
-        // Iterate though the tags
-        // Find the latest tag which is not a rc
-        for ( const i in response ) {
-          if ( response.hasOwnProperty(i) && response[i]['name'].indexOf('rc') === -1 ) {
-            this.releaseVersion = response[i]['name'].substr(1);
-            break;
-          }
-        }
-
-        // Check if build version and release version differ
-        this.updateAvailable = (this.version !== this.releaseVersion);
-      });
-  }
-
 }

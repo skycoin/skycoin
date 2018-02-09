1c9538983d7afbc0ad230a9b19b2d51d8bc0f90f
Eduardo Sánchez <dev0024@simelo.tech>
2018-02-09 18:59:03 -0500

    [core] [hexdump] [test] refs #913 Gofmt'd files

--- src/testutil/message_hexdump.go
+++ src/testutil/message_hexdump.go
@@ -1,14 +1,14 @@
 package testutil
 
 import (
+	"crypto/sha256"
 	"fmt"
-	"reflect"
-	"github.com/skycoin/skycoin/src/daemon/gnet"
+	"github.com/skycoin/skycoin/src/cipher"
 	"github.com/skycoin/skycoin/src/cipher/encoder"
+	"github.com/skycoin/skycoin/src/daemon/gnet"
+	"reflect"
 	"strconv"
 	"time"
-	"crypto/sha256"
-	"github.com/skycoin/skycoin/src/cipher"
 )
 
 func serializeMessage(msg gnet.Message) []byte {
@@ -25,61 +25,61 @@ func serializeMessage(msg gnet.Message) []byte {
 	return m
 }
 
-func getSliceContentsString(sl []string,offset int) string {
+func getSliceContentsString(sl []string, offset int) string {
 	var res string = ""
 	var counter int = 0
-	if offset != -1{
-		var hex = strconv.FormatInt(int64(offset),16)
+	if offset != -1 {
+		var hex = strconv.FormatInt(int64(offset), 16)
 		var l = len(hex)
-		for i := 0; i < 4 - l; i++ {
+		for i := 0; i < 4-l; i++ {
 			hex = "0" + hex
 		}
 		hex = "0x" + hex
-		res += hex +  " "
+		res += hex + " "
 	}
 	for i := 0; i < len(sl); i++ {
 		counter++
 		res += sl[i] + " "
-		if counter == 16{
+		if counter == 16 {
 			res += "\n"
 			if offset != -1 {
-				res += "       "//7 spaces
+				res += "       " //7 spaces
 			}
 			counter = 0
 		}
 	}
-	for i := 0; i < (16-counter); i++ {
+	for i := 0; i < (16 - counter); i++ {
 		res += "..."
 	}
 	res += "..."
 	return res
 }
 
-func printLHexDumpWithFormat(offset int, name string, buffer []byte){
-	var hexBuff = make([]string,len(buffer))
-	for  i := 0; i < len(buffer) ;i++  {
-		hexBuff[i] = strconv.FormatInt(int64(buffer[i]),16)
+func printLHexDumpWithFormat(offset int, name string, buffer []byte) {
+	var hexBuff = make([]string, len(buffer))
+	for i := 0; i < len(buffer); i++ {
+		hexBuff[i] = strconv.FormatInt(int64(buffer[i]), 16)
 	}
-	for  i := 0; i < len(buffer) ;i++  {
-		if len(hexBuff[i]) == 1{
+	for i := 0; i < len(buffer); i++ {
+		if len(hexBuff[i]) == 1 {
 			hexBuff[i] = "0" + hexBuff[i]
 		}
 	}
-	fmt.Println(getSliceContentsString(hexBuff,offset),name)
+	fmt.Println(getSliceContentsString(hexBuff, offset), name)
 }
 
 func GenerateRandomSha256() cipher.SHA256 {
 	return sha256.Sum256([]byte(string(time.Now().Unix())))
 }
 
-func HexDump(message gnet.Message){
+func HexDump(message gnet.Message) {
 	var serializedMsg = serializeMessage(message)
 
-	printLHexDumpWithFormat(-1,"Full message",serializedMsg)
+	printLHexDumpWithFormat(-1, "Full message", serializedMsg)
 
 	fmt.Println("------------------------------------------------------------------------")
 	var offset int = 0
-	printLHexDumpWithFormat(0,"Prefix",serializedMsg[0:8])
+	printLHexDumpWithFormat(0, "Prefix", serializedMsg[0:8])
 	offset += len(serializedMsg[0:8])
 	var v = reflect.Indirect(reflect.ValueOf(message))
 
@@ -90,15 +90,15 @@ func HexDump(message gnet.Message){
 		if f.Tag.Get("enc") != "-" {
 			if v_f.CanSet() || f.Name != "_" {
 				if v.Field(i).Kind() == reflect.Slice {
-					printLHexDumpWithFormat(offset,f.Name + " header",encoder.Serialize(v.Field(i).Slice(0, v.Field(i).Len()).Interface())[0:4])
+					printLHexDumpWithFormat(offset, f.Name+" header", encoder.Serialize(v.Field(i).Slice(0, v.Field(i).Len()).Interface())[0:4])
 					offset += len(encoder.Serialize(v.Field(i).Slice(0, v.Field(i).Len()).Interface())[0:4])
 
 					for j := 0; j < v.Field(i).Len(); j++ {
-						printLHexDumpWithFormat(offset,f.Name + "#" + strconv.Itoa(j),encoder.Serialize(v.Field(i).Slice(j, j+1).Interface()))
+						printLHexDumpWithFormat(offset, f.Name+"#"+strconv.Itoa(j), encoder.Serialize(v.Field(i).Slice(j, j+1).Interface()))
 						offset += len(encoder.Serialize(encoder.Serialize(v.Field(i).Slice(j, j+1).Interface())))
 					}
 				} else {
-					printLHexDumpWithFormat(offset,f.Name,encoder.Serialize(v.Field(i).Interface()))
+					printLHexDumpWithFormat(offset, f.Name, encoder.Serialize(v.Field(i).Interface()))
 					offset += len(encoder.Serialize(v.Field(i).Interface()))
 				}
 			} else {
@@ -106,6 +106,5 @@ func HexDump(message gnet.Message){
 			}
 		}
 	}
-
 
 }
--- src/testutil/message_hexdump_test.go
+++ src/testutil/message_hexdump_test.go
@@ -1,16 +1,15 @@
 package testutil
 
 import (
-	"testing"
+	"github.com/skycoin/skycoin/src/cipher"
+	"github.com/skycoin/skycoin/src/coin"
 	"github.com/skycoin/skycoin/src/daemon"
 	"github.com/skycoin/skycoin/src/daemon/pex"
-	"github.com/skycoin/skycoin/src/coin"
-	"github.com/skycoin/skycoin/src/cipher"
+	"testing"
 )
 
-
 func TestIntroductionMessage(t *testing.T) {
-	var message = daemon.NewIntroductionMessage(1234,5,7890)
+	var message = daemon.NewIntroductionMessage(1234, 5, 7890)
 	HexDump(message)
 }
 
@@ -20,11 +19,11 @@ func TestGetPeersMessage(t *testing.T) {
 }
 
 func TestGivePeersMessage(t *testing.T) {
-	var peers = make([]pex.Peer,3)
+	var peers = make([]pex.Peer, 3)
 	var peer0 pex.Peer = *pex.NewPeer("118.178.135.93:6000")
 	var peer1 pex.Peer = *pex.NewPeer("47.88.33.156:6000")
 	var peer2 pex.Peer = *pex.NewPeer("121.41.103.148:6000")
-	peers = append(peers, peer0,peer1,peer2)
+	peers = append(peers, peer0, peer1, peer2)
 	var message = daemon.NewGivePeersMessage(peers)
 	HexDump(message)
 }
@@ -42,16 +41,16 @@ func TestPongMessage(t *testing.T) {
 }
 
 func TestGetBlocksMessage(t *testing.T) {
-	var message = daemon.NewGetBlocksMessage(1234,5678)
+	var message = daemon.NewGetBlocksMessage(1234, 5678)
 	HexDump(message)
 }
 
 func TestGiveBlocksMessage(t *testing.T) {
-	var blocks = make([]coin.SignedBlock,1)
+	var blocks = make([]coin.SignedBlock, 1)
 	var body1 = coin.BlockBody{
-		Transactions: make([]coin.Transaction,0),
+		Transactions: make([]coin.Transaction, 0),
 	}
-	var block1 coin.Block = coin.Block {
+	var block1 coin.Block = coin.Block{
 		Body: body1,
 		Head: coin.BlockHeader{
 			Version:  0x02,
@@ -63,7 +62,7 @@ func TestGiveBlocksMessage(t *testing.T) {
 		}}
 	var sig, _ = cipher.SigFromHex("123")
 	var signedBlock = coin.SignedBlock{
-		Sig: sig,
+		Sig:   sig,
 		Block: block1,
 	}
 	blocks = append(blocks, signedBlock)
@@ -77,67 +76,67 @@ func TestAnnounceBlocksMessage(t *testing.T) {
 }
 
 func TestGetTxnsMessage(t *testing.T) {
-	var shas = make([]cipher.SHA256,0)
+	var shas = make([]cipher.SHA256, 0)
 
-	shas = append(shas,GenerateRandomSha256(),GenerateRandomSha256())
+	shas = append(shas, GenerateRandomSha256(), GenerateRandomSha256())
 	var message = daemon.NewGetTxnsMessage(shas)
 	HexDump(message)
 }
 
 func TestGiveTxnsMessage(t *testing.T) {
-	var transactions coin.Transactions = make([]coin.Transaction,0)
-	var transactionOutputs0 []coin.TransactionOutput = make([]coin.TransactionOutput,0)
-	var transactionOutputs1 []coin.TransactionOutput = make([]coin.TransactionOutput,0)
+	var transactions coin.Transactions = make([]coin.Transaction, 0)
+	var transactionOutputs0 []coin.TransactionOutput = make([]coin.TransactionOutput, 0)
+	var transactionOutputs1 []coin.TransactionOutput = make([]coin.TransactionOutput, 0)
 	var txOutput0 = coin.TransactionOutput{
-		Address:MakeAddress(),
-		Coins:12,
-		Hours:34,
+		Address: MakeAddress(),
+		Coins:   12,
+		Hours:   34,
 	}
 	var txOutput1 = coin.TransactionOutput{
-		Address:MakeAddress(),
-		Coins:56,
-		Hours:78,
+		Address: MakeAddress(),
+		Coins:   56,
+		Hours:   78,
 	}
 	var txOutput2 = coin.TransactionOutput{
-		Address:MakeAddress(),
-		Coins:9,
-		Hours:12,
+		Address: MakeAddress(),
+		Coins:   9,
+		Hours:   12,
 	}
 	var txOutput3 = coin.TransactionOutput{
-		Address:MakeAddress(),
-		Coins:34,
-		Hours:56,
+		Address: MakeAddress(),
+		Coins:   34,
+		Hours:   56,
 	}
-	transactionOutputs0 = append(transactionOutputs0,txOutput0,txOutput1)
-	transactionOutputs1 = append(transactionOutputs1,txOutput2,txOutput3)
+	transactionOutputs0 = append(transactionOutputs0, txOutput0, txOutput1)
+	transactionOutputs1 = append(transactionOutputs1, txOutput2, txOutput3)
 
-	var sig0,sig1,sig2,sig3 cipher.Sig
+	var sig0, sig1, sig2, sig3 cipher.Sig
 	sig0, _ = cipher.SigFromHex("sig0")
 	sig1, _ = cipher.SigFromHex("sig1")
 	sig2, _ = cipher.SigFromHex("sig2")
 	sig3, _ = cipher.SigFromHex("sig3")
 	var transaction0 = coin.Transaction{
-		Type:123,
-		In:[]cipher.SHA256{GenerateRandomSha256(),GenerateRandomSha256()},
-		InnerHash:GenerateRandomSha256(),
-		Length:5000,
-		Out:transactionOutputs0,
-		Sigs:[]cipher.Sig{sig0,sig1},
+		Type:      123,
+		In:        []cipher.SHA256{GenerateRandomSha256(), GenerateRandomSha256()},
+		InnerHash: GenerateRandomSha256(),
+		Length:    5000,
+		Out:       transactionOutputs0,
+		Sigs:      []cipher.Sig{sig0, sig1},
 	}
 	var transaction1 = coin.Transaction{
-		Type:123,
-		In:[]cipher.SHA256{GenerateRandomSha256(),GenerateRandomSha256()},
-		InnerHash:GenerateRandomSha256(),
-		Length:5000,
-		Out:transactionOutputs1,
-		Sigs:[]cipher.Sig{sig2,sig3},
+		Type:      123,
+		In:        []cipher.SHA256{GenerateRandomSha256(), GenerateRandomSha256()},
+		InnerHash: GenerateRandomSha256(),
+		Length:    5000,
+		Out:       transactionOutputs1,
+		Sigs:      []cipher.Sig{sig2, sig3},
 	}
-	transactions = append(transactions,transaction0,transaction1)
+	transactions = append(transactions, transaction0, transaction1)
 	var message = daemon.NewGiveTxnsMessage(transactions)
 	HexDump(message)
 }
 
 func TestAnnounceTxnsMessage(t *testing.T) {
-	var message = daemon.NewAnnounceTxnsMessage([]cipher.SHA256{GenerateRandomSha256(),GenerateRandomSha256()})
-	HexDump(message) 
+	var message = daemon.NewAnnounceTxnsMessage([]cipher.SHA256{GenerateRandomSha256(), GenerateRandomSha256()})
+	HexDump(message)
 }

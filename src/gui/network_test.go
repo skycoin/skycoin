51cab1c3f660cb13563ce7771a52240c9be1d13e
VavilenTatarskiy <levintsev@gmail.com>
2018-02-19 22:16:11 +0300

    added tests for gui/network.go

--- src/gui/network_test.go
+++ src/gui/network_test.go
@@ -4,11 +4,13 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
 
-	"github.com/skycoin/skycoin/src/daemon"
 	"github.com/stretchr/testify/require"
+
+	"github.com/skycoin/skycoin/src/daemon"
 )
 
 func TestConnection(t *testing.T) {
@@ -19,12 +21,31 @@ func TestConnection(t *testing.T) {
 		err                        string
 		addr                       string
 		gatewayGetConnectionResult *daemon.Connection
+		result                     *daemon.Connection
 	}{
 		{
 			name:   "405",
 			method: http.MethodPost,
 			status: http.StatusMethodNotAllowed,
+			err:    "405 Method Not Allowed",
+		},
+		{
+			name:   "404 - empty addr",
+			method: http.MethodGet,
+			status: http.StatusNotFound,
+			err:    "404 Not Found",
+			addr:   "",
+			gatewayGetConnectionResult: nil,
+			result: nil,
+		},
+		{
+			name:   "200",
+			method: http.MethodGet,
+			status: http.StatusOK,
 			err:    "",
+			addr:   "addr",
+			gatewayGetConnectionResult: &daemon.Connection{},
+			result: &daemon.Connection{},
 		},
 	}
 	for _, tc := range tt {
@@ -32,13 +53,18 @@ func TestConnection(t *testing.T) {
 			endpoint := "/network/connection"
 			gateway := NewGatewayerMock()
 			gateway.On("GetConnection", tc.addr).Return(tc.gatewayGetConnectionResult)
-
+			v := url.Values{}
+			if tc.addr != "" {
+				v.Add("addr", tc.addr)
+			}
+			if len(v) > 0 {
+				endpoint += "?" + v.Encode()
+			}
 			req, err := http.NewRequest(tc.method, endpoint, nil)
 			require.NoError(t, err)
 
 			rr := httptest.NewRecorder()
-			csrfStore := &CSRFStore{}
-			handler := NewServerMux(configuredHost, ".", gateway, csrfStore)
+			handler := NewServerMux(configuredHost, ".", gateway, &CSRFStore{})
 			handler.ServeHTTP(rr, req)
 
 			status := rr.Code
@@ -48,12 +74,219 @@ func TestConnection(t *testing.T) {
 				require.Equal(t, tc.err, strings.TrimSpace(rr.Body.String()), "case: %s, handler returned wrong error message: got `%v`| %s, want `%v`",
 					tc.name, strings.TrimSpace(rr.Body.String()), status, tc.err)
 			} else {
-				var msg *CoinSupply
+				var msg *daemon.Connection
 				err = json.Unmarshal(rr.Body.Bytes(), &msg)
 				require.NoError(t, err)
 				require.Equal(t, tc.result, msg)
 			}
 		})
 	}
+}
 
+func TestConnections(t *testing.T) {
+	tt := []struct {
+		name                        string
+		method                      string
+		status                      int
+		err                         string
+		gatewayGetConnectionsResult *daemon.Connections
+		result                      *daemon.Connections
+	}{
+		{
+			name:   "405",
+			method: http.MethodPost,
+			status: http.StatusMethodNotAllowed,
+			err:    "405 Method Not Allowed",
+		},
+		{
+			name:   "200",
+			method: http.MethodGet,
+			status: http.StatusOK,
+			err:    "",
+			gatewayGetConnectionsResult: &daemon.Connections{},
+			result: &daemon.Connections{},
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			endpoint := "/network/connections"
+			gateway := NewGatewayerMock()
+			gateway.On("GetConnections").Return(tc.gatewayGetConnectionsResult)
+			req, err := http.NewRequest(tc.method, endpoint, nil)
+			require.NoError(t, err)
+
+			rr := httptest.NewRecorder()
+			handler := NewServerMux(configuredHost, ".", gateway, &CSRFStore{})
+			handler.ServeHTTP(rr, req)
+
+			status := rr.Code
+			require.Equal(t, tc.status, status, "case: %s, handler returned wrong status code: got `%v` want `%v`", tc.name, status, tc.status)
+
+			if status != http.StatusOK {
+				require.Equal(t, tc.err, strings.TrimSpace(rr.Body.String()), "case: %s, handler returned wrong error message: got `%v`| %s, want `%v`",
+					tc.name, strings.TrimSpace(rr.Body.String()), status, tc.err)
+			} else {
+				var msg *daemon.Connections
+				err = json.Unmarshal(rr.Body.Bytes(), &msg)
+				require.NoError(t, err)
+				require.Equal(t, tc.result, msg)
+			}
+		})
+	}
+}
+
+func TestDefaultConnections(t *testing.T) {
+	tt := []struct {
+		name                               string
+		method                             string
+		status                             int
+		err                                string
+		gatewayGetDefaultConnectionsResult []string
+		result                             []string
+	}{
+		{
+			name:   "405",
+			method: http.MethodPost,
+			status: http.StatusMethodNotAllowed,
+			err:    "405 Method Not Allowed",
+		},
+		{
+			name:   "200",
+			method: http.MethodGet,
+			status: http.StatusOK,
+			err:    "",
+			gatewayGetDefaultConnectionsResult: []string{},
+			result: []string{},
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			endpoint := "/network/defaultConnections"
+			gateway := NewGatewayerMock()
+			gateway.On("GetDefaultConnections").Return(tc.gatewayGetDefaultConnectionsResult)
+			req, err := http.NewRequest(tc.method, endpoint, nil)
+			require.NoError(t, err)
+
+			rr := httptest.NewRecorder()
+			handler := NewServerMux(configuredHost, ".", gateway, &CSRFStore{})
+			handler.ServeHTTP(rr, req)
+
+			status := rr.Code
+			require.Equal(t, tc.status, status, "case: %s, handler returned wrong status code: got `%v` want `%v`", tc.name, status, tc.status)
+
+			if status != http.StatusOK {
+				require.Equal(t, tc.err, strings.TrimSpace(rr.Body.String()), "case: %s, handler returned wrong error message: got `%v`| %s, want `%v`",
+					tc.name, strings.TrimSpace(rr.Body.String()), status, tc.err)
+			} else {
+				var msg []string
+				err = json.Unmarshal(rr.Body.Bytes(), &msg)
+				require.NoError(t, err)
+				require.Equal(t, tc.result, msg)
+			}
+		})
+	}
+}
+
+func TestGetTrustConnections(t *testing.T) {
+	tt := []struct {
+		name                             string
+		method                           string
+		status                           int
+		err                              string
+		gatewayGetTrustConnectionsResult []string
+		result                           []string
+	}{
+		{
+			name:   "405",
+			method: http.MethodPost,
+			status: http.StatusMethodNotAllowed,
+			err:    "405 Method Not Allowed",
+		},
+		{
+			name:   "200",
+			method: http.MethodGet,
+			status: http.StatusOK,
+			err:    "",
+			gatewayGetTrustConnectionsResult: []string{},
+			result: []string{},
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			endpoint := "/network/connections/trust"
+			gateway := NewGatewayerMock()
+			gateway.On("GetTrustConnections").Return(tc.gatewayGetTrustConnectionsResult)
+			req, err := http.NewRequest(tc.method, endpoint, nil)
+			require.NoError(t, err)
+
+			rr := httptest.NewRecorder()
+			handler := NewServerMux(configuredHost, ".", gateway, &CSRFStore{})
+			handler.ServeHTTP(rr, req)
+
+			status := rr.Code
+			require.Equal(t, tc.status, status, "case: %s, handler returned wrong status code: got `%v` want `%v`", tc.name, status, tc.status)
+
+			if status != http.StatusOK {
+				require.Equal(t, tc.err, strings.TrimSpace(rr.Body.String()), "case: %s, handler returned wrong error message: got `%v`| %s, want `%v`",
+					tc.name, strings.TrimSpace(rr.Body.String()), status, tc.err)
+			} else {
+				var msg []string
+				err = json.Unmarshal(rr.Body.Bytes(), &msg)
+				require.NoError(t, err)
+				require.Equal(t, tc.result, msg)
+			}
+		})
+	}
+}
+
+func TestGetExchgConnection(t *testing.T) {
+	tt := []struct {
+		name                            string
+		method                          string
+		status                          int
+		err                             string
+		gatewayGetExchgConnectionResult []string
+		result                          []string
+	}{
+		{
+			name:   "405",
+			method: http.MethodPost,
+			status: http.StatusMethodNotAllowed,
+			err:    "405 Method Not Allowed",
+		},
+		{
+			name:   "200",
+			method: http.MethodGet,
+			status: http.StatusOK,
+			err:    "",
+			gatewayGetExchgConnectionResult: []string{},
+			result: []string{},
+		},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			endpoint := "/network/connections/exchange"
+			gateway := NewGatewayerMock()
+			gateway.On("GetExchgConnection").Return(tc.gatewayGetExchgConnectionResult)
+			req, err := http.NewRequest(tc.method, endpoint, nil)
+			require.NoError(t, err)
+
+			rr := httptest.NewRecorder()
+			handler := NewServerMux(configuredHost, ".", gateway, &CSRFStore{})
+			handler.ServeHTTP(rr, req)
+
+			status := rr.Code
+			require.Equal(t, tc.status, status, "case: %s, handler returned wrong status code: got `%v` want `%v`", tc.name, status, tc.status)
+
+			if status != http.StatusOK {
+				require.Equal(t, tc.err, strings.TrimSpace(rr.Body.String()), "case: %s, handler returned wrong error message: got `%v`| %s, want `%v`",
+					tc.name, strings.TrimSpace(rr.Body.String()), status, tc.err)
+			} else {
+				var msg []string
+				err = json.Unmarshal(rr.Body.Bytes(), &msg)
+				require.NoError(t, err)
+				require.Equal(t, tc.result, msg)
+			}
+		})
+	}
 }

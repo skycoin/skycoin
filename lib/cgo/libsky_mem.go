e96b2e3e3b0de7729c491f507ed6a6f1cf6dd944
Olemis Lang <olemis+work@gmail.com>
2018-03-17 00:09:52 -0400

    [lib] refs #968 - Initial version of memory management functions

--- lib/cgo/libsky_mem.go
+++ lib/cgo/libsky_mem.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"unsafe"
+
+	"github.com/skycoin/skycoin/src/cipher"
 )
+
+/*
+  #include "../../include/skytypes.h"
+*/
+import "C"
 
 type Handle uint64
 
@@ -26,8 +33,44 @@ func closeHandle(handle Handle) {
 	delete(handleMap, handle)
 }
 
-func inplaceArrayObj(p unsafe.Pointer, length int) interface{} {
+func inplaceByteArray(p unsafe.Pointer, length int) *[]byte {
 	// Create slice without copying data
-	return (*[1 << 30]byte)(p)[:length:length]
-
+	// TODO: Memory efficiency
+	slice := (*[1 << 30]byte)(p)[:length:length]
+	return &slice
 }
+
+func inplacePubKey(p *C.PubKey) *cipher.PubKey {
+	return (*cipher.PubKey)(unsafe.Pointer(inplaceByteArray(unsafe.Pointer(p), 33)))
+}
+
+func inplacePubKeySlice(p *C.PubKeySlice) *cipher.PubKeySlice {
+	// Create slice without copying data
+	slice := (*[1 << 30]cipher.PubKey)(p.data)[:p.len:p.len]
+	return (*cipher.PubKeySlice)(unsafe.Pointer(&slice))
+}
+
+func inplaceSecKey(p *C.SecKey) *cipher.SecKey {
+	return (*cipher.SecKey)(unsafe.Pointer(inplaceByteArray(unsafe.Pointer(p), 32)))
+}
+
+func inplaceSig(p *C.Sig) *cipher.Sig {
+	return (*cipher.Sig)(unsafe.Pointer(inplaceByteArray(unsafe.Pointer(p), 64 + 1)))
+}
+
+func inplaceChecksum(p *C.Checksum) *cipher.Checksum {
+	return (*cipher.Checksum)(unsafe.Pointer(inplaceByteArray(unsafe.Pointer(p), 4)))
+}
+
+func inplaceRipemd160(p *C.Ripemd160) *cipher.Ripemd160 {
+	return (*cipher.Ripemd160)(unsafe.Pointer(inplaceByteArray(unsafe.Pointer(p), 20)))
+}
+
+func inplaceSHA256(p *C.SHA256) *cipher.SHA256 {
+	return (*cipher.SHA256)(unsafe.Pointer(inplaceByteArray(unsafe.Pointer(p), 20)))
+}
+
+func inplaceAddress(p *C.Address) *cipher.Address {
+	return (*cipher.Address)(unsafe.Pointer(p))
+}
+

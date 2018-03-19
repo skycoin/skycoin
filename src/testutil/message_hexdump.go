d499a9c268f69b12dcd42892a37966b2e0d7d367
Olemis Lang <olemis+work@gmail.com>
2018-03-18 21:38:14 -0400

    [lib] refs #913 - Test's -> Example's . Moved onto src/daemon/messages_test.go

--- src/testutil/message_hexdump.go
+++ src/testutil/message_hexdump.go
@@ -1,33 +1,9 @@
 package testutil
 
 import (
-	"crypto/sha256"
 	"fmt"
-	"reflect"
 	"strconv"
-	"time"
-
-	"github.com/skycoin/skycoin/src/cipher"
-	"github.com/skycoin/skycoin/src/cipher/encoder"
-	"github.com/skycoin/skycoin/src/daemon"
-	"github.com/skycoin/skycoin/src/daemon/gnet"
 )
-
-var registered = false
-
-func serializeMessage(msg gnet.Message) []byte {
-	t := reflect.ValueOf(msg).Elem().Type()
-	msgID := gnet.MessageIDMap[t]
-	bMsg := encoder.Serialize(msg)
-
-	// message length
-	bLen := encoder.SerializeAtomic(uint32(len(bMsg) + len(msgID)))
-	m := make([]byte, 0)
-	m = append(m, bLen...)     // length prefix
-	m = append(m, msgID[:]...) // message id
-	m = append(m, bMsg...)     // message bytes
-	return m
-}
 
 func getSliceContentsString(sl []string, offset int) string {
 	var res string = ""
@@ -68,7 +44,7 @@ func getSliceContentsString(sl []string, offset int) string {
 	return res
 }
 
-func printLHexDumpWithFormat(offset int, name string, buffer []byte) {
+func PrintLHexDumpWithFormat(offset int, name string, buffer []byte) {
 	var hexBuff = make([]string, len(buffer))
 	for i := 0; i < len(buffer); i++ {
 		hexBuff[i] = strconv.FormatInt(int64(buffer[i]), 16)
@@ -81,56 +57,7 @@ func printLHexDumpWithFormat(offset int, name string, buffer []byte) {
 	fmt.Println(getSliceContentsString(hexBuff, offset), name)
 }
 
-func GenerateRandomSha256() cipher.SHA256 {
-	return sha256.Sum256([]byte(string(time.Now().Unix())))
-}
-
-func HexDump(message gnet.Message) {
-	var messagesConfig = daemon.NewMessagesConfig()
-	if registered == false {
-		messagesConfig.Register()
-		registered = true
-	}
-
-	var serializedMsg = gnet.EncodeMessage(message)
-
-	printLHexDumpWithFormat(-1, "Full message", serializedMsg)
-
-	fmt.Println("------------------------------------------------------------------------")
-	var offset int = 0
-	printLHexDumpWithFormat(0, "Length", serializedMsg[0:4])
-	printLHexDumpWithFormat(4, "Prefix", serializedMsg[4:8])
-	offset += len(serializedMsg[0:8])
-	var v = reflect.Indirect(reflect.ValueOf(message))
-
-	t := v.Type()
-	for i := 0; i < v.NumField(); i++ {
-		v_f := v.Field(i)
-		f := t.Field(i)
-		if f.Tag.Get("enc") != "-" {
-			if v_f.CanSet() || f.Name != "_" {
-				if v.Field(i).Kind() == reflect.Slice {
-					printLHexDumpWithFormat(offset, f.Name+" length", encoder.Serialize(v.Field(i).Slice(0, v.Field(i).Len()).Interface())[0:4])
-					offset += len(encoder.Serialize(v.Field(i).Slice(0, v.Field(i).Len()).Interface())[0:4])
-
-					for j := 0; j < v.Field(i).Len(); j++ {
-						printLHexDumpWithFormat(offset, f.Name+"#"+strconv.Itoa(j), encoder.Serialize(v.Field(i).Slice(j, j+1).Interface()))
-						offset += len(encoder.Serialize(encoder.Serialize(v.Field(i).Slice(j, j+1).Interface())))
-					}
-				} else {
-					printLHexDumpWithFormat(offset, f.Name, encoder.Serialize(v.Field(i).Interface()))
-					offset += len(encoder.Serialize(v.Field(i).Interface()))
-				}
-			} else {
-				//don't write anything
-			}
-		}
-	}
-
-	printFinalHex(len(serializedMsg))
-
-}
-func printFinalHex(i int) {
+func PrintFinalHex(i int) {
 	var finalHex = strconv.FormatInt(int64(i), 16)
 	var l = len(finalHex)
 	for i := 0; i < 4-l; i++ {

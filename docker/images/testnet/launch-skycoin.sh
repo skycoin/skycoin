568f5e073af5b27e7d9004d377df411d4ca6331e
Mauricio López-Quintana Conesa <dev0005@simelo.tech>
2018-03-25 11:53:09 -0400

    [docker] [testnet] refs #828 Removes container IP address from connections list to avoid self-connect.

--- docker/images/testnet/launch-skycoin.sh
+++ docker/images/testnet/launch-skycoin.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+MY_IP=`ip addr show | grep global | awk '{print $2}' | cut -d\/ -f 1`
 mkdir -p $DATA_DIR$COIN
 nslookup skycoin-peer | awk '/Address/{print $3":16000"}' > $DATA_DIR$COIN/connections.txt
 nslookup skycoin-master | awk '/Address/{print $3":16000"}' >> $DATA_DIR$COIN/connections.txt
+sed -i '/$MY_IP/d' $DATA_DIR$COIN/connections.txt
 $@

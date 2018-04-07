bebc98c6edd9dcc78e80aa896b9d7a7d25bccc11
Mauricio López-Quintana Conesa <dev0005@simelo.tech>
2018-04-07 19:53:36 -0400

    [docker] [skycoin] refs #1184 Fixes adduser invocation.

--- launcher.sh
+++ launcher.sh
@@ -3,7 +3,7 @@ COMMAND="skycoin --data-dir $DATA_DIR --wallet-dir $WALLET_DIR $@"
 VOLUME_UID=`stat -c %u $DATA_DIR`
 USERNAME=`id -nu $VOLUME_UID`
 if [[ $? -ne 0 ]] ; then
-    adduser --uid $VOLUME_UID skycoin
+    adduser -D -u $VOLUME_UID skycoin
     su skycoin $COMMAND
 else
     su $USERNAME $COMMAND

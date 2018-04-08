674e4b76270e54f32996a4427befe137c8f50bd8
Mauricio López-Quintana Conesa <dev0005@simelo.tech>
2018-04-08 13:58:37 -0400

    [docker] [skycoin] refs #1184 Fixes error while setting UID and USERNAME.

--- docker_launcher.sh
+++ docker_launcher.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 COMMAND="skycoin --data-dir $DATA_DIR --wallet-dir $WALLET_DIR $@"
-VOLUME_UID=`stat -c %U $DATA_DIR`
-if [[ $VOLUME_UID = "UNKNOWN" ]] ; then
+VOLUME_USERNAME=`stat -c %U $DATA_DIR`
+VOLUME_UID=`stat -c %u $DATA_DIR`
+if [[ $VOLUME_USERNAME = "UNKNOWN" ]] ; then
     adduser -D -u $VOLUME_UID skycoin
     su skycoin $COMMAND
 else
-    su $USERNAME $COMMAND
+    su $VOLUME_USERNAME $COMMAND
 fi

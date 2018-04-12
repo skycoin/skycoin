cb82cb4c35dd82644795c2893b0845369a9464b6
ratmil <eclipse@mysimplenetwork.com>
2018-04-12 12:19:22 -0400

    Fix error in array conversion from wrapper function to original function in libc

--- lib/cgo/cipher.poly1305.sum_amd64.go
+++ lib/cgo/cipher.poly1305.sum_amd64.go
@@ -15,15 +15,12 @@ import (
 import "C"
 
 // export SKY_poly1305_Sum
-func SKY_poly1305_Sum_amd64(_out *[]byte, _m *C.GoSlice_, _key *[]byte) (____error_code uint32) {
+func SKY_poly1305_Sum_amd64(_out *[16]byte, _m []byte, _key *[32]byte) (____error_code uint32) {
 	____error_code = 0
 	defer func() {
 		____error_code = catchApiPanic(____error_code, recover())
 	}()
 	//TODO: stdevEclipse Check Pointer casting
-	out := (*[16]byte)(unsafe.Pointer(_out))
-	m := *(*[]byte)(unsafe.Pointer(_m))
-	key := (*[32]byte)(unsafe.Pointer(_key))
-	poly1305.Sum(out, m, key)
+	poly1305.Sum(_out, _m, _key)
 	return
 }

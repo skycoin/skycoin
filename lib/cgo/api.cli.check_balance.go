c13762eb0c11583cca937ca0f41dea54b3030acf
ratmil <eclipse@mysimplenetwork.com>
2018-04-14 14:30:13 -0400

    [libc] ref #1191 Use webrpc client specific handle for api.cli.check_balance

--- lib/cgo/api.cli.check_balance.go
+++ lib/cgo/api.cli.check_balance.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	cli "github.com/skycoin/skycoin/src/api/cli"
-	webrpc "github.com/skycoin/skycoin/src/api/webrpc"
 	"unsafe"
 )
 
@@ -16,44 +15,40 @@ import (
 import "C"
 
 //export SKY_cli_CheckWalletBalance
-func SKY_cli_CheckWalletBalance(_c *C.Handle, _walletFile string, _arg2 *C.cli__BalanceResult) (____error_code uint32) {
+func SKY_cli_CheckWalletBalance(_c *C.WebrpcClient__Handle, _walletFile string, _arg2 *C.cli__BalanceResult) (____error_code uint32) {
 	____error_code = 0
 	defer func() {
 		____error_code = catchApiPanic(____error_code, recover())
 	}()
-	obj, ok := lookupHandleObj(Handle(*_c))
+	client, ok := lookupWebRpcClientHandle(*_c)
 	____error_code = SKY_ERROR
 	if ok {
-		if client, isClient := (obj).(*webrpc.Client); isClient {
-			balance, ____return_err := cli.CheckWalletBalance(client, _walletFile)
-			if ____return_err == nil {
-				*_arg2 = *(*C.cli__BalanceResult)(unsafe.Pointer(balance))
-			}
-			____error_code = libErrorCode(____return_err)
+		balance, ____return_err := cli.CheckWalletBalance(client, _walletFile)
+		if ____return_err == nil {
+			*_arg2 = *(*C.cli__BalanceResult)(unsafe.Pointer(balance))
 		}
+		____error_code = libErrorCode(____return_err)
 	}
 	return
 }
 
 //export SKY_cli_GetBalanceOfAddresses
-func SKY_cli_GetBalanceOfAddresses(_c *C.Handle, _addrs *C.GoSlice_, _arg2 *C.cli__BalanceResult) (____error_code uint32) {
+func SKY_cli_GetBalanceOfAddresses(_c *C.WebrpcClient__Handle, _addrs *C.GoSlice_, _arg2 *C.cli__BalanceResult) (____error_code uint32) {
 	____error_code = 0
 	defer func() {
 		____error_code = catchApiPanic(____error_code, recover())
 	}()
-	obj, ok := lookupHandleObj(Handle(*_c))
+	client, ok := lookupWebRpcClientHandle(*_c)
 	____error_code = SKY_ERROR
 	if ok {
-		if client, isClient := (obj).(*webrpc.Client); isClient {
-			//TODO: stdevEclipse Test this typecast
-			addrs := *(*[]string)(unsafe.Pointer(_addrs))
-			__arg2, ____return_err := cli.GetBalanceOfAddresses(client, addrs)
-			____error_code = libErrorCode(____return_err)
-			if ____return_err == nil {
-				*_arg2 = *(*C.cli__BalanceResult)(unsafe.Pointer(__arg2))
-			}
-			____error_code = libErrorCode(____return_err)
+		//TODO: stdevEclipse Test this typecast
+		addrs := *(*[]string)(unsafe.Pointer(_addrs))
+		__arg2, ____return_err := cli.GetBalanceOfAddresses(client, addrs)
+		____error_code = libErrorCode(____return_err)
+		if ____return_err == nil {
+			*_arg2 = *(*C.cli__BalanceResult)(unsafe.Pointer(__arg2))
 		}
+		____error_code = libErrorCode(____return_err)
 	}
 	return
 }

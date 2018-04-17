094e4d5175764a35a589bbd0b58b50b1eff0bb2e
Maykel Arias Torres <dev0006@simelo.tech>
2018-04-17 10:52:54 -0400

    Fixed #1235 changes in the util /http/log.go

--- src/util/http/log.go
+++ src/util/http/log.go
@@ -13,7 +13,30 @@ func ElapsedHandler(logger logrus.FieldLogger, handler http.Handler) http.Handle
 		lrw := newWrappedResponseWriter(w)
 		start := time.Now()
 		handler.ServeHTTP(lrw, r)
-		logger.Infof("%v %s %s %v", lrw.statusCode, r.Method, r.URL.Path, time.Since(start))
+		if lrw.statusCode == 200 || lrw.statusCode == 304 {
+			logger.Infof("%v %s %s %v", lrw.statusCode, r.Method, r.URL.Path, time.Since(start))
+		} else {
+			if lrw.statusCode == 400 {
+				logger.Errorf("%v %s %s %s %v", lrw.statusCode, "Bad Request", r.Method, r.URL.Path, time.Since(start))
+			}
+			if lrw.statusCode == 403 {
+				logger.Errorf("%v %s %s %s %v", lrw.statusCode, "Forbidden", r.Method, r.URL.Path, time.Since(start))
+			}
+			if lrw.statusCode == 404 {
+				logger.Errorf("%v %s %s %s %v", lrw.statusCode, "Not Found", r.Method, r.URL.Path, time.Since(start))
+			}
+			if lrw.statusCode == 405 {
+				logger.Errorf("%v %s %s %s %v", lrw.statusCode, "Method Not Allowed", r.Method, r.URL.Path, time.Since(start))
+			}
+			if lrw.statusCode == 500 {
+				logger.Errorf("%v %s %s %s %v", lrw.statusCode, "Internal Server Error", r.Method, r.URL.Path, time.Since(start))
+			}
+			if lrw.statusCode == 501 {
+				logger.Errorf("%v %s %s %s %v", lrw.statusCode, "Not Implemented", r.Method, r.URL.Path, time.Since(start))
+			}
+
+		}
+
 	})
 }
 

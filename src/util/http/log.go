c40b7289e4ebc5576393c4757b5867f3291299d2
Olemis Lang <olemis+work@gmail.com>
2018-04-18 21:56:53 -0400

    [util] refs #1235 - HTTP error responses ([]byte) in logs
    Screenshot : t.me/skycoindevtravis/103

--- src/util/http/log.go
+++ src/util/http/log.go
@@ -13,20 +13,32 @@ func ElapsedHandler(logger logrus.FieldLogger, handler http.Handler) http.Handle
 		lrw := newWrappedResponseWriter(w)
 		start := time.Now()
 		handler.ServeHTTP(lrw, r)
-		logger.Infof("%v %s %s %v", lrw.statusCode, r.Method, r.URL.Path, time.Since(start))
+		logMethod := logger.Infof
+		if lrw.statusCode >= 400 {
+			logMethod = logger.WithFields(logrus.Fields{
+				"http.response": lrw.response,
+			}).Errorf
+		}
+		logMethod("%d %s %s %s", lrw.statusCode, r.Method, r.URL.Path, time.Since(start))
 	})
 }
 
 type wrappedResponseWriter struct {
 	http.ResponseWriter
 	statusCode int
+	response   []byte
 }
 
 func newWrappedResponseWriter(w http.ResponseWriter) *wrappedResponseWriter {
-	return &wrappedResponseWriter{w, http.StatusOK}
+	return &wrappedResponseWriter{w, http.StatusOK, nil}
 }
 
 func (lrw *wrappedResponseWriter) WriteHeader(code int) {
 	lrw.statusCode = code
 	lrw.ResponseWriter.WriteHeader(code)
+}
+
+func (lrw *wrappedResponseWriter) Write(buff []byte) (int, error) {
+	lrw.response = append(lrw.response, buff...)
+	return lrw.ResponseWriter.Write(buff)
 }

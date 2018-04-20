960ff3de81a7191520b2f13bc6cdae89437d51dc
mahansky <m.pro.mark@gmail.com>
2018-04-20 14:13:48 +0200

    Show error on wallet creation

--- src/gui/static/src/app/components/pages/wallets/create-wallet/create-wallet.component.ts
+++ src/gui/static/src/app/components/pages/wallets/create-wallet/create-wallet.component.ts
@@ -32,11 +32,12 @@ export class CreateWalletComponent implements OnInit {
   }
 
   createWallet() {
+    this.button.resetState();
     this.button.setLoading();
 
     const password = this.encrypt ? this.form.value.password : null;
     this.walletService.create(this.form.value.label, this.form.value.seed, this.scan, password)
-      .subscribe(() => this.dialogRef.close());
+      .subscribe(() => this.dialogRef.close(), e => this.button.setError(e));
   }
 
   generateSeed() {

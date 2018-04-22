43eaf38d80f3d61dc210a2c4b167b150a6e6c426
Maykel Arias Torres <dev0006@simelo.tech>
2018-04-21 20:35:05 -0400

    Fixed #1275 modifying the run.sh for a random port not used by the system

--- run.sh
+++ run.sh
@@ -8,13 +8,23 @@ pushd "$DIR" >/dev/null
 COMMIT=$(git rev-parse HEAD)
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 GOLDFLAGS="-X main.Commit=${COMMIT} -X main.Branch=${BRANCH}"
+PORT=$(shuf -n 1 -i 1024-65535)
+# PORT=$(tr -dc 0-9 < /dev/urandom | head -c 4 | xargs)
+IS_READY=$(fuser -n tcp $PORT)
+
+while [[ ! $IS_READY = "" ]]; do
+	# PORT=$(tr -dc 0-9 < /dev/urandom | head -c 4 | xargs)
+	PORT=$(shuf -n 1 -i 1024-65535)
+	IS_READY=$(fuser -n tcp $PORT)
+done
+
 
 go run -ldflags "${GOLDFLAGS}" cmd/skycoin/skycoin.go \
     -gui-dir="${DIR}/src/gui/static/" \
     -launch-browser=true \
     -enable-wallet-api=true \
     -rpc-interface=false \
-    -web-interface-port $(($RANDOM%65563))  \
+    -web-interface-port $PORT   \
     $@
 
 popd >/dev/null

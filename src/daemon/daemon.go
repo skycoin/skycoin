2fe0ad03ae6d8340a47039ba54378b29bf042219
Eduardo Sánchez <eduarosanchezg37@gmail.com>
2018-04-25 19:03:56 -0400

    [pex] refs #712 Undone temporal changes

--- src/daemon/daemon.go
+++ src/daemon/daemon.go
@@ -650,8 +650,8 @@ func (dm *Daemon) connectToRandomPeer() {
 		return
 	}
 
-	if false {
-	//if len(dm.getOutgoingConnections(pex.IsDefault)) == 0 {
+
+	if len(dm.getOutgoingConnections(pex.IsDefault)) == 0 {
 		peers := dm.Pex.RandomDefault(1)
 		if (len(peers) == 1) {
 			dm.connectToPeer(peers[0])

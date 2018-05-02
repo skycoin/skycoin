00e2e8f6b444aa1a167f19ca561aa6d58298d350
mahansky <m.pro.mark@gmail.com>
2018-05-02 18:53:29 +0200

    Rename variables

--- src/gui/static/src/app/utils/semver.ts
+++ src/gui/static/src/app/utils/semver.ts
@@ -7,22 +7,22 @@
  * @param to
  */
 export function shouldUpgradeVersion(from: string, to: string): boolean {
-  const fromParts = to.split('.');
-  const toSplit = from.split('-');
-  const toParts = toSplit[0].split('.');
+  const toParts = to.split('.');
+  const fromSplit = from.split('-');
+  const fromParts = fromSplit[0].split('.');
 
   for (let i = 0; i < 3; i++) {
-    const fromNumber = Number(fromParts[i]);
     const toNumber = Number(toParts[i]);
+    const fromNumber = Number(fromParts[i]);
 
-    if (fromNumber > toNumber) {
+    if (toNumber > fromNumber) {
       return true;
     }
 
-    if (fromNumber < toNumber) {
+    if (toNumber < fromNumber) {
       return false;
     }
   }
 
-  return toSplit.length === 2 && toSplit[1].indexOf('rc') !== -1;
+  return fromSplit.length === 2 && fromSplit[1].indexOf('rc') !== -1;
 }

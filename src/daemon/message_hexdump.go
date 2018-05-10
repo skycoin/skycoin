017cfc60d1de1785f49cc5cb8d0b0bb43c111a58
Eduardo Sánchez <eduarosanchezg37@gmail.com>
2018-05-09 21:09:02 -0400

    [hexdump] [test] refs #1110 Removed IAnnotationsGenerator for gnet.Message Updated tests accordingly

--- src/daemon/message_hexdump.go
+++ src/daemon/message_hexdump.go
@@ -13,41 +13,6 @@ import (
 
 var registered = false
 
-// MessagesAnnotationsGenerator : Implementation of IAnnotationsGenerator for type gnet.Message
-type MessagesAnnotationsGenerator struct {
-	Message gnet.Message
-}
-
-// GenerateAnnotations : Implementation of IAnnotationsGenerator.GenerateAnnotations for type gnet.Message
-func (mag *MessagesAnnotationsGenerator) GenerateAnnotations() []util.Annotation {
-	var annotations = make([]util.Annotation, 2)
-	annotations[0] = util.Annotation{Size: 4, Name: "Length"}
-	annotations[1] = util.Annotation{Size: 4, Name: "Prefix"}
-	var v = reflect.Indirect(reflect.ValueOf(mag.Message))
-
-	t := v.Type()
-	for i := 0; i < v.NumField(); i++ {
-		vF := v.Field(i)
-		f := t.Field(i)
-		if f.Tag.Get("enc") != "-" {
-			if vF.CanSet() || f.Name != "_" {
-				if v.Field(i).Kind() == reflect.Slice {
-					annotations = append(annotations, util.Annotation{Size: 4, Name: f.Name + " length"})
-					for j := 0; j < v.Field(i).Len(); j++ {
-						annotations = append(annotations, util.Annotation{Size: len(encoder.Serialize(v.Field(i).Slice(j, j+1).Interface())[4:]), Name: f.Name + "#" + strconv.Itoa(j)})
-					}
-				} else {
-					annotations = append(annotations, util.Annotation{Size: len(encoder.Serialize(v.Field(i).Interface())), Name: f.Name})
-				}
-			} else {
-				//don't write anything
-			}
-		}
-	}
-
-	return annotations
-}
-
 // MessagesAnnotationsIterator : Implementation of IAnnotationsIterator for type gnet.Message
 type MessagesAnnotationsIterator struct {
 	Message      gnet.Message

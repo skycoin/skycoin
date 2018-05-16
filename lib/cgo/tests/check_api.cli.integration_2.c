f6fba9dab34d8d35b1386ce2eabcf0e0da476c6d
Maykel Arias Torres <dev0006@simelo.tech>
2018-05-16 14:39:13 -0400

    [libc] ref#1191 remove GoString in TestGenerateAddresses  [====] Synthesis: Tested: 89 | Passing: 88 | Failing: 1 | Crashing: 0

--- lib/cgo/tests/check_api.cli.integration_2.c
+++ lib/cgo/tests/check_api.cli.integration_2.c
@@ -47,10 +47,10 @@ void createTempWalletDir(bool encrypt) {
   strcpy(fullUrl, TEST_DATA_DIR);
   strcat(fullUrl, walletPath);
   rf = fopen(fullUrl, "rb");
-  unsigned char buff[BUFFER_SIZE];
+  unsigned char buff[2048];
   int readBits;
   // Copy file rf to f
-  while ((readBits = fread(buff, 1, BUFFER_SIZE, rf)))
+  while ((readBits = fread(buff, 1, 2048, rf)))
     fwrite(buff, 1, readBits, f);
 
   fclose(rf);
@@ -101,12 +101,33 @@ Test(api_cli_integration, TestGenerateAddresses) {
   tt[1].expectOutput = expectCmd;
   tt[1].goldenFile = "generate-addresses-2.golden";
 
+  strcpy(cmd, "generateAddresses -p pwd -j");
+  strcpy(expectCmd, "{\n    \"addresses\": [\n        "
+                    "\"7g3M372kxwNwwQEAmrronu4anXTW8aD1XC\"\n    ]\n}\n");
+  tt[2].name = "generateAddresses in encrypted wallet";
+  tt[2].encrypted = true;
+  tt[2].args = cmd;
+  tt[2].expectOutput = expectCmd;
+  tt[2].goldenFile = "generate-addresses-encrypted.golden";
+
+  char output[BUFFER_SIZE];
+  Config__Handle configHandle;
+  App__Handle appHandle;
+  GoUint32 errcode;
+  json_value *w;
+  json_value *expect;
+  char buffName[BUFFER_SIZE];
+  char buffDir[BUFFER_SIZE];
+  GoString WalletName = {"WALLET_NAME", 11};
+  GoString WalletPath = {"WALLET_DIR", 10};
+  GoString pathname = {buffName, 0};
+  GoString Dir = {buffDir, 0};
+  unsigned char filePath[BUFFER_SIZE];
+  unsigned char goldenFileURL[BUFFER_SIZE];
+
   for (int i = 0; i < lenStruct; i++) {
+
     createTempWalletDir(tt[i].encrypted);
-    char output[BUFFER_SIZE];
-    Config__Handle configHandle;
-    App__Handle appHandle;
-    GoUint32 errcode;
 
     errcode = SKY_cli_LoadConfig(&configHandle);
     cr_assert(errcode == SKY_OK, "SKY_cli_LoadConfig failed");
@@ -119,6 +140,7 @@ Test(api_cli_integration, TestGenerateAddresses) {
     redirectStdOut();
     GoString Args = {tt[i].args, strlen(tt[i].args)};
     errcode = SKY_cli_App_Run(&appHandle, Args);
+
     // Get redirected standard output
     getStdOut(output, BUFFER_SIZE);
     cr_assert(errcode == SKY_OK, "SKY_cli_App_Run failed");
@@ -128,32 +150,39 @@ Test(api_cli_integration, TestGenerateAddresses) {
 
     cr_assert(eq(u8, output, tt[i].expectOutput));
 
-    json_value *w;
-    char buffName[BUFFER_SIZE];
-    char buffDir[BUFFER_SIZE];
-    GoString WalletName = {"WALLET_NAME", 11};
-    GoString WalletPath = {"WALLET_DIR", 10};
-    GoString pathname = {buffName, 0};
-    GoString Dir = {buffDir, 0};
     SKY_cli_Getenv(WalletPath, ((GoString_ *)&Dir));
     SKY_cli_Getenv(WalletName, ((GoString_ *)&pathname));
-    unsigned char filePath[BUFFER_SIZE];
+
     strcpy(filePath, Dir.p);
     strcat(filePath, "/");
     strcat(filePath, pathname.p);
+    printf(" filePath %s \n", filePath);
     w = loadJsonFile(filePath);
-    cr_assert(w != NULL, "Failed to json parse");
+    cr_assert(w != NULL, "Failed to json parse in test :%s", tt[i].name);
     registerJsonFree(w);
 
-    json_value *expect;
-    unsigned char goldenFileURL[BUFFER_SIZE];
     strcpy(goldenFileURL, TEST_DATA_DIR);
-    strcat(goldenFileURL, "/");
+    // strcat(goldenFileURL, "/");
     strcat(goldenFileURL, tt[i].goldenFile);
+    printf(" goldenFileURL %s \n", goldenFileURL);
     expect = loadJsonFile(goldenFileURL);
-    cr_assert(expect != NULL, "Failed to json parse");
+    cr_assert(expect != NULL, "Failed to json parse in test :%s", tt[i].name);
     registerJsonFree(expect);
+    FILE *f1;
+    FILE *f2;
 
-    cr_assert(compareJsonValues(expect, w) == 1);
+    f1 = fopen(filePath, "rb");
+    f2 = fopen(goldenFileURL, "rb");
+    int a;
+    int b;
+    do {
+      a = fgetc(f1);
+      b = fgetc(f2);
+
+      printf("%d == %d\n", a, b);
+    } while (a == b && a != EOF);
+    cr_assert(a == b, "Not eq file %s and %s", filePath, goldenFileURL);
+    cr_assert(compareJsonValues(expect, w) == 1, "compareJsonValues  in : %s",
+              tt[i].name);
   }
 };

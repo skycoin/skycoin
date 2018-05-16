fdf2dd83c7d7d41d94b46daec49d9b3afb5133fe
stdevEclipse <dev0003@simelo.tech>
2018-05-16 15:57:35 -0400

    [libc] refs #1191 Added test TestStableOutputs. [====] Synthesis: Tested: 3 | Passing: 3 | Failing: 0 | Crashing: 0.

--- lib/cgo/tests/api_integration/check_api.integration.c
+++ lib/cgo/tests/api_integration/check_api.integration.c
@@ -20,6 +20,15 @@
 #define JSON_BIG_FILE_SIZE 32768
 #define TEST_DATA_DIR "src/api/integration/testdata/"
 
+typedef struct{
+	char*  	golden_file;
+	int		addresses_count;
+	char**	addresses;
+	int 	hashes_count;
+	char**  hashes;
+	int		failure;
+}test_out_put;
+
 char* getNodeAddress(){
 	if( STABLE ){
 		return NODE_ADDRESS_DEFAULT;
@@ -45,6 +54,20 @@ json_value* loadGoldenFile(const char* file){
 		return loadJsonFile(path);
 	}
 	return NULL;
+}
+
+GoString* createGoStringSlice(char** pStrings, int count, GoSlice* slice){
+	GoString* goStrings = malloc(sizeof(GoString) * count);
+	cr_assert(goStrings != NULL, "Error creating GoString Slice");
+	registerMemCleanup( goStrings );
+	for(int i = 0; i < count; i++){
+		goStrings[i].p = pStrings[i];
+		goStrings[i].n = strlen(pStrings[i]);
+	}
+	slice->data = goStrings;
+	slice->len = count;
+	slice->cap = count;
+	return goStrings;
 }
 
 Test(api_integration, TestVersion) {
@@ -104,4 +127,83 @@ Test(api_integration, TestStableCoinSupply) {
 }
 
 Test(api_integration, TestStableOutputs) {
+	int result;
+	GoString_ jsonResult;
+	memset(&jsonResult, 0, sizeof(GoString_));
+	
+	char* pNodeAddress = getNodeAddress();
+	GoString nodeAddress = {pNodeAddress, strlen(pNodeAddress)};
+	Client__Handle clientHandle;
+	
+	result = SKY_api_NewClient(nodeAddress, &clientHandle);
+	cr_assert(result == SKY_OK, "Couldn\'t create client");
+	registerHandleClose( clientHandle );
+	
+	char* address_1[] = {
+		"ALJVNKYL7WGxFBSriiZuwZKWD4b7fbV1od",
+		"2THDupTBEo7UqB6dsVizkYUvkKq82Qn4gjf",
+		"qxmeHkwgAMfwXyaQrwv9jq3qt228xMuoT5",
+	};
+	
+	char* hashes_1[] = {
+		"9e53268a18f8d32a44b4fb183033b49bebfe9d0da3bf3ef2ad1d560500aa54c6",
+		"d91e07318227651129b715d2db448ae245b442acd08c8b4525a934f0e87efce9",
+		"01f9c1d6c83dbc1c993357436cdf7f214acd0bfa107ff7f1466d1b18ec03563e",
+		"fe6762d753d626115c8dd3a053b5fb75d6d419a8d0fb1478c5fffc1fe41c5f20",
+	};
+	int test_cases = 3;
+	test_out_put tests[] = {
+		{
+			"outputs-noargs.golden",
+			0, NULL, 0, NULL, 0
+		},
+		{
+			"outputs-addrs.golden",
+			3, address_1,
+			0, NULL, 0
+		},
+		{
+			"outputs-hashes.golden",
+			0, NULL,
+			4, hashes_1, 0
+		},
+	};
+	Handle outputHandle;
+	GoSlice strings;
+	
+	for(int i = 0; i < test_cases; i++){
+		memset(&strings, 0, sizeof(GoSlice));
+		cr_assert(tests[i].addresses_count == 0 || tests[i].hashes_count == 0);
+		if(tests[i].addresses_count == 0 && tests[i].hashes_count == 0){
+			result = SKY_api_Client_Outputs(&clientHandle, &outputHandle);
+		} else if(tests[i].addresses_count > 0){
+			createGoStringSlice(tests[i].addresses, 
+					tests[i].addresses_count, &strings);
+			result = SKY_api_Client_OutputsForAddresses(&clientHandle, 
+														strings, &outputHandle);
+		} else if(tests[i].hashes_count > 0){
+			createGoStringSlice(tests[i].hashes, 
+					tests[i].hashes_count, &strings);
+			result = SKY_api_Client_OutputsForHashes(&clientHandle, 
+														strings, &outputHandle);
+		}
+		cr_assert(result == SKY_OK, "SKY_api_Client_Outputs failed");
+		registerHandleClose( outputHandle );
+		
+		result = SKY_JsonEncode_Handle(outputHandle, &jsonResult);
+		cr_assert(result == SKY_OK, "Couldn\'t json encode");
+		registerMemCleanup((void*)jsonResult.p);
+		
+		json_char* json = (json_char*)jsonResult.p;
+		json_value* jsonOutput = json_parse(json, strlen(jsonResult.p));
+		cr_assert(jsonOutput != NULL, "json_parse failed");
+		registerJsonFree(jsonOutput);
+		
+		json_value* json_golden = loadGoldenFile(tests[i].golden_file);
+		cr_assert(json_golden != NULL, "loadGoldenFile failed");
+		registerJsonFree(json_golden);
+		
+		int equal = compareJsonValues(jsonOutput, json_golden);
+		cr_assert(equal, "Output different than expected");
+	}
 }

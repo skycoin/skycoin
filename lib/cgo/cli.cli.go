2b2f214ee6d40229c7fea88b68e1ab8b8877f08e
stdevEclipse <dev0003@simelo.tech>
2018-05-26 20:04:53 +0000

    [libc] refs #1191. Modified SKY_cli_LoadConfig to return handle as function result. [====] Synthesis: Tested: 128 | Passing: 128 | Failing: 0 | Crashing: 0.

--- lib/cgo/cli.cli.go
+++ lib/cgo/cli.cli.go
@@ -17,15 +17,10 @@ import (
 import "C"
 
 //export SKY_cli_LoadConfig
-func SKY_cli_LoadConfig(_arg0 *C.Config__Handle) (____error_code uint32) {
-	____error_code = 0
-	defer func() {
-		____error_code = catchApiPanic(____error_code, recover())
-	}()
+func SKY_cli_LoadConfig() (handle C.Config__Handle) {
 	__arg0, ____return_err := cli.LoadConfig()
-	____error_code = libErrorCode(____return_err)
 	if ____return_err == nil {
-		*_arg0 = registerConfigHandle(&__arg0)
+		handle = registerConfigHandle(&__arg0)
 	}
 	return
 }

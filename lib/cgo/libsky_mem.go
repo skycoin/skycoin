1d41f00e54c0878f8e943e3c59a5899b2279c7ff
Olemis Lang <olemis+work@gmail.com>
2018-06-02 14:48:15 -0400

    [lib] refs #992 - Fix lint errors caused by C.memcpy unsafe.Pointer returning param 0
    [====] Synthesis: Tested: 51 | Passing: 50 | Failing: 1 | Crashing: 1

--- lib/cgo/libsky_mem.go
+++ lib/cgo/libsky_mem.go
@@ -48,15 +48,22 @@ func inplaceAddress(p *C.cipher__Address) *cipher.Address {
 	return (*cipher.Address)(unsafe.Pointer(p))
 }
 
+func nop(p unsafe.Pointer) {
+	// Do nothing
+}
+
 /**
  * Copy helpers
  */
 
 func copyString(src string, dest *C.GoString_) {
-	srcLen := len(src)
-	dest.p = (*C.char)(C.malloc(C.size_t(srcLen + 1)))
 	strAddr := (*C.GoString_)(unsafe.Pointer(&src))
-	C.memcpy(unsafe.Pointer(dest.p), unsafe.Pointer(strAddr.p), C.size_t(srcLen))
+	srcLen := len(src)
+	dest.p = (*C.char)(C.memcpy(
+		C.malloc(C.size_t(srcLen+1)),
+		unsafe.Pointer(strAddr.p),
+		C.size_t(srcLen),
+	))
 	C.eos(dest.p, C.int(srcLen))
 	dest.n = C.GoInt_(srcLen)
 }
@@ -82,7 +89,7 @@ func copyToBuffer(src reflect.Value, dest unsafe.Pointer, n uint) {
 		return
 	}
 	srcAddr, elemSize := getBufferData(src)
-	C.memcpy(dest, srcAddr, C.size_t(n)*elemSize)
+	nop(C.memcpy(dest, srcAddr, C.size_t(n)*elemSize))
 }
 
 // Copy source slice/array/string onto instance of C.GSlice struct
@@ -106,7 +113,7 @@ func copyToGoSlice(src reflect.Value, dest *C.GoSlice_) {
 	if overflow {
 		n = int(dest.cap)
 	}
-	C.memcpy(dest.data, srcAddr, C.size_t(n)*elemSize)
+	nop(C.memcpy(dest.data, srcAddr, C.size_t(n)*elemSize))
 	// Do not modify slice metadata until memory is actually copied
 	if overflow {
 		dest.len = dest.cap - C.GoInt_(srcLen)

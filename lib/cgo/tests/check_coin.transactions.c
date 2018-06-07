9178e92f62ffea288b7fd6e8cfb19c5084d36c1d
Maykel Arias Torres <dev0006@simelo.tech>
2018-06-07 17:06:41 -0400

    [libc] ref #1191 finish TestTransactionVerify  [====] Synthesis: Tested: 92 | Passing: 92 | Failing: 0 | Crashing: 0

--- lib/cgo/tests/check_coin.transactions.c
+++ lib/cgo/tests/check_coin.transactions.c
@@ -18,6 +18,7 @@ Test(coin_transaction, TestTransactionVerify)
 
   // Mismatch header hash
   coin__Transaction tx;
+  coin__TransactionOutput* to_void;
   makeTransaction(&tx);
   memset(&tx.InnerHash, 0, sizeof(cipher__SHA256));
   GoUint32 errcode = SKY_coin_Transaction_Verify(&tx);
@@ -52,7 +53,7 @@ Test(coin_transaction, TestTransactionVerify)
   errcode = makeTransaction(&tx);
   cr_assert(errcode == SKY_OK);
   GoSlice slice = { NULL, 20, 20 };
-  memset(&tx.Sigs.data, 0, sizeof(slice));
+  memset(&tx.Sigs, 0, sizeof(slice));
   errcode = SKY_coin_Transaction_UpdateHeader(&tx);
   errcode = SKY_coin_Transaction_Verify(&tx);
   cr_assert(errcode != SKY_OK, "Invalid number of signatures");
@@ -75,22 +76,142 @@ Test(coin_transaction, TestTransactionVerify)
   makeTransactionFromUxOut(&ux, &s, &tx);
   cipher__SHA256 sha256;
   SKY_coin_Transaction_Hash(&tx, &sha256);
-  SKY_coin_Transaction_PushInput(&tx, &sha256 , 0);
+  SKY_coin_Transaction_PushInput(&tx, &sha256, 0);
   tx.Sigs.data = NULL;
   GoSlice slice_duplicate;
-  copySlice( ((GoSlice*)&slice_duplicate.data), (GoSlice*)&tx.In.data,sizeof(cipher__SecKey) );
+  copySlice(((GoSlice*)&slice_duplicate.data),
+            (GoSlice*)&tx.In.data,
+            sizeof(cipher__SecKey));
   SKY_coin_Transaction_SignInputs(&tx, slice_duplicate);
   errcode = SKY_coin_Transaction_UpdateHeader(&tx);
   errcode = SKY_coin_Transaction_Verify(&tx);
   cr_assert(errcode != SKY_OK, "Duplicate spend");
 
+  // Duplicate outputs
+  makeTransaction(&tx);
+  coin__TransactionOutput to;
+  to = (*(coin__TransactionOutput*)&tx.Out);
+  errcode = SKY_coin_Transaction_PushOutput(
+    &to, ((cipher__Address*)&to.Address), to.Coins, to.Hours);
+  errcode = SKY_coin_Transaction_UpdateHeader(&tx);
+  errcode = SKY_coin_Transaction_Verify(&tx);
+  cr_assert(errcode != SKY_OK, "Duplicate output in transaction");
+
+  // Invalid signature, empty
+  makeTransaction(&tx);
+  memset(&tx.Sigs, 0, sizeof(cipher__Sig));
+  errcode = SKY_coin_Transaction_Verify(&tx);
+  cr_assert(errcode != SKY_OK, "Duplicate spend");
+
+  // Output coins are 0
+
+  makeTransaction(&tx);
+  memset(&tx.Out, 0, sizeof(coin__TransactionOutput));
+  to_void = ((coin__TransactionOutput*)&tx.Out);
+  to_void->Coins = 0;
+  errcode = SKY_coin_Transaction_UpdateHeader(&tx);
+  errcode = SKY_coin_Transaction_Verify(&tx);
+  cr_assert(errcode != SKY_OK, "Zero coin output");
+
+  // Output coin overflow
+  makeTransaction(&tx);
+  memset(&tx.Out, 0, sizeof(coin__TransactionOutput));
+  to_void = ((coin__TransactionOutput*)&tx.Out);
+  to_void->Coins = 9223372036851775808;
+  errcode = SKY_coin_Transaction_UpdateHeader(&tx);
+  errcode = SKY_coin_Transaction_Verify(&tx);
+  cr_assert(errcode != SKY_OK, "Output coins overflow");
+
+  // Output coins are not multiples of 1e6 (valid, decimal restriction is not
+  // enforced here)
+
+  memset(&tx.Out, 0, sizeof(coin__TransactionOutput));
+  makeTransaction(&tx);
+  to = (*(coin__TransactionOutput*)&tx.Out);
+  to.Coins += 10;
+  tx.Sigs.data = NULL;
+  errcode = SKY_coin_Transaction_UpdateHeader(&tx);
+  SKY_coin_Transaction_PushInput(&tx, &sha256, 0);
+  GoSlice slice_decimal;
+  memset(&slice_decimal.data, 0, sizeof(cipher__SecKey));
+  SKY_coin_Transaction_SignInputs(&tx, slice_decimal);
+  cr_assert(0 != (to.Coins % ((GoUint64_)1.000000e+006)));
+  errcode = SKY_coin_Transaction_Verify(&tx);
+  cr_assert(errcode != SKY_OK);
+
+  // Valid
+  memset(&tx.Out, 0, sizeof(coin__TransactionOutput));
+  makeTransaction(&tx);
+  to_void = ((coin__TransactionOutput*)&tx.Out);
+  to_void->Coins = 1.000000e+007;
+  to_void++;
+  to_void->Coins = 1.000000e+006;
+  errcode = SKY_coin_Transaction_UpdateHeader(&tx);
+  errcode = SKY_coin_Transaction_Verify(&tx);
+  cr_assert(errcode == SKY_OK);
 }
 
-// Test(coin_transaction, TestTransactionPushInput)
-// {
+Test(coin_transaction, TestTransactionVerifyInput)
+{
+  coin__Transaction tx;
+  GoUint64_ errcode;
+  coin__UxArray uxArray;
 
-//   coin__Transaction tx;
-//   coin__UxOut ux;
-//   int errcode = makeUxOut(&ux);
-//   cr_assert(errcode == SKY_OK);
-// }
+  // Invalid uxIn args
+  makeTransaction(&tx);
+  cli__PasswordFromBytes seckey;
+  SKY_coin_UxArray_Coins(&seckey, NULL);
+  errcode = SKY_coin_Transaction_VerifyInput(&tx, &seckey);
+  cr_assert(errcode != SKY_OK, "tx.In != uxIn");
+  SKY_coin_UxArray_Coins(&seckey, 0);
+  errcode = SKY_coin_Transaction_VerifyInput(&tx, &seckey);
+  cr_assert(errcode != SKY_OK, "tx.In != uxIn");
+  SKY_coin_UxArray_Coins(&seckey, 3);
+  errcode = SKY_coin_Transaction_VerifyInput(&tx, &seckey);
+  cr_assert(errcode != SKY_OK, "tx.In != uxIn");
+
+  // 	// tx.In != tx.Sigs
+  // ux, s := makeUxOutWithSecret(t)
+  // tx = makeTransactionFromUxOut(ux, s)
+  // tx.Sigs = []cipher.Sig{}
+  // _require.PanicsWithLogMessage(t, "tx.In != tx.Sigs", func() {
+  // 	tx.VerifyInput(UxArray{ux})
+  // })
+
+  // ux, s = makeUxOutWithSecret(t)
+  // tx = makeTransactionFromUxOut(ux, s)
+  // tx.Sigs = append(tx.Sigs, cipher.Sig{})
+  // _require.PanicsWithLogMessage(t, "tx.In != tx.Sigs", func() {
+  // 	tx.VerifyInput(UxArray{ux})
+  // })
+
+  // tx.InnerHash != tx.HashInner()
+  coin__UxOut ux;
+  cipher__SecKey s;
+  errcode = makeUxOutWithSecret(&ux, &s);
+  cr_assert(errcode == SKY_OK);
+  errcode = makeTransactionFromUxOut(&ux, &s, &tx);
+  cr_assert(errcode == SKY_OK);
+  memset(&tx.Sigs, 0, sizeof(cipher__Sig));
+  memset(&uxArray, 0, sizeof(coin__UxArray));
+  uxArray.data = &ux;
+  SKY_coin_UxArray_Coins(&uxArray, 1);
+  errcode = SKY_coin_Transaction_VerifyInput(&tx, &uxArray);
+  cr_assert(errcode != SKY_OK, "tx.In != tx.Sigs");
+
+  errcode = makeUxOutWithSecret(&ux, &s);
+  cr_assert(errcode == SKY_OK);
+  errcode = makeTransactionFromUxOut(&ux, &s, &tx);
+  cr_assert(errcode == SKY_OK);
+
+  coin__UxOut uxo;
+  coin__UxArray uxa;
+  uxa.data = uxo.;
+}
+
+Test(coin_transaction, TestTransactionPushInput)
+{
+  coin__Transaction tx;
+  memset(&tx, 0, sizeof(coin__Transaction));
+  coin__UxOut makeUxOut()
+}

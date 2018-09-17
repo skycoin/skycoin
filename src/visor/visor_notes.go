c807dde5daffce791c172919d39f8bce324dc5eb
jonathansx <jnsaxen@protonmail.com>
2018-09-17 12:37:15 +0200

    recommit

--- src/visor/visor_notes.go
+++ src/visor/visor_notes.go
@@ -4,14 +4,13 @@ import (
 	"github.com/skycoin/skycoin/src/notes"
 )
 
-
 // GetAllNotes returns all saved Notes
 func (v Visor) GetAllNotes() []notes.Note {
 	return v.Notes.GetAll()
 }
 
 // GetNoteByTxID If note wasn't found by Id -> return empty Note
-func (v Visor) GetNoteByTxID(txID string) (notes.Note) {
+func (v Visor) GetNoteByTxID(txID string) notes.Note {
 	return v.Notes.GetByTxID(txID)
 }
 
@@ -20,7 +19,7 @@ func (v Visor) AddNote(note notes.Note) error {
 	return v.Notes.Add(note)
 }
 
-// Remove Note by TransactionId
+// RemoveNote by TransactionId
 func (v Visor) RemoveNote(txId string) error {
 	return v.Notes.RemoveByTxID(txId)
 }

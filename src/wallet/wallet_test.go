12a90f933db803777d4cb9bf1bf4400cdd51ce09
Jonathan-S <jnsaxen@protonmail.com>
2018-09-21 22:33:29 +0200

    Refactoring

--- src/wallet/wallet_test.go
+++ src/wallet/wallet_test.go
@@ -747,20 +747,6 @@ func TestLoadDupWallets(t *testing.T) {
 	}
 }
 
-// GenerateRandomBytes returns securely generated random bytes.
-// It will return an error if the system's secure random
-// number generator fails to function correctly, in which
-// case the caller should not continue.
-func generateRandomBytes(n int) ([]byte, error) {
-	b := make([]byte, n)
-	_, err := rand.Read(b)
-	if err != nil {
-		return nil, err
-	}
-
-	return b, nil
-}
-
 func TestWalletGenerateAddress(t *testing.T) {
 	tt := []struct {
 		name               string

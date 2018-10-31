efdae31443a9b351b6dcc1d349fd7dccec2bdaea
Olemis Lang <olemis+work@gmail.com>
2018-10-31 14:56:56 -0400

    [ci] refs #1996 - Install gcc@6 deps' binary bottles and gcc@6 itself from sources

---
+++ ci-scripts/install-travis-gcc.sh
@@ -0,0 +1,15 @@
+
+# Install gcc6 (6.4.0-2 on Mac OS) for Travis builds
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+  sudo apt-get install -qq g++-6 && sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 90;
+fi
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  brew update
+  echo 'Available versions (gcc)' && brew list --versions gcc
+  brew list gcc@6 &>/dev/null || (echo "Install gcc6 deps" && (brew deps -n gcc@6 | xargs -I NAME bash -c 'brew install NAME') && echo "Install gcc@6 from sources" && HOMEBREW_BUILD_FROM_SOURCE=1 brew install gcc@6 )
+fi
+
+cd $TRAVIS_BUILD_DIR
+

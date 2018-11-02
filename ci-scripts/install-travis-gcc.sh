407202622fa1e2081d7c793863e22a0c3cab513f
Olemis Lang <olemis+work@gmail.com>
2018-11-02 00:25:35 -0400

    [ci] refs #1996 - Create and brew install new formula gcc@64

--- ci-scripts/install-travis-gcc.sh
+++ ci-scripts/install-travis-gcc.sh
@@ -7,8 +7,13 @@ fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   brew update
-  echo 'Available versions (gcc)' && brew list --versions gcc
-  brew list gcc@7 &>/dev/null || brew install gcc@6 || brew link --overwrite gcc@6
+  echo 'Available versions (gcc)'
+  brew list --versions gcc
+  echo 'Installing gcc@64 formula'
+  cd "$(brew --repository)/Library/Taps/homebrew/homebrew-core"
+  git show 42d31bba7772fb01f9ba442d9ee98b33a6e7a055:Formula/gcc\@6.rb > Formula/gcc\@64.rb
+  sed -i '' -e 's/GccAT6/GccAT64/g' Formula/gcc\@64.rb
+  brew install gcc@64 || brew link --overwrite gcc\@64
 fi
 
 cd $TRAVIS_BUILD_DIR

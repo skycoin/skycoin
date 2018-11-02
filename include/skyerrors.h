51c921057a32462ee0474a2a45e61d2997ee54f3
Olemis Lang <olemis+work@gmail.com>
2018-11-02 02:36:31 -0400

    [lib] refs #1191 - Rename sha256xor error vars
    ... as specified in review https://github.com/skycoin/skycoin/pull/1639#pullrequestreview-170955680
    
    ErrSHA256orMissingPassword => ErrMissingPassword
    ErrLenghtDataOverflowMaxUint32 => ErrDataTooLarge
    ErrInvalidDataChecksumNotMatched => ErrInvalidChecksum
    ErrInvalidBlockSizeMultiple32Bytes => ErrInvalidBlockSize
    ErrSHA256orInvalidPassword => ErrInvalidPassword
    ErrReadDataHashFailedLength => ErrReadDataHashFailed
    
    [====] Synthesis: Tested: 137 | Passing: 137 | Failing: 0 | Crashing: 0

--- include/skyerrors.h
+++ include/skyerrors.h
@@ -84,14 +84,14 @@
 #define SKY_ErrBitcoinWIFInvalidChecksum            0x02000028
 #define SKY_ErrEmptySeed                            0x02000029
 #define SKY_ErrInvalidSig                           0x0200002A
-#define SKY_ErrSHA256orMissingPassword              0x0200002B
-#define SKY_ErrLenghtDataOverflowMaxUint32          0x0200002C
+#define SKY_ErrMissingPassword                      0x0200002B
+#define SKY_ErrDataTooLarge                         0x0200002C
 #define SKY_ErrInvalidChecksumLength                0x0200002D
-#define SKY_ErrInvalidDataChecksumNotMatched        0x0200002E
+#define SKY_ErrInvalidChecksum                      0x0200002E
 #define SKY_ErrInvalidNonceLength                   0x0200002F
-#define SKY_ErrInvalidBlockSizeMultiple32Bytes      0x02000030
-#define SKY_ErrReadDataHashFailedLength             0x02000031
-#define SKY_ErrSHA256orInvalidPassword              0x02000032
+#define SKY_ErrInvalidBlockSize                     0x02000030
+#define SKY_ErrReadDataHashFailed                   0x02000031
+#define SKY_ErrInvalidPassword                      0x02000032
 #define SKY_ErrReadDataLengthFailed                 0x02000033
 #define SKY_ErrInvalidDataLength                    0x02000034
 
@@ -171,9 +171,9 @@
 #define SKY_ErrInvalidEncryptedField                  0x0B000004
 #define SKY_ErrWalletEncrypted                        0x0B000005
 #define SKY_ErrWalletNotEncrypted                     0x0B000006
-#define SKY_ErrMissingPassword                        0x0B000007
+#define SKY_ErrWalletMissingPassword                  0x0B000007
 #define SKY_ErrMissingEncrypt                         0x0B000008
-#define SKY_ErrInvalidPassword                        0x0B000009
+#define SKY_ErrWalletInvalidPassword                  0x0B000009
 #define SKY_ErrMissingSeed                            0x0B00000A
 #define SKY_ErrMissingAuthenticated                   0x0B00000B
 #define SKY_ErrWrongCryptoType                        0x0B00000C

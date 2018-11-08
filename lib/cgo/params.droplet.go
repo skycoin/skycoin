e221ddaa88aee339af284816bbee6d378829e187
Olemis Lang <olemis+work@gmail.com>
2018-11-07 20:11:40 -0500

    [params] refs #1778 - libc wrappers for params.droplet
    [====] Synthesis: Tested: 107 | Passing: 107 | Failing: 0 | Crashing: 0

---
+++ lib/cgo/params.droplet.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"reflect"
+
+	params "github.com/skycoin/skycoin/src/params"
+)
+
+/*
+
+  #include <string.h>
+  #include <stdlib.h>
+
+  #include "skytypes.h"
+*/
+import "C"
+
+//export
+func SKY_params_MaxDropletDivisor() uint64 {
+	return params.MaxDropletDivisor()
+}
+
+//export
+func SKY_params_DropletPrecisionCheck(amount uint64) uint32 {
+	return libErrorCode(params.DropletPrecisionCheck(amount))
+}

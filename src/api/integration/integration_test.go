74918e6b4bd0ed5fd5cc2e6f7635eb6bfa3b94fb
gz-c <token@protonmail.com>
2019-02-10 14:10:29 +0800

    Fix tests

--- src/api/integration/integration_test.go
+++ src/api/integration/integration_test.go
@@ -5894,11 +5894,6 @@ func TestDisableWalletAPI(t *testing.T) {
 		}
 
 		t.Run(tc.name, f(tc))
-
-		if strings.HasPrefix(tc.endpoint, "/api/v1") {
-			tc.endpoint = strings.TrimPrefix(tc.endpoint, "/api/v1")
-			t.Run(tc.name, f(tc))
-		}
 	}
 
 	// Confirms that no new wallet is created

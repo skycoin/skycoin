ca002f32f77c71d7f436805aba5d760dbf099ab2
gz-c <token@protonmail.com>
2019-02-26 18:44:55 +0800

    Fix TestGetWalletSeedDisabledAPI check

--- src/api/integration/wallet_test.go
+++ src/api/integration/wallet_test.go
@@ -571,11 +571,11 @@ func TestGetWalletSeedDisabledAPI(t *testing.T) {
 		return
 	}
 
-	if doLive(t) && !doLiveWallet(t) {
-		return
+	if mode(t) == testModeLive && !*testLiveWallet {
+		t.Skip("Skipping tests because live mode enabled but wallet tests disabled")
 	}
 
-	if mode(t) != testModeEnableSeedAPI {
+	if mode(t) == testModeEnableSeedAPI {
 		t.Skip("Skipping because enable seed API tests is on")
 	}
 

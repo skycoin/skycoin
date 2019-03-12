608e120d0ee991d20a11e9adc8c8f535aa1a5007
iketheadore <2589926+iketheadore@users.noreply.github.com>
2019-03-12 20:38:09 +1100

    Hide osx keychain password

--- ci-scripts/add-key.sh
+++ ci-scripts/add-key.sh
@@ -4,15 +4,15 @@ set -e -o pipefail
 
 KEY_CHAIN=login.keychain
 echo "security create keychain"
-if ! security show-keychain-info $KEY_CHAIN ; then 
-	security create-keychain -p travis $KEY_CHAIN ;
+if ! security show-keychain-info $KEY_CHAIN ; then
+	security create-keychain -p $OSX_KEYCHAIN_PWD $KEY_CHAIN ;
 fi
 # Make the keychain the default so identities are found
 echo "security default-keychain"
 security default-keychain -s $KEY_CHAIN
 # Unlock the keychain
 echo "unlock the keychain"
-security unlock-keychain -p travis $KEY_CHAIN
+security unlock-keychain -p $OSX_KEYCHAIN_PWD $KEY_CHAIN
 # Set keychain locking timeout to 3600 seconds
 echo "set keychain locking timeout to 3600"
 security set-keychain-settings -t 3600 -u $KEY_CHAIN

8d5b238fa1af6c3f671011897e3b6c8aa2d9eb19
iketheadore <2589926+iketheadore@users.noreply.github.com>
2019-03-28 15:29:11 +1100

    Change time_since_last_block back to gauge type

--- src/api/metrics.go
+++ src/api/metrics.go
@@ -20,8 +20,8 @@ var (
 			Name: "unconfirmed_txns",
 			Help: "Number of unconfirmed transactions",
 		})
-	promTimeSinceLastBlock = prometheus.NewCounter(
-		prometheus.CounterOpts{
+	promTimeSinceLastBlock = prometheus.NewGauge(
+		prometheus.GaugeOpts{
 			Name: "time_since_last_block_seconds",
 			Help: "Time since the last block created",
 		})

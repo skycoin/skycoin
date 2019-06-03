f2073285d73cd678ddf20660d85d81815a0f6b1d
Amaury Hernandez <amherag@gmail.com>
2019-06-02 18:03:55 -0700

    Resolved a merge conflict that was not solved before commiting.

--- src/api/gateway.go
+++ src/api/gateway.go
@@ -35,23 +35,6 @@ func NewGateway(d *daemon.Daemon, v *visor.Visor, w *wallet.Service, m *kvstorag
 
 // Gatewayer interface for Gateway methods
 type Gatewayer interface {
-<<<<<<< HEAD
-	Spend(wltID string, password []byte, coins uint64, dest cipher.Address, mainExprs []byte) (*coin.Transaction, error)
-	CreateTransaction(w wallet.CreateTransactionParams) (*coin.Transaction, []wallet.UxBalance, error)
-	GetWalletBalance(wltID string) (wallet.BalancePair, wallet.AddressBalances, error)
-	GetWallet(wltID string) (*wallet.Wallet, error)
-	GetWallets() (wallet.Wallets, error)
-	UpdateWalletLabel(wltID, label string) error
-	GetWalletUnconfirmedTransactions(wltID string) ([]visor.UnconfirmedTransaction, error)
-	GetWalletUnconfirmedTransactionsVerbose(wltID string) ([]visor.UnconfirmedTransaction, [][]visor.TransactionInput, error)
-	CreateWallet(wltName string, options wallet.Options) (*wallet.Wallet, error)
-	RecoverWallet(wltID, seed string, password []byte) (*wallet.Wallet, error)
-	NewAddresses(wltID string, password []byte, n uint64) ([]cipher.Address, error)
-	GetWalletDir() (string, error)
-	EncryptWallet(wltID string, password []byte) (*wallet.Wallet, error)
-	DecryptWallet(wltID string, password []byte) (*wallet.Wallet, error)
-	GetWalletSeed(wltID string, password []byte) (string, error)
-=======
 	Daemoner
 	Visorer
 	Walleter
@@ -78,7 +61,6 @@ type Visorer interface {
 	HeadBkSeq() (uint64, bool, error)
 	GetBlockchainMetadata() (*visor.BlockchainMetadata, error)
 	ResendUnconfirmedTxns() ([]cipher.SHA256, error)
->>>>>>> develop
 	GetSignedBlockByHash(hash cipher.SHA256) (*coin.SignedBlock, error)
 	GetSignedBlockByHashVerbose(hash cipher.SHA256) (*coin.SignedBlock, [][]visor.TransactionInput, error)
 	GetSignedBlockBySeq(seq uint64) (*coin.SignedBlock, error)
@@ -91,28 +73,8 @@ type Visorer interface {
 	GetLastBlocksVerbose(num uint64) ([]coin.SignedBlock, [][][]visor.TransactionInput, error)
 	GetUnspentOutputsSummary(filters []visor.OutputsFilter) (*visor.UnspentOutputsSummary, error)
 	GetBalanceOfAddrs(addrs []cipher.Address) ([]wallet.BalancePair, error)
-<<<<<<< HEAD
-	GetBlockchainMetadata() (*visor.BlockchainMetadata, error)
-	GetBlockchainProgress() (*daemon.BlockchainProgress, error)
-	GetConnection(addr string) (*daemon.Connection, error)
-	GetConnections(f func(c daemon.Connection) bool) ([]daemon.Connection, error)
-	Disconnect(id uint64) error
-	GetDefaultConnections() []string
-	GetTrustConnections() []string
-	GetExchgConnection() []string
-	GetAllUnconfirmedTransactions() ([]visor.UnconfirmedTransaction, error)
-	GetAllUnconfirmedTransactionsVerbose() ([]visor.UnconfirmedTransaction, [][]visor.TransactionInput, error)
-	GetTransaction(txid cipher.SHA256) (*visor.Transaction, error)
-	GetTransactionVerbose(txid cipher.SHA256) (*visor.Transaction, []visor.TransactionInput, error)
-	GetTransactions(flts []visor.TxFilter) ([]visor.Transaction, error)
-	GetTransactionsVerbose(flts []visor.TxFilter) ([]visor.Transaction, [][]visor.TransactionInput, error)
-	GetProgramState(flts []visor.TxFilter) ([]byte, error)
-	InjectBroadcastTransaction(txn coin.Transaction) error
-	ResendUnconfirmedTxns() ([]cipher.SHA256, error)
-=======
 	VerifyTxnVerbose(txn *coin.Transaction, signed visor.TxnSignedFlag) ([]visor.TransactionInput, bool, error)
 	AddressCount() (uint64, error)
->>>>>>> develop
 	GetUxOutByID(id cipher.SHA256) (*historydb.UxOut, error)
 	GetSpentOutputsForAddresses(addr []cipher.Address) ([][]historydb.UxOut, error)
 	GetVerboseTransactionsForAddress(a cipher.Address) ([]visor.Transaction, [][]visor.TransactionInput, error)

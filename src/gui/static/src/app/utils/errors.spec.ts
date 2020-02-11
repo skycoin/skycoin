df750e0e56e7ded03be23f9f6721b4d9aa619c06
Senyoret1 <34079003+Senyoret1@users.noreply.github.com>
2020-02-11 19:56:30 -0400

    Fix the unit tests of the desktop wallet

--- src/gui/static/src/app/utils/errors.spec.ts
+++ src/gui/static/src/app/utils/errors.spec.ts
@@ -1,13 +1,13 @@
 import { processErrorMsg } from './errors';
 
 describe('errors', () => {
-  const message1 = '400 Bad Request - error description';
-  const message2 = '403 Forbidden - error description';
-  const message3 = '500 Internal Server Error - error description';
+  const message1 = '400 Bad Request - error description.';
+  const message2 = '403 Forbidden - error description.';
+  const message3 = '500 Internal Server Error - error description.';
 
   it('parses message from 400 and 403 responses', () => {
-    expect(processErrorMsg(message1)).toEqual('Error description');
-    expect(processErrorMsg(message2)).toEqual('Error description');
+    expect(processErrorMsg(message1)).toEqual('Error description.');
+    expect(processErrorMsg(message2)).toEqual('Error description.');
   });
 
   it('does not parse message from other responses', () => {

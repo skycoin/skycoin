a111d767f24304a036e06d102c7e14b1d1603c75
iketheadore <2589926+iketheadore@users.noreply.github.com>
2020-03-05 00:56:49 +1100

    Set db verify check point to 0.27.0

--- src/skycoin/skycoin.go
+++ src/skycoin/skycoin.go
@@ -39,7 +39,7 @@ var (
 	// DBVerifyCheckpointVersion is a checkpoint for determining if DB verification should be run.
 	// Any DB upgrading from less than this version to equal or higher than this version will be forced to verify.
 	// Update this version checkpoint if a newer version requires a new verification run.
-	DBVerifyCheckpointVersion       = "0.25.0"
+	DBVerifyCheckpointVersion       = "0.27.0"
 	dbVerifyCheckpointVersionParsed semver.Version
 )
 
@@ -608,22 +608,6 @@ func createDirIfNotExist(dir string) error {
 	}
 
 	return os.Mkdir(dir, 0750)
-}
-
-func shouldVerifyDB(appVersion, dbVersion, checkpointVersion *semver.Version) bool {
-	// If the dbVersion is not set, verify
-	if dbVersion == nil {
-		return true
-	}
-
-	// If the dbVersion is less than the verification checkpoint version
-	// and the appVersion is greater than or equal to the checkpoint version,
-	// verify
-	if dbVersion.LT(*checkpointVersion) && appVersion.GTE(*checkpointVersion) {
-		return true
-	}
-
-	return false
 }
 
 func init() {

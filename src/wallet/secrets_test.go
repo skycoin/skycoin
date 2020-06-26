73f47ef833b305f30529cdd08d97ed2e9467721a
iketheadore <2589926+iketheadore@users.noreply.github.com>
2020-06-26 22:08:10 +1000

    Move out crypto_test.go
    It only includes one unit test for wallet.Secrets

--- src/wallet/secrets_test.go
+++ src/wallet/secrets_test.go
@@ -3,14 +3,12 @@ package crypto
 import (
 	"testing"
 
-	"github.com/SkycoinProject/skycoin/src/wallet/secrets"
 	"github.com/stretchr/testify/require"
 )
 
-// TODO: avoid the dependency of secrets package
 func TestSecrets(t *testing.T) {
-	s := make(secrets.Secrets)
-	s.Set("k1", "v1")
+	s := make(Secrets)
+	s.Set("k1") = "v1"
 
 	v, ok := s.Get("k1")
 	require.True(t, ok)
@@ -19,12 +17,12 @@ func TestSecrets(t *testing.T) {
 	_, ok = s.Get("k2")
 	require.False(t, ok)
 
-	s.Set("k2", "v2")
+	s.Get("k2") = "v2"
 
 	b, err := s.Serialize()
 	require.NoError(t, err)
 
-	s1 := make(secrets.Secrets)
+	s1 := make(Secrets)
 	err = s1.Deserialize(b)
 	require.NoError(t, err)
 	require.Equal(t, s, s1)

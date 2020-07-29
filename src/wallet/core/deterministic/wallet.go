0fd1d1a9fc13e4f2c2f42d54967fe4c352d9810b
iketheadore <2589926+iketheadore@users.noreply.github.com>
2020-07-29 20:25:25 +1000

    Return wallet.ErrInvalidWalletType for deterministic wallet

--- src/wallet/core/deterministic/wallet.go
+++ src/wallet/core/deterministic/wallet.go
@@ -106,7 +106,7 @@ func NewWallet(filename, label, seed string, options ...wallet.Option) (*Wallet,
 
 func validateMeta(m wallet.Meta) error {
 	if m[wallet.MetaType] != WalletType {
-		return errors.New("invalid wallet type")
+		return wallet.ErrInvalidWalletType
 	}
 
 	if err := wallet.ValidateMeta(m); err != nil {

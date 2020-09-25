56398d1cbff4c0114dd6d08377a99dfdec009098
iketheadore <2589926+iketheadore@users.noreply.github.com>
2020-09-25 22:11:57 +1000

    Register wallet for integration tests

--- src/api/integration/wallet_test.go
+++ src/api/integration/wallet_test.go
@@ -18,6 +18,12 @@ import (
 	"github.com/SkycoinProject/skycoin/src/testutil"
 	"github.com/SkycoinProject/skycoin/src/wallet"
 	"github.com/SkycoinProject/skycoin/src/wallet/crypto"
+
+	// register the supported wallets
+	_ "github.com/SkycoinProject/skycoin/src/wallet/bip44wallet"
+	_ "github.com/SkycoinProject/skycoin/src/wallet/collection"
+	_ "github.com/SkycoinProject/skycoin/src/wallet/deterministic"
+	_ "github.com/SkycoinProject/skycoin/src/wallet/xpubwallet"
 )
 
 func skipWalletIfLive(t *testing.T) bool {

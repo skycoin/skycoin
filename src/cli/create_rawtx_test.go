iketheadore/skycoinproject-skycoin

Fix unit test errors caused by the changing of coin hour burn factor

src/cli/create_rawtx_test.go
@@ -7,11 +7,16 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/SkycoinProject/skycoin/src/cipher"
+	"github.com/SkycoinProject/skycoin/src/params"
 	"github.com/SkycoinProject/skycoin/src/readable"
 	"github.com/SkycoinProject/skycoin/src/testutil"
 	"github.com/SkycoinProject/skycoin/src/transaction"
 	"github.com/SkycoinProject/skycoin/src/util/fee"
 )
+
+func TestMain(m *testing.M) {
+	params.UserVerifyTxn.BurnFactor = 10
+}
 
 func TestMakeChangeOut(t *testing.T) {
 	// single destination test

src/transaction/create_test.go
@@ -14,9 +14,15 @@ import (
 
 	"github.com/SkycoinProject/skycoin/src/cipher"
 	"github.com/SkycoinProject/skycoin/src/coin"
+	"github.com/SkycoinProject/skycoin/src/params"
 	"github.com/SkycoinProject/skycoin/src/testutil"
 	"github.com/SkycoinProject/skycoin/src/util/fee"
 )
+
+func TestMain(m *testing.M) {
+	// pin the burn factor to 10
+	params.UserVerifyTxn.BurnFactor = 10
+}
 
 func TestCreate(t *testing.T) {
 	headTime := uint64(time.Now().UTC().Unix())

src/wallet/transaction_test.go
@@ -15,10 +15,15 @@ import (
 
 	"github.com/SkycoinProject/skycoin/src/cipher"
 	"github.com/SkycoinProject/skycoin/src/coin"
+	"github.com/SkycoinProject/skycoin/src/params"
 	"github.com/SkycoinProject/skycoin/src/testutil"
 	"github.com/SkycoinProject/skycoin/src/transaction"
 	"github.com/SkycoinProject/skycoin/src/util/fee"
 )
+
+func TestMain(m *testing.M) {
+	params.UserVerifyTxn.BurnFactor = 10
+}
 
 func TestWalletSignTransaction(t *testing.T) {
 	txnSigned, uxs, seckeys := makeTransaction(t, 4)

skycoin/skycoin

src/cli/create_rawtx_test.go
@@ -7,11 +7,16 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/skycoin/skycoin/src/cipher"
+	"github.com/skycoin/skycoin/src/params"
 	"github.com/skycoin/skycoin/src/readable"
 	"github.com/skycoin/skycoin/src/testutil"
 	"github.com/skycoin/skycoin/src/transaction"
 	"github.com/skycoin/skycoin/src/util/fee"
 )
+
+func TestMain(m *testing.M) {
+	params.UserVerifyTxn.BurnFactor = 10
+}
 
 func TestMakeChangeOut(t *testing.T) {
 	// single destination test

src/transaction/create_test.go
@@ -14,9 +14,15 @@ import (
 
 	"github.com/skycoin/skycoin/src/cipher"
 	"github.com/skycoin/skycoin/src/coin"
+	"github.com/skycoin/skycoin/src/params"
 	"github.com/skycoin/skycoin/src/testutil"
 	"github.com/skycoin/skycoin/src/util/fee"
 )
+
+func TestMain(m *testing.M) {
+	// pin the burn factor to 10
+	params.UserVerifyTxn.BurnFactor = 10
+}
 
 func TestCreate(t *testing.T) {
 	headTime := uint64(time.Now().UTC().Unix())

src/wallet/transaction_test.go
@@ -15,12 +15,17 @@ import (
 
 	"github.com/skycoin/skycoin/src/cipher"
 	"github.com/skycoin/skycoin/src/coin"
+	"github.com/skycoin/skycoin/src/params"
 	"github.com/skycoin/skycoin/src/testutil"
 	"github.com/skycoin/skycoin/src/transaction"
 	"github.com/skycoin/skycoin/src/util/fee"
 	"github.com/skycoin/skycoin/src/wallet"
 	"github.com/skycoin/skycoin/src/wallet/collection"
 )
+
+func TestMain(m *testing.M) {
+	params.UserVerifyTxn.BurnFactor = 10
+}
 
 func TestWalletSignTransaction(t *testing.T) {
 	txnSigned, uxs, seckeys := makeTransaction(t, 4)
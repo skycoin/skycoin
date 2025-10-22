b61560b00d6be747488d5edc3af73be108dd4f0a
Moses Narrow <moe_narrow@use.startmail.com>
2025-10-22 13:48:59 -0500

    fix remaining 3 unconvert errors: remove unnecessary type conversions

--- src/util/useragent/useragent_test.go
+++ src/util/useragent/useragent_test.go
@@ -176,7 +176,7 @@ func TestDataJSON(t *testing.T) {
 	require.Equal(t, `"skycoin:0.25.0"`, string(x))
 
 	var e Data
-	err = json.Unmarshal([]byte(x), &e)
+	err = json.Unmarshal(x, &e)
 	require.NoError(t, err)
 	require.Equal(t, d, e)
 

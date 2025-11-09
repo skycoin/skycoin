d52a7d0198bcca933ef62689360634749ca9a282
Moses Narrow <moe_narrow@use.startmail.com>
2025-11-08 18:18:27 -0600

    update skycoin with newcoin

--- cmd/skycoin/skycoin.go
+++ cmd/skycoin/skycoin.go
@@ -11,9 +11,11 @@ and then run cmd/newcoin
 */
 
 import (
-	"github.com/skycoin/skywire/pkg/skywire-utilities/pkg/flags"
+	cc "github.com/ivanpirog/coloredcobra"
+	"github.com/spf13/cobra"
 
 	"github.com/skycoin/skycoin/cmd/skycoin/commands"
+	"github.com/skycoin/skywire/pkg/skywire-utilities/pkg/flags"
 )
 
 func init() {

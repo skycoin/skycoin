0f7ff75b34851a70c199cca53de41d415150fa96
Moses Narrow <moe_narrow@use.startmail.com>
2025-11-08 20:05:03 -0600

    Fix coin.template formatting to match goimports output
    - Remove extra blank line in var block closing
    - Remove extra spacing in cobra.Command struct fields (Use, Short, Run)
    - Fix closing brace indentation
    - Regenerate root.go from template

--- cmd/skycoin/commands/root.go
+++ cmd/skycoin/commands/root.go
@@ -15,6 +15,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+
 	"github.com/skycoin/skycoin/src/fiber"
 	"github.com/skycoin/skycoin/src/readable"
 	"github.com/skycoin/skycoin/src/skycoin"
@@ -102,7 +103,6 @@ var (
 		VersionURL:            "https://version.skycoin.com/skycoin/version.txt",
 		Bip44Coin:             8000,
 	})
-
 )
 
 func init() {
@@ -111,14 +111,14 @@ func init() {
 
 // RootCmd is the root command
 var RootCmd = &cobra.Command{
-	Use:     "skycoin",
-	Short:   "skycoin wallet",
+	Use:   "skycoin",
+	Short: "skycoin wallet",
 	Long: `
 ┌─┐┬┌─┬ ┬┌─┐┌─┐┬┌┐┌
 └─┐├┴┐└┬┘│  │ │││││
 └─┘┴ ┴ ┴ └─┘└─┘┴┘└┘
 	skycoin wallet`,
-	Run:  func(cmd *cobra.Command, args []string) {
+	Run: func(cmd *cobra.Command, args []string) {
 		// create a new fiber coin instance
 		coin := skycoin.NewCoin(skycoin.Config{
 			Node: nodeConfig,
@@ -139,5 +139,5 @@ var RootCmd = &cobra.Command{
 		if err := coin.Run(); err != nil {
 			os.Exit(1)
 		}
-		},
+	},
 }

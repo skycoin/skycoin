0a1390b0d30b9e6adee23b17ef2968449ff8986a
Moses Narrow <moe_narrow@use.startmail.com>
2025-12-12 14:18:19 -0600

    make format

--- src/daemon/gnet/pool_test.go
+++ src/daemon/gnet/pool_test.go
@@ -1110,7 +1110,7 @@ func TestPoolBroadcastMessage(t *testing.T) {
 	if runtime.GOARCH == "386" {
 		t.Skip("Flaky on 32-bit architecture - see pool_test.go TestPoolBroadcastMessage")
 	}
-	
+
 	resetHandler()
 	EraseMessages()
 	RegisterMessage(BytePrefix, ByteMessage{})

1e4b39cc557d3c43edb9b00a7b8bc351905d6e43
Moses Narrow <moe_narrow@use.startmail.com>
2025-12-29 14:41:32 -0600

    fix: Add stub assembly functions for non-amd64 platforms
    Add fieldMulAsm and fieldSqrAsm stub implementations in field_generic.go
    to satisfy type checker on non-amd64 platforms (arm64, 386, etc).
    
    The stubs panic if called, but they are guarded by `if hasAsm` checks
    in field.go, so they will never be executed on non-amd64 platforms.
    
    This fixes linter errors on darwin/arm64 and other non-amd64 platforms.

--- src/cipher/secp256k1-go/secp256k1-go2/field_generic.go
+++ src/cipher/secp256k1-go/secp256k1-go2/field_generic.go
@@ -4,3 +4,13 @@ package secp256k1go
 
 // hasAsm indicates assembly implementations are NOT available
 const hasAsm = false
+
+// fieldMulAsm is a stub - not used on non-amd64 platforms
+func fieldMulAsm(r, a, b *Field) {
+	panic("fieldMulAsm should not be called on non-amd64 platforms")
+}
+
+// fieldSqrAsm is a stub - not used on non-amd64 platforms
+func fieldSqrAsm(r, a *Field) {
+	panic("fieldSqrAsm should not be called on non-amd64 platforms")
+}

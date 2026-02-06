ae4f7e9898da8329eca2fb892766aa099bf84ae6
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2026-02-06 11:36:55 -0600

    Fix gosec G104 error in cmd/release/skycoin.go

--- cmd/release/skycoin.go
+++ cmd/release/skycoin.go
@@ -34,7 +34,7 @@ func main() {
 		NoExtraNewlines: true,
 		NoBottomNewline: true,
 	})
-	commands.RootCmd.Execute() //nolint:errcheck
+	commands.RootCmd.Execute() //nolint:errcheck,gosec
 }
 
 const help = "{{if .HasAvailableSubCommands}}{{end}} {{if gt (len .Aliases) 0}}\r\n\r\n" +

33c209b6002b084bec4c5add4151d936d715046a
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2026-03-14 09:24:50 -0500

    Remove core-js dependency from gui/static and explorer
    Angular 21 targets modern browsers that natively support Reflect and
    other ES6+ features, making core-js polyfills unnecessary.

--- explorer/src/polyfills.ts
+++ explorer/src/polyfills.ts
@@ -34,10 +34,6 @@
 // import 'core-js/es6/weak-map';
 // import 'core-js/es6/set';
 
-/** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
-
 
 
 /***************************************************************************************************
--- src/gui/static/src/polyfills.ts
+++ src/gui/static/src/polyfills.ts
@@ -34,9 +34,6 @@
 // import 'core-js/es6/weak-map';
 // import 'core-js/es6/set';
 
-/** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-
 
 
 /***************************************************************************************************
